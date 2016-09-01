371ae1d3f9a7e1072aaf3f509e55b1f2b5b1c9e9
chloe <chloeleichen@gmail.com>
2016-09-01 22:37:45 +1000

    use hashhistory on gh-page for now

--- packages/magda-web/src/index.js
+++ packages/magda-web/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Search from './Search';
 import SearchBody from './SearchBody';
 import './index.css';
-import { Router, Route, browserHistory, indexRoute } from 'react-router'
+import { Router, Route, hashHistory, browserHistory, indexRoute } from 'react-router'
 
 // http://baseurl.com/dataset?publisher=australianbroadcastingcorporation&q=tax
 
@@ -11,9 +11,10 @@ import { Router, Route, browserHistory, indexRoute } from 'react-router'
 //<Route path="/magda-web/build/" component={Search}>
 
 let baseurl = location.hostname === "localhost" ? '/' : '/magda-web/build/';
+let history = location.hostname === "localhost" ? browserHistory : hashHistory;
 
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={history}>
       <Route path={baseurl} component={Search}>
         <indexRoute component={Search}/>
         <Route path="/dataset" component={SearchBody}/>
