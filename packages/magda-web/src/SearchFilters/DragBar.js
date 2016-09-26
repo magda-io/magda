8df50ed02fdc5c3ee9c3af1bc3020a0fcc4febfa
chloe <chloeleichen@gmail.com>
2016-09-26 18:28:06 +1000

    drag bar

--- packages/magda-web/src/SearchFilters/DragBar.js
+++ packages/magda-web/src/SearchFilters/DragBar.js
@@ -1,7 +1,59 @@
 import React, { Component } from 'react';
+import {event as d3Event, select as d3Select} from 'd3-selection';
+import {drag as d3Drag} from 'd3-drag';
+
+const r = 15;
+let positions =[
+  {id: 0, y: r},
+  {id: 1, y: 100}
+];
+
 class DragBar extends Component {
+    constructor(props){
+      super(props);
+    }
+
+    componentDidMount(){
+      let that = this;
+      // create the circles
+      this._circles = d3Select(this._g).selectAll('circle')
+      .data(positions).enter().append('circle')
+      .attr('cx', r)
+      .attr('r', r)
+      .style('fill','#3498db')
+      .attr('cy', d=>d.y);
+
+      let dragInteraction = d3Drag().on('start', start).on('drag', drag).on('end', end);
+      this._circles.call(dragInteraction);
+
+      function start(d){
+        d3Select(this).style('fill', 'red');
+      }
+
+      function drag(d){
+        console.log(d3Event.y);
+        let target = d3Select(this);
+        if(d3Event.y> 0 && d3Event.y < that.props.height){
+          positions[d.id].y = d3Event.y;
+          that.update();
+        }
+        target.style('fill', 'red');
+      }
+
+      function end(d){
+        d3Select(this).style('fill', '#3498db');
+      }
+
+    }
+
+    update(){
+      this._circles.data(positions).attr('cy', d=> d.y)
+    }
+
+
     render(){
-      return <svg className='progress-bar'>
+      return <svg className='drag-bar__inner' width={r*2} height={this.props.height}>
+                <g ref={g=>{this._g = g}}/>
              </svg>
     }
 }
--- packages/magda-web/src/SearchFilters/FilterDateRange.js
+++ packages/magda-web/src/SearchFilters/FilterDateRange.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Filter from './Filter';
 import maxBy from 'lodash.maxby';
 import FilterHeader from './FilterHeader';
+import DragBar from './DragBar';
 
 
 class FilterDateRange extends Filter {
@@ -9,14 +10,20 @@ class FilterDateRange extends Filter {
     super(props);
     this.resetStartDate = this.resetStartDate.bind(this);
     this.resetEndDate = this.resetEndDate.bind(this);
+    this.state={
+      searchText: '',
+      startDatePos: undefined,
+      endDatePos: undefined
+    }
   }
 
-  toggleFilter(option){
+
+  toggleFilter(option, i){
     let currentStartDate = +this.props.location.query.startDate;
     let currentEndDate = +this.props.location.query.endDate;
     let optionDate = + option.value;
     // if neither current Start date and end date, then set selection to both
-    if(!currentEndDate && !currentEndDate){
+    if(!currentStartDate && !currentEndDate){
         this.props.updateQuery({ 'startDate': optionDate});
         this.props.updateQuery({ 'endDate': optionDate});
     } else {
@@ -61,7 +68,7 @@ class FilterDateRange extends Filter {
     return false;
   }
 
-  renderCondition(option){
+  renderCondition(option, i){
     if(!option){
       return null;
     }
@@ -69,22 +76,32 @@ class FilterDateRange extends Filter {
       width: +option.hitCount/maxBy(this.props.options, 'hitCount').hitCount * 200 + 'px'
     }
 
-    return <button style={divStyle} type='button' className={`${this.checkActiveOption(option) ? 'is-active' : ''} btn-date-option btn`} onClick={this.toggleFilter.bind(this, option)}>{option.value}</button>;
+    return <button style={divStyle} type='button' className={`${this.checkActiveOption(option) ? 'is-active' : ''} btn-date-option btn`} onClick={this.toggleFilter.bind(this, option, i)}>{option.value}</button>;
   }
 
   render(){
+    // temp, 32 is the height of each option
+    let height = this.props.options.length * 32;
     return (
       <div className='filter'>
         <FilterHeader query={this.props.location.query['startDate']}
                       resetFilter={this.resetFilter}
                       title={this.props.title}/>
         <button className='btn' onClick={this.resetStartDate}>Any start date </button>
+        {this.state.searchText.length === 0 &&
+            <div className='clearfix' id='drag-bar'>
+              <div className='col-xs-1'>
+                <DragBar startDatePos={this.state.startDatePos} endDatePos={this.state.endDatePos} height={height}/>
+              </div>
+              <div className='col-xs-11'>
+                <div className='options'>
+                  {this.props.options.sort((a,b)=>+a.value - b.value).map((option, i)=>
+                        <div key={i}>{this.renderCondition(option, i)}</div>
+                  )}
+                </div>
+            </div>
+        </div>}
 
-        <div className='options'>
-            {this.state.searchText.length === 0 && this.props.options.sort((a,b)=>+a.value - b.value).map((option, i)=>
-                  <div key={i}>{this.renderCondition(option)}</div>
-            )}
-        </div>
         <button className='btn' onClick={this.resetEndDate}>Any end date </button>
       </div>
     );
