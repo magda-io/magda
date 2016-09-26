b3fc76b10b24ad44ccf09f48127743637f8a01b4
chloe <chloeleichen@gmail.com>
2016-09-26 14:58:13 +1000

    add drag bar

---
+++ packages/magda-web/src/SearchFilters/DragBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class DragBar extends Component {
+    render(){
+      return <syg className='progress-bar'>
+             </syg>
+    }
+}
+
+DragBar.propTypes = {};
+DragBar.defaultProps = {};
+
+export default DragBar;
