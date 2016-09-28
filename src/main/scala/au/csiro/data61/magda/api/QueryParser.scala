5f140f58e32c146542de9732186273fb0d6d5ddf
Alex Gilleran <alex.gilleran@nicta.com.au>
2016-09-28 11:50:32 +1000

    Kind of ok query parsing.

--- src/main/scala/au/csiro/data61/magda/api/QueryParser.scala
+++ src/main/scala/au/csiro/data61/magda/api/QueryParser.scala
@@ -5,21 +5,22 @@ import scala.util.parsing.combinator.Parsers
 import scala.util.parsing.input.Reader
 import scala.util.parsing.input.Position
 import scala.util.parsing.input.NoPosition
-import au.csiro.data61.magda.util.DateParser
+import au.csiro.data61.magda.util.DateParser._
+import java.time.Instant
 
-object Tokens {
+private object Tokens {
   sealed trait QueryToken
   case class Quote(exactQuery: String) extends QueryToken
-  //    case class DateFrom(rawDate: String) extends QueryToken
-  //    case class DateTo(rawDate: String) extends QueryToken
-  //    case class Publisher(rawPublisher: String) extends QueryToken
+  case class DateFrom(rawDate: String) extends QueryToken
+  case class DateTo(rawDate: String) extends QueryToken
+  case class Publisher(rawPublisher: String) extends QueryToken
   case class Format(rawFormat: String) extends QueryToken
-  case class FreeText(general: String) extends QueryToken
+  case class FreeTextWord(general: String) extends QueryToken
 }
 
 trait QueryCompilationError
 
-object QueryLexer extends RegexParsers {
+private object QueryLexer extends RegexParsers {
   case class QueryLexerError(message: String) extends QueryCompilationError
 
   override def skipWhitespace = true
@@ -30,19 +31,19 @@ object QueryLexer extends RegexParsers {
   }
 
   private def keyword[R](word: String, constructor: String => R)(): Parser[R] = {
-    s"$word [a-zA-Z0-9_]+".r ^^ { str => constructor(str.substring(2 + word.length)) }
+    s"$word [a-zA-Z0-9_]+".r ^^ { str => constructor(str.substring(1 + word.length)) }
   }
 
-  def freeText: Parser[Tokens.FreeText] = {
-    ".+".r ^^ { str => Tokens.FreeText(str) }
+  def freeTextWord: Parser[Tokens.FreeTextWord] = {
+    "\\s?[^\\s]+\\s?".r ^^ { str => Tokens.FreeTextWord(str) }
   }
 
-  //    def from = keyword("from", Tokens.DateFrom.apply)
-  //    def to = keyword("to", Tokens.DateTo.apply)
-  //    def by = keyword("by", Tokens.Publisher.apply)
+  def from = keyword("from", Tokens.DateFrom.apply)
+  def to = keyword("to", Tokens.DateTo.apply)
+  def by = keyword("by", Tokens.Publisher.apply)
   def as = keyword("as", Tokens.Format.apply)
 
-  def tokens: Parser[List[Tokens.QueryToken]] = phrase(rep1(quote | as | freeText))
+  def tokens: Parser[List[Tokens.QueryToken]] = phrase(rep1(quote | as | from | to | by | freeTextWord))
 
   def apply(code: String): Either[QueryLexerError, List[Tokens.QueryToken]] = {
     parse(tokens, code) match {
@@ -56,14 +57,14 @@ object AST {
   sealed trait QueryAST
   case class And(left: QueryAST, right: QueryAST) extends QueryAST
   case class Quote(quote: String) extends QueryAST
-  //    case class DateFrom(quote: DateParser.ParseResult) extends QueryAST
-  //    case class DateTo(quote: DateParser.ParseResult) extends QueryAST
-  //    case class Publisher(quote: String) extends QueryAST
+  case class DateFrom(instant: Instant) extends QueryAST
+  case class DateTo(instant: Instant) extends QueryAST
+  case class Publisher(quote: String) extends QueryAST
   case class Format(quote: String) extends QueryAST
-  case class FreeText(freeText: String) extends QueryAST
+  case class FreeTextWord(freeText: String) extends QueryAST
 }
 
-object QueryParser extends Parsers {
+private object QueryParser extends Parsers {
   case class QueryParserError(msg: String) extends QueryCompilationError
   override type Elem = Tokens.QueryToken
 
@@ -77,7 +78,7 @@ object QueryParser extends Parsers {
   def query: Parser[AST.QueryAST] = phrase(queryPartList)
   def queryPartList: Parser[AST.QueryAST] = rep1(queryPart) ^^ { case list => list reduceRight AST.And }
   def queryPart: Parser[AST.QueryAST] = {
-    quote | format | freeText
+    quote | format | dateFrom | dateTo | publisher | freeTextWord
   }
 
   private def quote: Parser[AST.Quote] = {
@@ -85,11 +86,38 @@ object QueryParser extends Parsers {
   }
 
   private def format: Parser[AST.Format] = {
-    accept("from", { case Tokens.Format(formatString) => AST.Format(formatString) })
+    accept("format", { case Tokens.Format(formatString) => AST.Format(formatString) })
   }
 
-  private def freeText: Parser[AST.FreeText] = {
-    accept("free text", { case Tokens.FreeText(freeText) => AST.FreeText(freeText) })
+  private def parseDateFromRaw[A >: AST.QueryAST](rawDate: String, applyFn: Instant => A, recoveryFn: => AST.QueryAST) : A = {
+    val date = parseDate(rawDate)
+    date match {
+      case InstantResult(instant) => applyFn(instant)
+      case ConstantResult(constant) => constant match {
+        case Now => applyFn(Instant.now())
+      }
+      case _ => recoveryFn
+    }
+  }
+
+  private def dateFrom: Parser[AST.QueryAST] = {
+    accept("from date", {
+      case Tokens.DateFrom(rawDate) => parseDateFromRaw(rawDate, AST.DateFrom.apply, AST.And(AST.FreeTextWord("from"), AST.FreeTextWord(rawDate)))
+    })
+  }
+
+  private def dateTo: Parser[AST.QueryAST] = {
+    accept("to date", {
+      case Tokens.DateTo(rawDate) => parseDateFromRaw(rawDate, AST.DateTo.apply, AST.And(AST.FreeTextWord("to"), AST.FreeTextWord(rawDate)))
+    })
+  }
+
+  private def publisher: Parser[AST.Publisher] = {
+    accept("publisher", { case Tokens.Publisher(publisher) => AST.Publisher(publisher) })
+  }
+
+  private def freeTextWord: Parser[AST.FreeTextWord] = {
+    accept("free text", { case Tokens.FreeTextWord(freeText) => AST.FreeTextWord(freeText.trim) })
   }
 
   def apply(tokens: Seq[Tokens.QueryToken]): Either[QueryParserError, AST.QueryAST] = {
