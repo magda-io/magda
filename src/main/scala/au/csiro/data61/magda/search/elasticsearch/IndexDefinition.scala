7d696a84188c09dd86954867d5d7d32c45945aa6
Alex Gilleran <AlexGilleran@users.noreply.github.com>
2016-11-24 12:17:07 +1100

    Update IndexDefinition.scala

--- src/main/scala/au/csiro/data61/magda/search/elasticsearch/IndexDefinition.scala
+++ src/main/scala/au/csiro/data61/magda/search/elasticsearch/IndexDefinition.scala
@@ -43,7 +43,7 @@ case class IndexDefinition(
 object IndexDefinition {
   val indices = Seq(new IndexDefinition(
     name = "datasets",
-    version = 10,
+    version = 11,
     definition =
       create.index("datasets")
         .indexSetting("recovery.initial_shards", 1)
@@ -68,7 +68,7 @@ object IndexDefinition {
         ).analysis(CustomAnalyzerDefinition("untokenized", KeywordTokenizer, LowercaseTokenFilter))),
     new IndexDefinition(
       name = "regions",
-      version = 11,
+      version = 10,
       definition =
         create.index("regions")
           .indexSetting("recovery.initial_shards", 1)
