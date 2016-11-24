9c83baa1f61af7aff028bfed3d55ec9f05a3df5a
chloe <chloeleichen@gmail.com>
2016-11-24 15:20:31 +1100

    add tests

--- packages/magda-web/src/helpers/defined.js
+++ packages/magda-web/src/helpers/defined.js
@@ -1,15 +1,3 @@
-/*global define*/
-define(function() {
-    /**
-     * @exports defined
-     *
-     * @param {Object} value The object.
-     * @returns {Boolean} Returns true if the object is defined, returns false otherwise.
-     *
-     */
-    function defined(value) {
-        return value !== undefined && value !== null;
-    }
-
-    return defined;
-});
+export default function defined(value) {
+    return value !== undefined && value !== null;
+}
--- packages/magda-web/src/helpers/parseQuery.js
+++ packages/magda-web/src/helpers/parseQuery.js
@@ -1,6 +1,5 @@
-import defined from './defined'
-// temp
-const NUMBERRESULTSPERPAGE = 20;
+import defined from './defined';
+import config from '../config';
 
 export default function(query){
   let keyword = defined(query.q) ? query.q : '';
@@ -9,9 +8,9 @@ export default function(query){
   let publisher = queryToString('+by', query.publisher);
   let format = queryToString('+as', query.format);
   let location = queryToLocation(query.regionId, query.regionType);
-  let startIndex = defined(query.page) ? (query.page - 1)*NUMBERRESULTSPERPAGE + 1 : 0;
+  let startIndex = defined(query.page) ? (query.page - 1)*config().resultsPerPage + 1 : 0;
 
-  let apiQuery = encodeURI(`${keyword}${publisher}${format}${dateFrom}${dateTo}${location}&start=${startIndex}&limit=${NUMBERRESULTSPERPAGE}`);
+  let apiQuery = encodeURI(`${keyword}${publisher}${format}${dateFrom}${dateTo}${location}&start=${startIndex}&limit=${config().resultsPerPage}`);
   return apiQuery;
 }
 
