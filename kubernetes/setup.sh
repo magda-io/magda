bb2abcd85473b069027a100c0509179814a8d595
Kevin Ring <kevin@kotachrome.com>
2017-01-17 10:11:34 +1100

    Fix setup.sh to use the right dockerfile for the elasticsearch container.

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -8,7 +8,7 @@ kubectl create -f kubernetes/registry.yml
 docker build -t localhost:5000/data61/magda-metadata-local:latest -f docker/api.local.dockerfile docker
 docker push localhost:5000/data61/magda-metadata-local:latest
 
-docker build -t localhost:5000/data61/elasticsearch-kubernetes:2.4.1 -f docker/api.local.dockerfile docker
+docker build -t localhost:5000/data61/elasticsearch-kubernetes:2.4.1 -f docker/elasticsearch-kubernetes.dockerfile docker
 docker push localhost:5000/data61/elasticsearch-kubernetes:2.4.1
 
 kubectl create -f target/kubernetes/local.yml
