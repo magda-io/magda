88e083cc0710cc57dada9047ff2b38c2773f25f3
Kevin Ring <kevin@kotachrome.com>
2017-01-17 11:10:32 +1100

    Revert unnecessary change to setup.sh.

--- deploy/kubernetes/setup.sh
+++ deploy/kubernetes/setup.sh
@@ -1,6 +1,6 @@
-#minikube config set memory 4096
-#minikube start
-#eval $(minikube docker-env)
+minikube config set memory 4096
+minikube start
+eval $(minikube docker-env)
 
 sbt editsource:clean editsource:edit
 kubectl create -f deploy/kubernetes/registry.yml
@@ -12,4 +12,4 @@ docker build -t localhost:5000/data61/elasticsearch-kubernetes:2.4.1 -f deploy/d
 docker push localhost:5000/data61/elasticsearch-kubernetes:2.4.1
 
 kubectl create -f target/kubernetes/local.yml
-#minikube service api
+minikube service api
