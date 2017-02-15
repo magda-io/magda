4a02a0dbfca9fec40818a673b88421e843cf9f81
Kevin Ring <kevin@kotachrome.com>
2017-02-15 18:27:16 +1100

    Dodgy swagger generation script with hardcoded paths.

--- CkanConnector/src/index.ts
+++ CkanConnector/src/index.ts
@@ -1,4 +1,4 @@
-import { AspectDefinition, AspectDefinitionsApi } from '../generated/registry/api'
+import { AspectDefinition, AspectDefinitionsApi } from './generated/registry/api'
 import { Observable } from 'rx'
 import retry from './retry'
 
@@ -6,41 +6,27 @@ const aspectDefinitions = [
     {
         id: 'basic',
         name: 'Basic Information',
-        jsonSchema: require('../../../registry-aspects/basic.schema.json')
+        jsonSchema: require('../../registry-aspects/basic.schema.json')
     },
     {
         id: 'source',
         name: 'Source',
-        jsonSchema: require('../../../registry-aspects/source.schema.json')
+        jsonSchema: require('../../registry-aspects/source.schema.json')
     }
 ];
 
 function createAspectDefinitions(aspectDefinitions: AspectDefinition[]) {
-    const api = new AspectDefinitionsApi();
+    const api = new AspectDefinitionsApi("http://localhost:6100/api/0.1");
 
     const aspectDefinitionSource = Observable.from(aspectDefinitions).controlled();
 
     const promise = aspectDefinitionSource.flatMap(aspectDefinition => {
-        return api.putById({
-            id: aspectDefinition.id,
-            aspect: aspectDefinition
-        }).then(result => {
+        return api.putById(aspectDefinition.id, aspectDefinition).then(result => {
             aspectDefinitionSource.request(1);
             return result;
         }).catch(e => {
             aspectDefinitionSource.request(1);
-            if (!e.json) {
-                throw e;
-            } else {
-                const throwPromise: Promise<AspectDefinition> = e.json().catch(jsonError => {
-                    // If we can't get JSON from the response, rethrow the original error.
-                    throw e;
-                }).then(json => {
-                    // The JSON is our actual error.
-                    throw json;
-                });
-                return throwPromise;
-            }
+            throw e;
         });
     }).toArray().toPromise();
 
@@ -53,7 +39,13 @@ function createAspectDefinitions(aspectDefinitions: AspectDefinition[]) {
 retry(
     () => createAspectDefinitions(aspectDefinitions),
     2, 2,
-    (e, retriesLeft) => console.log(`Failed to create aspect definition, ${retriesLeft} retries left.  Error was:\n${JSON.stringify(e)}`)
+    (e, retriesLeft) => {
+        if (e.response && e.response.statusCode && e.body) {
+            console.log(`Failed to create aspect definition, ${retriesLeft} retries left.  Status code: ${e.response.statusCode}, body:\n${JSON.stringify(e.body, null, '  ')}`);
+        } else {
+            console.log(`Failed to create aspect definition, ${retriesLeft} retries left.  Exception:\n${e.toString()}`);
+        }        
+    }
 ).then(createdAspects => {
     console.log(createdAspects);
 }).catch(e => {
