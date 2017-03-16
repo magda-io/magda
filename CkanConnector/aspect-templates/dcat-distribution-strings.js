b49a80e54549de11a59966308859d4c693dd3aa0
Kevin Ring <kevin@kotachrome.com>
2017-03-16 17:16:35 +1100

    Don't add CKAN's nulls for modified/created either.

--- CkanConnector/aspect-templates/dcat-distribution-strings.js
+++ CkanConnector/aspect-templates/dcat-distribution-strings.js
@@ -1,8 +1,8 @@
 return {
     title: resource.name || resource.id,
     description: resource.description || undefined,
-    issued: resource.created, // TODO: handle time zone
-    modified: resource.last_modified,
+    issued: resource.created || undefined, // TODO: handle time zone
+    modified: resource.last_modified || undefined,
     license: dataset.license_title || undefined,
     accessURL: resource.webstore_url || undefined,
     downloadURL: resource.url || undefined,
