622048fb6876609c0b64f4698554a65295dd5c82
Kevin Ring <kevin@kotachrome.com>
2017-03-16 11:24:48 +1100

    Don't store nulls from CKAN in aspects.

--- CkanConnector/aspect-templates/dcat-distribution-strings.js
+++ CkanConnector/aspect-templates/dcat-distribution-strings.js
@@ -1,11 +1,11 @@
 return {
     title: resource.name || resource.id,
-    description: resource.description,
+    description: resource.description || undefined,
     issued: resource.created, // TODO: handle time zone
     modified: resource.last_modified,
-    license: dataset.license_title,
-    accessURL: resource.webstore_url,
-    downloadURL: resource.url,
-    mediaType: resource.mimetype,
-    format: resource.format
+    license: dataset.license_title || undefined,
+    accessURL: resource.webstore_url || undefined,
+    downloadURL: resource.url || undefined,
+    mediaType: resource.mimetype || undefined,
+    format: resource.format || undefined
 };
