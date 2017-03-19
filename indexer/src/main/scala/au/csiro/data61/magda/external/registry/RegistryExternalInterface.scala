983c027d81189b00f166d7d64c1550d276de9f62
Kevin Ring <kevin@kotachrome.com>
2017-03-20 10:39:15 +1100

    Tweak URLs.

--- indexer/src/main/scala/au/csiro/data61/magda/external/registry/RegistryExternalInterface.scala
+++ indexer/src/main/scala/au/csiro/data61/magda/external/registry/RegistryExternalInterface.scala
@@ -22,11 +22,11 @@ class RegistryExternalInterface(interfaceConfig: InterfaceConfig, implicit val c
 
   override def getInterfaceConfig = interfaceConfig
 
-  val baseUrl = s"api/0.1/records?aspect=dcat-dataset-strings&optionalAspect=temporal-coverage"
+  val baseUrl = s"api/0.1/records?aspect=dcat-dataset-strings"
 
   override def getDataSets(start: Long, number: Int): scala.concurrent.Future[List[DataSet]] =
     // TODO: start as pageToken is not right.
-    fetcher.request(s"$baseUrl&optionalAspect=dataset-distributions&dereference=true&pageToken=$start&limit=$number").flatMap { response =>
+    fetcher.request(s"$baseUrl&optionalAspect=dataset-distributions&optionalAspect=temporal-coverage&dereference=true&pageToken=$start&limit=$number").flatMap { response =>
       response.status match {
         case OK => Unmarshal(response.entity).to[RegistryRecordsResponse].map { registryResponse =>
           mapCatching[RegistryRecord, DataSet](registryResponse.records,
