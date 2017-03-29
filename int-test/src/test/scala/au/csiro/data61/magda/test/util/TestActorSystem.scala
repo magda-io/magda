a88c521b7b9db08fec5cd88265686e0b72080fc9
Alex Gilleran <alex.gilleran@nicta.com.au>
2017-03-29 16:12:37 +1100

    Updating non-existent log level.

--- int-test/src/test/scala/au/csiro/data61/magda/test/util/TestActorSystem.scala
+++ int-test/src/test/scala/au/csiro/data61/magda/test/util/TestActorSystem.scala
@@ -8,7 +8,7 @@ import au.csiro.data61.magda.test.util.ContinuousIntegration
 object TestActorSystem {
   // This has to be separated out to make overriding the config work for some stupid reason.
   val config = ConfigFactory.parseString(s"""
-    akka.loglevel = ${if (ContinuousIntegration.isCi) "WARN" else "INFO"}
+    akka.loglevel = ${if (ContinuousIntegration.isCi) "WARNING" else "INFO"}
   """).resolve().withFallback(AppConfig.conf(Some("local")))
 
   def actorSystem = ActorSystem("TestActorSystem", config)
