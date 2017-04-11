9023ca06093fd836487551c4be2db39af0dbabef
Kevin Ring <kevin@kotachrome.com>
2017-04-11 16:19:03 +1000

    Remove unnecessary imports.

--- registry-api/src/test/scala/au/csiro/data61/magda/registry/AspectsServiceSpec.scala
+++ registry-api/src/test/scala/au/csiro/data61/magda/registry/AspectsServiceSpec.scala
@@ -1,12 +1,6 @@
 package au.csiro.data61.magda.registry
 
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.testkit.ScalatestRouteTest
-import akka.testkit.TestProbe
-import org.scalatest.Matchers
-import org.scalatest.fixture.FunSpec
-import scalikejdbc._
 
 class AspectsServiceSpec extends ApiSpec {
   it("starts with none defined") { api =>
