0f978af2ac6813cb6e2f9eea136ed7e71c18d734
Kevin Ring <kevin@kotachrome.com>
2017-04-11 14:43:04 +1000

    Remove a couple more imports.

--- registry-api/src/main/scala/au/csiro/data61/magda/registry/Api.scala
+++ registry-api/src/main/scala/au/csiro/data61/magda/registry/Api.scala
@@ -9,8 +9,6 @@ import akka.actor.ActorRef
 import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
