1240ccc5f816e80ecd125a81784e68dd88b31a39
Kevin Ring <kevin@kotachrome.com>
2017-04-11 16:18:30 +1000

    More tests, API testing base class.

--- registry-api/src/test/scala/au/csiro/data61/magda/registry/AspectsServiceSpec.scala
+++ registry-api/src/test/scala/au/csiro/data61/magda/registry/AspectsServiceSpec.scala
@@ -3,53 +3,97 @@ package au.csiro.data61.magda.registry
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.testkit.TestProbe
 import org.scalatest.Matchers
-import org.scalatest.FunSpec
+import org.scalatest.fixture.FunSpec
 import scalikejdbc._
-import scalikejdbc.scalatest.AutoRollback
 
-class AspectsServiceSpec extends FunSpec with ScalatestRouteTest with Matchers with Protocols with SprayJsonSupport {
-  private val webHookActor = system.actorOf(WebHookActor.props, name = "WebHookActor")
-  private val api = new Api(webHookActor, testConfig, system, executor, materializer)
-
-  override def testConfigSource = "db.default.url = \"jdbc:postgresql://localhost/postgres?currentSchema=test\""
-
-  override def withFixture(test: NoArgTest) = {
-    DB localTx { implicit session =>
-      sql"DROP SCHEMA IF EXISTS test CASCADE".update.apply()
-      sql"CREATE SCHEMA test".update.apply()
-
-      val stream = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/evolveschema.sql"))
-      val sqlString = stream.mkString
-      val initializeSql = SQL(sqlString)
-      stream.close()
-      initializeSql.update().apply()
+class AspectsServiceSpec extends ApiSpec {
+  it("starts with none defined") { api =>
+    Get("/api/0.1/aspects") ~> api.routes ~> check {
+      status shouldEqual StatusCodes.OK
+      responseAs[List[AspectDefinition]].length shouldEqual 0
     }
-
-    super.withFixture(test)
   }
 
-  describe("Aspects") {
-    it("starts with none defined") {
+  it("can be added with POST") { api =>
+    val aspectDefinition = AspectDefinition("testId", "testName", None)
+    Post("/api/0.1/aspects", aspectDefinition) ~> api.routes ~> check {
+      status shouldEqual StatusCodes.OK
+      responseAs[AspectDefinition] shouldEqual AspectDefinition("testId", "testName", None)
+
       Get("/api/0.1/aspects") ~> api.routes ~> check {
         status shouldEqual StatusCodes.OK
-        responseAs[List[AspectDefinition]].length shouldEqual 0
+
+        val aspectDefinitions = responseAs[List[AspectDefinition]]
+        aspectDefinitions.length shouldEqual 1
+        aspectDefinitions(0) shouldEqual AspectDefinition("testId", "testName", None)
       }
     }
+  }
 
-    it("can be added") {
-      val aspectDefinition = AspectDefinition("testId", "testName", None)
-      Post("/api/0.1/aspects", aspectDefinition) ~> api.routes ~> check {
+  it("can be added with PUT") { api =>
+    val aspectDefinition = AspectDefinition("testId", "testName", None)
+    Put("/api/0.1/aspects/testId", aspectDefinition) ~> api.routes ~> check {
+      status shouldEqual StatusCodes.OK
+      responseAs[AspectDefinition] shouldEqual AspectDefinition("testId", "testName", None)
+
+      Get("/api/0.1/aspects") ~> api.routes ~> check {
+        status shouldEqual StatusCodes.OK
+
+        val aspectDefinitions = responseAs[List[AspectDefinition]]
+        aspectDefinitions.length shouldEqual 1
+        aspectDefinitions(0) shouldEqual AspectDefinition("testId", "testName", None)
+      }
+    }
+  }
+
+  it("can be updated with PUT") { api =>
+    val aspectDefinition = AspectDefinition("testId", "testName", None)
+    Post("/api/0.1/aspects", aspectDefinition) ~> api.routes ~> check {
+      val newDefinition = aspectDefinition.copy(name = "newName")
+      Put("/api/0.1/aspects/testId", newDefinition) ~> api.routes ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[AspectDefinition] shouldEqual AspectDefinition("testId", "newName", None)
+      }
+    }
+  }
+
+  it("can be retrieved by ID") { api =>
+    val aspectDefinition = AspectDefinition("testId", "testName", None)
+    Put("/api/0.1/aspects/testId", aspectDefinition) ~> api.routes ~> check {
+      status shouldEqual StatusCodes.OK
+      responseAs[AspectDefinition] shouldEqual AspectDefinition("testId", "testName", None)
+
+      Get("/api/0.1/aspects/testId") ~> api.routes ~> check {
         status shouldEqual StatusCodes.OK
         responseAs[AspectDefinition] shouldEqual AspectDefinition("testId", "testName", None)
+      }
+    }
+  }
 
-        Get("/api/0.1/aspects") ~> api.routes ~> check {
-          status shouldEqual StatusCodes.OK
+  it("POST supports invalid URL characters in ID") { api =>
+    val aspectDefinition = AspectDefinition("in valid", "testName", None)
+    Post("/api/0.1/aspects", aspectDefinition) ~> api.routes ~> check {
+      status shouldEqual StatusCodes.OK
+      responseAs[AspectDefinition] shouldEqual AspectDefinition("in valid", "testName", None)
 
-          val aspectDefinitions = responseAs[List[AspectDefinition]]
-          aspectDefinitions.length shouldEqual 1
-          aspectDefinitions(0) shouldEqual AspectDefinition("testId", "testName", None)
-        }
+      Get("/api/0.1/aspects/in%20valid") ~> api.routes ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[AspectDefinition] shouldEqual AspectDefinition("in valid", "testName", None)
+      }
+    }
+  }
+
+  it("PUT supports invalid URL characters in ID") { api =>
+    val aspectDefinition = AspectDefinition("in valid", "testName", None)
+    Put("/api/0.1/aspects/in%20valid", aspectDefinition) ~> api.routes ~> check {
+      status shouldEqual StatusCodes.OK
+      responseAs[AspectDefinition] shouldEqual AspectDefinition("in valid", "testName", None)
+
+      Get("/api/0.1/aspects/in%20valid") ~> api.routes ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[AspectDefinition] shouldEqual AspectDefinition("in valid", "testName", None)
       }
     }
   }
