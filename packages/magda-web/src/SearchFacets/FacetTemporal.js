b460e45044893515ae763e2fea203f688c605c9f
chloe <chloeleichen@gmail.com>
2017-05-04 15:45:55 +1000

    more facets

--- packages/magda-web/src/SearchFacets/FacetTemporal.js
+++ packages/magda-web/src/SearchFacets/FacetTemporal.js
@@ -14,12 +14,10 @@ const itemHeight = 37;
 class FacetTemporal extends Component {
   constructor(props) {
     super(props);
-    this.renderDragBar = this.renderDragBar.bind(this);
     this.onResetDateTo = this.onResetDateTo.bind(this);
     this.onResetDateFrom = this.onResetDateFrom.bind(this);
     this.onToggleOption = this.onToggleOption.bind(this);
     this.onToggleOpen = this.onToggleOpen.bind(this);
-    this.onDrag = this.onDrag.bind(this);
     this.state = {
       isOpen: false
     }
@@ -90,7 +88,6 @@ class FacetTemporal extends Component {
   }
 
   renderOption(option, onClick){
-    let marginBottom = 1;
     if(!option){
       return null;
     }
@@ -98,16 +95,10 @@ class FacetTemporal extends Component {
       width: +option.hitCount/maxBy(this.props.options, 'hitCount').hitCount * 160 + 'px'
     }
 
-    let buttonStyle = {
-      height: itemHeight - marginBottom + 'px',
-      marginBottom: marginBottom + 'px'
-    }
-
     return (
     <button type='button'
             className={`${this.checkActiveOption(option) ? 'is-active' : ''} btn-facet-option btn-facet-date-option btn`}
             onClick={onClick.bind(this, option)}
-            style={buttonStyle}
             >
       <span style={divStyle} className='btn-facet-option__volume-indicator'/>
       <span className='btn-facet-option__name'>{option.value}</span>
@@ -115,35 +106,7 @@ class FacetTemporal extends Component {
     </button>)
   }
 
-  /**
-   * When dragging the dragbar, update the bar positions and update the url
-   * @param {number} id id here indicates bar is dragged therefore which property needs updates. if id === 0, then it the top bar being dragged therefore 'dateTo' should be updated
-   * @param {number} value the position relative to the wrapper that the drag bar has been dragged to
-   */
-  onDrag(id, value){
-    // the index of the option that the dragged bar position corresponds to
-    // offset by one, for the "any end date" option
-    let index = Math.floor(value / itemHeight) - 1;
-    let date = this.props.options[index];
-    let datesArray = this.props.activeDates.slice();
 
-    // only updates if the dragged position is within the range
-    if(index > 0 && index < this.props.options.length){
-      // if it's upper bound
-      if(id === 0){
-        datesArray[1] = date.upperBound;
-      } else{
-        datesArray[0] = date.lowerBound;
-      }
-    } else{
-      if(id=== 0){
-        datesArray[1] = date.upperBound;
-      } else {
-        datesArray[0] = date.lowerBound;
-      }
-    }
-    this.props.onToggleOption(datesArray);
-  }
 
   findLowerBound(){
     // find all bound and pick the smallest index
@@ -169,16 +132,6 @@ class FacetTemporal extends Component {
 
   }
 
-  renderDragBar(){
-    // the height of the dragbar should be the same with the height of all the options + any start date + any end date
-    // remove last padding
-    let height = (this.props.options.length + 2) * itemHeight - 4;
-    let fromIndex = defined(this.props.activeDates[0]) ? this.findLowerBound() : this.props.options.length + 1;
-    let toIndex = defined(this.props.activeDates[1]) ? this.findUpperBound() : 0;
-
-    let dragBarData=[(toIndex * itemHeight), (fromIndex * itemHeight)];
-    return <DragBar dragBarData={dragBarData} onDrag={this.onDrag} height={height}/>
-  }
 
   render(){
     let that = this;
@@ -189,9 +142,6 @@ class FacetTemporal extends Component {
                      hasQuery={this.props.hasQuery}
                      onClick={this.onToggleOpen}/>
              {this.state.isOpen && <div className='clearfix facet-temporal facet-body'>
-               <div className='slider'>
-                 {this.renderDragBar()}
-               </div>
                <ul className='list-unstyled list'>
                 <li><button className='btn btn-facet-option btn-facet-date-option' onClick={this.onResetDateTo}>Any end date </button></li>
                  {that.props.options.map(o=><li key={o.value}>{that.renderOption(o, this.onToggleOption)}</li>)}
