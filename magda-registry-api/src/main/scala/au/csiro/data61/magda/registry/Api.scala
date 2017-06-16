ef65cc2628ef2305939b9e279af71f5c2fc5cc1e
Kevin Ring <kevin@kotachrome.com>
2017-06-16 17:48:21 +1000

    WIP verify security rules in the registry API.

--- magda-registry-api/src/main/scala/au/csiro/data61/magda/registry/Api.scala
+++ magda-registry-api/src/main/scala/au/csiro/data61/magda/registry/Api.scala
@@ -10,7 +10,7 @@ import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes._
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{HttpMethod, HttpMethods, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
 import akka.stream.Materializer
@@ -69,18 +69,30 @@ class Api(val webHookActor: ActorRef, implicit val config: Config, implicit val 
 
   webHookActor ! WebHookActor.Process
 
+  def checkCredentials(allowAnonymous: Seq[HttpMethod], allowAuthenticated: Seq[HttpMethod]) = (request: RequestContext) => {
+    if (allowAnonymous.contains(request.request.method)) {
+      true
+    } else if (allowAuthenticated.contains(request.request.method)) {
+      true // TODO: check that this is an authenticated user
+    } else {
+      false // TODO: check if we're admin
+    }
+  }
+
+  def checkAuthorization(allowAnonymous: Seq[HttpMethod], allowAuthenticated: Seq[HttpMethod]) = authorize(checkCredentials(allowAnonymous, allowAuthenticated))
+
   implicit val timeout = Timeout(FiniteDuration(1, TimeUnit.SECONDS))
   val routes = cors() {
     handleExceptions(myExceptionHandler) {
       pathPrefix("v0") {
         path("ping") { complete("OK") } ~
-          pathPrefix("aspects") { new AspectsService(system, materializer).route } ~
-          pathPrefix("records") { new RecordsService(webHookActor, system, materializer).route } ~
-          pathPrefix("hooks") { new HooksService(system, materializer).route } ~
-          new SwaggerDocService("localhost", 9001, system).allRoutes ~
-          pathPrefix("swagger") {
-            getFromResourceDirectory("swagger") ~ pathSingleSlash(get(redirect("index.html", StatusCodes.PermanentRedirect)))
-          }
+        pathPrefix("aspects") { checkAuthorization(Seq(HttpMethods.GET), Seq()) { new AspectsService(system, materializer).route } } ~
+        pathPrefix("records") { checkAuthorization(Seq(HttpMethods.GET), Seq(HttpMethods.POST)) { new RecordsService(webHookActor, system, materializer).route } } ~
+        pathPrefix("hooks") { checkAuthorization(Seq(HttpMethods.GET), Seq()) { new HooksService(system, materializer).route } } ~
+        new SwaggerDocService("localhost", 9001, system).allRoutes ~
+        pathPrefix("swagger") {
+          getFromResourceDirectory("swagger") ~ pathSingleSlash(get(redirect("index.html", StatusCodes.PermanentRedirect)))
+        }
       }
     }
   }
