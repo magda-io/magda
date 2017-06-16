d401d794b4ebdad80d8e5bd4296cf12197a13e36
Alex Gilleran <alex.gilleran@nicta.com.au>
2017-06-16 16:24:12 +1000

    Changed the info coming back from a login.

--- magda-gateway/src/oauth2/redirect.ts
+++ magda-gateway/src/oauth2/redirect.ts
@@ -4,15 +4,13 @@ import { URL } from 'url'
 export function redirectOnSuccess(toURL: string, req: Request, res: Response) {
     const source = new URL(toURL);
     source.searchParams.set('result', 'success');
-    source.searchParams.set('expires', (<Date>(<any>req).session.cookie._expires).toISOString());
-    source.searchParams.delete('reason');
+    source.searchParams.delete('errorMessage');
     res.redirect(<string>source.href);
 }
 
 export function redirectOnError(err: any, toURL: string, req: Request, res: Response) {
     const source = new URL(toURL);
     source.searchParams.set('result', 'failure');
-    source.searchParams.set('reason', err);
-    source.searchParams.delete('expires');
+    source.searchParams.set('errorMessage', err);
     res.redirect(<string>source.href);
 }
