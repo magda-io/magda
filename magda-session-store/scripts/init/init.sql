204f75ab63c6647676997d404c7fc11713536079
Alex Gilleran <alex.gilleran@nicta.com.au>
2017-06-16 10:28:35 +1000

    Adding session init sql.

---
+++ magda-session-store/scripts/init/init.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE session
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+\connect session
+
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+	"sess" json NOT NULL,
+	"expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
