48fec2af03c0a7b83e3f3b1c7e2c5677814b05e3
Chloe <chloeleichen@gmail.com>
2017-06-27 13:33:12 +1000

    remove subscribe

--- magda-web-client/src/Components/Social.js
+++ magda-web-client/src/Components/Social.js
@@ -9,7 +9,6 @@ export default class Social extends React.Component {
     const url: string = window.location.href;
     return (
       <div className='social'>
-        <div><a className='btn btn-default' href={config.rssUrl} target='_blank'><i className='fa fa-rss' aria-hidden='true'></i>Subscribe</a></div>
         <div>
           <a className='twitter-share-button btn btn-default' href={`https://twitter.com/intent/tweet?url=${url}`}
           data-size='large'><i className='fa fa-twitter' aria-hidden='true'></i>Tweet</a>
