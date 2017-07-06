5f1ffd62063d29588f05f6e6c1154d9cd572b697
chloe <chloeleichen@gmail.com>
2017-07-06 16:41:08 +1000

    a chart for testing

--- magda-web-client/src/Dataset/DatasetVisualisation.js
+++ magda-web-client/src/Dataset/DatasetVisualisation.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import VegaLite from 'react-vega-lite';
 
 class DatasetVisualisation extends Component {
-  render(){
-    const datasrc = this.props.dataset;
-    debugger
-    return <div className='dataset-details container' >
 
+  render(){
+    const spec = {
+      "description": "A simple bar chart with embedded data.",
+      "mark": "bar",
+      "encoding": {
+        "x": {"field": "a", "type": "ordinal"},
+        "y": {"field": "b", "type": "quantitative"}
+      }
+    };
+
+    const barData = {
+      "values": [
+        {"a": "A","b": 20}, {"a": "B","b": 34}, {"a": "C","b": 55},
+        {"a": "D","b": 19}, {"a": "E","b": 40}, {"a": "F","b": 34},
+        {"a": "G","b": 91}, {"a": "H","b": 78}, {"a": "I","b": 25}
+      ]
+    };
+    return <div className='dataset-details container' >
+              <div className='vis'>
+                <VegaLite spec={spec} data={barData} />
+              </div>
           </div>
   }
 }
