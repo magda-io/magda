da2daf336f7c995bbce445011c1a21fe9200bd39
chloe <chloeleichen@gmail.com>
2017-07-07 13:55:24 +1000

    data-vis

--- magda-web-client/src/Dataset/DatasetVisualisation.js
+++ magda-web-client/src/Dataset/DatasetVisualisation.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import VegaLite from 'react-vega-lite';
+import ReactTable from 'react-table';
+import JsonForm from 'react-json';
+import '../UI/ReactTable.css';
+import './DatasetVisualisation.css'
+
+const defaultSpec = {
+  "description": "Example data.",
+  "mark": "bar",
+  "encoding": {
+    "x": {"field": "a", "type": "ordinal"},
+    "y": {"field": "b", "type": "quantitative"}
+  }
+}
 
 class DatasetVisualisation extends Component {
 
-  render(){
-    const spec = {
-      "description": "A simple bar chart with embedded data.",
-      "mark": "bar",
-      "encoding": {
-        "x": {"field": "a", "type": "ordinal"},
-        "y": {"field": "b", "type": "quantitative"}
-      }
-    };
+  constructor(props) {
+    super(props);
+    this.state = {spec: defaultSpec};
+    this.logChange = this.logChange.bind(this);
+  }
 
+  logChange(data){
+    // need to validate data
+    this.setState({
+      spec: data
+    })
+  }
+
+  render(){
     const barData = {
       "values": [
         {"a": "A","b": 20}, {"a": "B","b": 34}, {"a": "C","b": 55},
@@ -21,9 +38,39 @@ class DatasetVisualisation extends Component {
         {"a": "G","b": 91}, {"a": "H","b": 78}, {"a": "I","b": 25}
       ]
     };
-    return <div className='dataset-details container' >
+
+    const columns = [
+      {Header: 'a', accessor: 'a'},
+      {Header: 'b', accessor: 'b'}
+    ]
+
+    const settings = {
+      form: true,
+      fields: {
+        description: {type: 'string'},
+        mark: {type: 'select', settings: {options: ['bar', 'line']}},
+      }
+    }
+    return <div className='dataset-visualization container' >
               <div className='vis'>
-                <VegaLite spec={spec} data={barData} />
+                <div className="row">
+
+                  <div className="col-sm-6">
+                    <h2>{this.state.spec.description}</h2>
+                    <VegaLite spec={this.state.spec} data={barData} />
+                  </div>
+                  <div className="col-sm-6"><div className='json'>
+                    <h2>Customise visualisation</h2>
+                    <JsonForm value={ this.state.spec } onChange={ this.logChange } settings={settings}/>,
+                  </div>
+                  </div>
+                </div>
+                <h2>{this.state.spec.description}</h2>
+                <ReactTable
+                  minRows={3}
+                  data={barData.values}
+                  columns={columns}
+                />
               </div>
           </div>
   }
