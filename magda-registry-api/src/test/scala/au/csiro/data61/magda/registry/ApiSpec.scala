f4696e42361495d17b2246cdf18cc46c981792e6
Kevin Ring <kevin@kotachrome.com>
2017-07-20 14:04:56 +1000

    Tone down Flyway's logging during tests.

--- magda-registry-api/src/test/scala/au/csiro/data61/magda/registry/ApiSpec.scala
+++ magda-registry-api/src/test/scala/au/csiro/data61/magda/registry/ApiSpec.scala
@@ -3,9 +3,11 @@ package au.csiro.data61.magda.registry
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.testkit.TestProbe
+import ch.qos.logback.classic.{Level, Logger}
 import org.flywaydb.core.Flyway
 import org.scalatest.Matchers
 import org.scalatest.fixture.FunSpec
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.duration._
 import scalikejdbc._
@@ -15,10 +17,19 @@ abstract class ApiSpec extends FunSpec with ScalatestRouteTest with Matchers wit
 
   val databaseUrl = Option(System.getenv("npm_package_config_databaseUrl")).getOrElse("jdbc:postgresql://localhost:5432/postgres")
 
+  // Stop Flyway from producing so much spam that Travis terminates the process.
+  LoggerFactory.getLogger("org.flywaydb").asInstanceOf[Logger].setLevel(Level.WARN)
+
+  val flyway = new Flyway()
+  flyway.setDataSource(databaseUrl, "postgres", "")
+  flyway.setSchemas("test")
+  flyway.setLocations("classpath:/sql")
+
   override def testConfigSource =
     s"""
       |db.default.url = "${databaseUrl}?currentSchema=test"
       |authorization.skip = true
+      |akka.loglevel = INFO
     """.stripMargin
 
   override def withFixture(test: OneArgTest) = {
@@ -32,10 +43,6 @@ abstract class ApiSpec extends FunSpec with ScalatestRouteTest with Matchers wit
       sql"CREATE SCHEMA test".update.apply()
     }
 
-    val flyway = new Flyway()
-    flyway.setDataSource(databaseUrl, "postgres", "")
-    flyway.setSchemas("test")
-    flyway.setLocations("classpath:/sql")
     flyway.migrate()
 
     super.withFixture(test.toNoArgTest(FixtureParam(api, webHookActorProbe)))
