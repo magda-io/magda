a02c6f5574661eae002b8f90870af7fc5b0b4807
chloe <chloeleichen@gmail.com>
2017-07-27 10:54:23 +1000

    use pre for text

--- magda-web-client/src/UI/DataPreviewTextBox.js
+++ magda-web-client/src/UI/DataPreviewTextBox.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import OverviewBox from './OverviewBox';
-import MarkdownViewer from '../UI/MarkdownViewer';
 import './DataPreviewTextBox.css';
 class DataPreviewTextBox extends Component {
     render(){
       return <div className='data-preview-text-box'>
-              <MarkdownViewer markdown={this.props.data.data}/ >
+              <pre>{this.props.data.data}</pre>
              </div>
     }
 }
