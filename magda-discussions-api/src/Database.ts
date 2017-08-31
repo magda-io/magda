8f05feeb447891364636b2151dbe48f14a0b40fc
Kevin Ring <kevin@kotachrome.com>
2017-08-31 16:36:22 +1000

    Another rename, CLI refactoring.

--- magda-discussions-api/src/Database.ts
+++ magda-discussions-api/src/Database.ts
@@ -1,104 +1,118 @@
-import pool from "./pool";
+import createPool from "./createPool";
 import { Maybe } from "tsmonad";
 import { Discussion, Message } from "./model";
 import arrayToMaybe from "@magda/typescript-common/dist/util/arrayToMaybe";
+import * as pg from 'pg';
 
-export function getDiscussion(
-  discussionId: string
-): Promise<Maybe<Discussion>> {
-  return pool
-    .query('SELECT * FROM discussions WHERE "id" = $1', [discussionId])
-    .then(res => arrayToMaybe(res.rows));
-}
-
-export function addDiscussion(): Promise<Discussion> {
-  return pool
-    .query("INSERT INTO discussions (id) VALUES (DEFAULT) RETURNING id")
-    .then(res => res.rows[0]);
-}
-
-export function getMessagesForDiscussion(
-  discussionId: string
-): Promise<Message[]> {
-  return pool
-    .query('SELECT * FROM messages WHERE "discussionId" = $1', [discussionId])
-    .then(res => res.rows);
-}
-
-export function addMessageToDiscussion(
-  userId: string,
-  discussionId: string,
-  message: Object
-): Promise<Message> {
-  return pool
-    .query(
-      'INSERT INTO messages(message, "userId", "discussionId") VALUES($1, $2, $3) RETURNING *',
-      [message, userId, discussionId]
-    )
-    .then(res => res.rows[0]);
-}
-
-export function getLinkedDiscussion(
-  linkedType: string,
-  linkedId: string
-): Promise<Maybe<Discussion>> {
-  return pool
-    .query(
-      'SELECT "discussionId" AS "id" FROM linkeddiscussions WHERE "linkedType" = $1 AND "linkedId" = $2',
-      [linkedType, linkedId]
-    )
-    .then(res => arrayToMaybe(res.rows));
-}
-
-export function addLinkedDiscussion(
-  linkedType: string,
-  linkedId: string
-): Promise<Discussion> {
-  return addDiscussion()
-    .then(discussion =>
-      pool.query(
-        'INSERT INTO linkeddiscussions("linkedType", "linkedId", "discussionId") VALUES($1, $2, $3) RETURNING "discussionId" AS id',
-        [linkedType, linkedId, discussion.id]
-      )
-    )
-    .then(result => result.rows[0]);
+export interface DatabaseOptions {
+    dbHost: string,
+    dbPort: number
 }
 
 const messagesForDatasetSql = `
-  SELECT messages.id AS id, message, "userId", messages."discussionId", modified, created FROM
-  messages INNER JOIN linkeddiscussions
-  ON "messages"."discussionId" = "linkeddiscussions"."discussionId"
-  WHERE "linkeddiscussions"."linkedType" = $1 AND "linkeddiscussions"."linkedId" = $2
+    SELECT messages.id AS id, message, "userId", messages."discussionId", modified, created FROM
+    messages INNER JOIN linkeddiscussions
+    ON "messages"."discussionId" = "linkeddiscussions"."discussionId"
+    WHERE "linkeddiscussions"."linkedType" = $1 AND "linkeddiscussions"."linkedId" = $2
 `;
 
-export function getLinkedMessages(
-  linkedType: string,
-  linkedId: string
-): Promise<Message[]> {
-  return pool.query(messagesForDatasetSql, [linkedType, linkedId]).then(res => {
-    return res.rows;
-  });
-}
+export default class Database {
+    private pool: pg.Pool;
 
-export function addMessageToLinkedDiscussion(
-  userId: string,
-  linkedType: string,
-  linkedId: string,
-  message: Object
-): Promise<{ message: Message; discussion: Discussion }> {
-  const addMessage = (discussion: Discussion) =>
-    addMessageToDiscussion(userId, discussion.id, message).then(message => ({
-      message,
-      discussion
-    }));
+    constructor(options: DatabaseOptions) {
+        this.pool = createPool(options);
+    }
 
-  return getLinkedDiscussion(linkedType, linkedId).then(maybe =>
-    maybe.caseOf({
-      just: discussion => addMessage(discussion),
-      nothing: () =>
-        addLinkedDiscussion(linkedType, linkedId).then(discussion =>
-          addMessage(discussion)
-        )
-    })
-  );
+    getDiscussion(
+        discussionId: string
+    ): Promise<Maybe<Discussion>> {
+        return this.pool
+            .query('SELECT * FROM discussions WHERE "id" = $1', [discussionId])
+            .then(res => arrayToMaybe(res.rows));
+    }
+
+    addDiscussion(): Promise<Discussion> {
+        return this.pool
+            .query("INSERT INTO discussions (id) VALUES (DEFAULT) RETURNING id")
+            .then(res => res.rows[0]);
+    }
+
+    getMessagesForDiscussion(
+        discussionId: string
+    ): Promise<Message[]> {
+        return this.pool
+            .query('SELECT * FROM messages WHERE "discussionId" = $1', [discussionId])
+            .then(res => res.rows);
+    }
+
+    addMessageToDiscussion(
+        userId: string,
+        discussionId: string,
+        message: Object
+    ): Promise<Message> {
+        return this.pool
+            .query(
+            'INSERT INTO messages(message, "userId", "discussionId") VALUES($1, $2, $3) RETURNING *',
+            [message, userId, discussionId]
+            )
+            .then(res => res.rows[0]);
+    }
+
+    getLinkedDiscussion(
+        linkedType: string,
+        linkedId: string
+    ): Promise<Maybe<Discussion>> {
+        return this.pool
+            .query(
+            'SELECT "discussionId" AS "id" FROM linkeddiscussions WHERE "linkedType" = $1 AND "linkedId" = $2',
+            [linkedType, linkedId]
+            )
+            .then(res => arrayToMaybe(res.rows));
+    }
+
+    addLinkedDiscussion(
+        linkedType: string,
+        linkedId: string
+    ): Promise<Discussion> {
+        return this.addDiscussion()
+            .then(discussion =>
+                this.pool.query(
+                    'INSERT INTO linkeddiscussions("linkedType", "linkedId", "discussionId") VALUES($1, $2, $3) RETURNING "discussionId" AS id',
+                    [linkedType, linkedId, discussion.id]
+                )
+            )
+            .then(result => result.rows[0]);
+    }
+
+    getLinkedMessages(
+        linkedType: string,
+        linkedId: string
+    ): Promise<Message[]> {
+        return this.pool.query(messagesForDatasetSql, [linkedType, linkedId]).then(res => {
+            return res.rows;
+        });
+    }
+
+    addMessageToLinkedDiscussion(
+        userId: string,
+        linkedType: string,
+        linkedId: string,
+        message: Object
+    ): Promise<{ message: Message; discussion: Discussion }> {
+        const addMessage = (discussion: Discussion) =>
+            this.addMessageToDiscussion(userId, discussion.id, message).then(message => ({
+                message,
+                discussion
+            }));
+
+        return this.getLinkedDiscussion(linkedType, linkedId).then(maybe =>
+            maybe.caseOf({
+                just: discussion => addMessage(discussion),
+                nothing: () =>
+                    this.addLinkedDiscussion(linkedType, linkedId).then(discussion =>
+                        addMessage(discussion)
+                    )
+            })
+        );
+    }
 }
