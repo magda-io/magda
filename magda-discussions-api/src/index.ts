df6d4c40e5a3b080854746727c821d3abe648f32
Kevin Ring <kevin@kotachrome.com>
2017-08-31 16:29:07 +1000

    Renames.

--- magda-discussions-api/src/index.ts
+++ magda-discussions-api/src/index.ts
@@ -1,14 +1,34 @@
 import * as express from 'express';
+import * as yargs from 'yargs';
 
 import apiRouter from './api';
-const nodeConfig = require('config');
+
+const argv = yargs
+    .config()
+    .help()
+    .option('listenPort', {
+        describe: 'The TCP/IP port on which the auth-api should listen.',
+        type: 'number',
+        default: 6104
+    })
+    .option('dbHost', {
+        describe: 'The host running the auth database.',
+        type: 'string',
+        default: 'localhost'
+    })
+    .option('dbPort', {
+        describe: 'The port running the auth database.',
+        type: 'number',
+        default: 5432
+    })
+    .argv;
 
 const app = express();
 app.use(require("body-parser").json());
 
 app.use("/v0", apiRouter);
 
-const listenPort = nodeConfig.get("listenPort")
+const listenPort = argv.listenPort;
 app.listen(listenPort);
 console.log("Listening on " + listenPort)
 
