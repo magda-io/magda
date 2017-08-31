6a3a8ebe609ebf7082ab4620c21c45849584925a
Kevin Ring <kevin@kotachrome.com>
2017-08-31 13:49:26 +1000

    WIP CLI for auth-api.

--- magda-auth-api/src/index.ts
+++ magda-auth-api/src/index.ts
@@ -1,7 +1,27 @@
 import * as express from 'express';
+import * as yargs from 'yargs';
 
 import apiRouter from './api';
-const nodeConfig = require('config');
+
+const argv = yargs
+    .config()
+    .help()
+    .option('listenPort', {
+        describe: 'The TCP/IP port on which the auth-api should listen.',
+        type: 'number',
+        default: 6104
+    })
+    .option('dbHost', {
+        describe: 'The host running the session database.',
+        type: 'string',
+        default: 'localhost'
+    })
+    .option('dbPort', {
+        describe: 'The port running the session database.',
+        type: 'number',
+        default: 5432
+    })
+    .argv;
 
 // Create a new Express application.
 var app = express();
@@ -9,8 +29,8 @@ app.use(require("body-parser").json());
 
 app.use('/v0', apiRouter);
 
-app.listen(nodeConfig.get("listenPort"));
-console.log("Auth API started on port " + nodeConfig.get("listenPort"));
+app.listen(argv.listenPort);
+console.log("Auth API started on port " + argv.listenPort);
 
 process.on("unhandledRejection", (reason: string, promise: any) => {
   console.error(reason);
