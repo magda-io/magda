9b4df919160307459e08f0c696a92d7c4ce7d4fe
Kevin Ring <kevin@kotachrome.com>
2017-09-01 14:58:56 +1000

    prod -> production, remove unnecessary function.

--- magda-scala-common/src/main/scala/au/csiro/data61/magda/AppConfig.scala
+++ magda-scala-common/src/main/scala/au/csiro/data61/magda/AppConfig.scala
@@ -5,8 +5,6 @@ import com.typesafe.config.ConfigParseOptions
 import com.typesafe.config.ConfigResolveOptions
 
 object AppConfig {
-  def getEnv = if (System.getenv("SCALA_ENV") == null) "host" else System.getenv("SCALA_ENV")
-
   /** The global config, potentially including env-custom settings for libraries like akka */
   def conf(envOption: Option[String] = None) = {
     ConfigFactory.load().withFallback(ConfigFactory.parseResources("common.conf"))
