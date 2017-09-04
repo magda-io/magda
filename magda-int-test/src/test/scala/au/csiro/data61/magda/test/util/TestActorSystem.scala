df0d3b56a28f6c3b3059570219e304bf458b2289
Kevin Ring <kevin@kotachrome.com>
2017-09-04 14:18:46 +1000

    Make test settings more closely match the way they used to be.

--- magda-int-test/src/test/scala/au/csiro/data61/magda/test/util/TestActorSystem.scala
+++ magda-int-test/src/test/scala/au/csiro/data61/magda/test/util/TestActorSystem.scala
@@ -12,7 +12,24 @@ object TestActorSystem {
     indexer.refreshInterval = -1
     akka.http.server.request-timeout = 30s
     indexedServices.registry.path = "v0/"
-  """).resolve().withFallback(AppConfig.conf(Some("local")))
+    autoCrawl = false
+    maxResults = 100
+    http.port = 80
+    registry.webhookUrl = "http://indexer/v0/registry-hook"
+    indexedServices.registry.baseUrl = "http://registry-api/"
+    featureFlags.registryOnly = true
+    indexedServices.registry.ignore = false
+    regionSources {
+      SA4.disabled = true
+      SA3.disabled = true
+      SA2.disabled = true
+      SA1.disabled = true
+      LGA.disabled = false
+      POA.disabled = true
+      COM_ELB_ID_2016.disabled = true
+      STE.disabled = false
+    }
+  """).resolve().withFallback(AppConfig.conf())
 
   def actorSystem = ActorSystem("TestActorSystem", config)
 }
