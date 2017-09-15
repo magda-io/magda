64839f415242dfbca71d406045a4c9868c15e178
Stephen Davies <steve9164@gmail.com>
2017-09-15 18:12:13 +1000

    Continue GraphQL API

--- magda-registry-api/src/main/scala/au/csiro/data61/magda/registry/GraphQLDataFetcher.scala
+++ magda-registry-api/src/main/scala/au/csiro/data61/magda/registry/GraphQLDataFetcher.scala
@@ -4,7 +4,11 @@ import scalikejdbc.DB
 import spray.json._
 
 class GraphQLDataFetcher {
-  def getRecordsPage(pageToken: Option[String]) : GraphQLSchema.RecordsPageGraphQL = {
+  def getRecord(id: String) : GraphQLSchema.Record = {
+    GraphQLSchema.Record(id = id, name = "made-up name", Nil, null)
+  }
+  // , paths: Vector[Vector[String]]
+  def getRecordsPage(pageToken: Option[String], aspects: ) : GraphQLSchema.RecordsPageGraphQL = {
     DB readOnly { session => {
       val summariesPage = RecordPersistence.getAll(session, pageToken, null, null)
       GraphQLSchema.RecordsPageGraphQL(records = summariesPage.records.map(rs => GraphQLSchema.Record(id=rs.id, name=rs.name, aspectsList=rs.aspects, aspects = JsObject())), nextPageToken = summariesPage.nextPageToken.get)
