8d5a293a2a825736a9827c3a9e7d796d92460605
Stephen Davies <steve9164@gmail.com>
2017-09-18 19:23:06 +1000

    Sangria GraphQL API with Projector

--- magda-registry-api/src/main/scala/au/csiro/data61/magda/registry/GraphQLDataFetcher.scala
+++ magda-registry-api/src/main/scala/au/csiro/data61/magda/registry/GraphQLDataFetcher.scala
@@ -4,14 +4,20 @@ import scalikejdbc.DB
 import spray.json._
 
 class GraphQLDataFetcher {
-  def getRecord(id: String) : GraphQLSchema.Record = {
+  def getRecord(id: String, aspects: List[String]) : GraphQLSchema.Record = {
+    DB readOnly { session => {
+      val record = RecordPersistence.getByIdWithAspects(session, id, Nil, aspects)
+    }}
     GraphQLSchema.Record(id = id, name = "made-up name", Nil, null)
   }
   // , paths: Vector[Vector[String]]
-  def getRecordsPage(pageToken: Option[String], aspects: ) : GraphQLSchema.RecordsPageGraphQL = {
+  def getRecordsPage(pageToken: Option[String], aspects: List[String]) : GraphQLSchema.RecordsPageGraphQL = {
     DB readOnly { session => {
-      val summariesPage = RecordPersistence.getAll(session, pageToken, null, null)
-      GraphQLSchema.RecordsPageGraphQL(records = summariesPage.records.map(rs => GraphQLSchema.Record(id=rs.id, name=rs.name, aspectsList=rs.aspects, aspects = JsObject())), nextPageToken = summariesPage.nextPageToken.get)
+      val page = RecordPersistence.getAllWithAspects(session, Nil, aspects, pageToken)
+      GraphQLSchema.RecordsPageGraphQL(
+        records = page.records.map(r => GraphQLSchema.Record(id=r.id, name=r.name, aspectsList=Nil, aspects = r.aspects)),
+        nextPageToken = page.nextPageToken.get
+      )
     }}
   }
 }
