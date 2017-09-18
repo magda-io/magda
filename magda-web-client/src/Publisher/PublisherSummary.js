0ec438afc569265602ae8dc21cacd079adb2bd87
chloe <chloeleichen@gmail.com>
2017-09-18 16:36:34 +1000

    publisher no description

--- magda-web-client/src/Publisher/PublisherSummary.js
+++ magda-web-client/src/Publisher/PublisherSummary.js
@@ -10,11 +10,11 @@ function PublisherSummary(props: {publisher: Publisher}) {
   return (
       <div className='publisher-summray white-box media'>
                 <div className='media-left'>
-                    <img className='media-object publisher_image' src={details.imageUrl} alt={props.publisher.name}/>
+                    <img className='media-object publisher_image' src={details.imageUrl ? details.imageUrl : 'http://via.placeholder.com/200x150?text=no+logo+available'} alt={props.publisher.name}/>
                 </div>
                 <div className='media-body'>
                     <Link to={'publishers/' + encodeURIComponent(props.publisher.id)}><h3>{props.publisher.name}</h3></Link>
-                    <div className='publisher-description'>{details.description.slice(0, 200) + '...'}</div>
+                    <div className='publisher-description'>{details.description && details.description.slice(0, 200) + '...'}</div>
                 </div>
              </div>
   );
