096073a0cca819102349f298db8f29bbb89eb72f
Alex Gilleran <alex.gilleran@nicta.com.au>
2017-09-22 15:46:22 +1000

    Hopefully have made it work on dev!

--- deploy/helm/magda/templates/_helpers.tpl
+++ deploy/helm/magda/templates/_helpers.tpl
@@ -18,3 +18,44 @@ We truncate at 63 chars because some Kubernetes name fields are limited to this 
 {{- define "dockerimage" -}}
 "{{ .Values.image.repository | default .Values.global.image.repository }}/magda-{{ .Chart.Name }}:{{ .Values.image.tag | default .Values.global.image.tag }}"
 {{- end -}}
+
+{{- define "magda.postgres-env" }}
+        - name: BACKUP
+          value: {{ .Values.waleBackup.method | quote }}
+        - name: WALE_S3_PREFIX
+          value: {{ .Values.waleBackup.s3Prefix }}
+        - name: AWS_ACCESS_KEY_ID
+          value: {{ .Values.waleBackup.awsAccessKeyId }}
+        - name: AWS_SECRET_ACCESS_KEY
+          value: {{ .Values.waleBackup.secretAccessKey }}
+        - name: AWS_REGION
+          value: {{ .Values.waleBackup.awsRegion }}
+        - name: WALE_WABS_PREFIX
+          value: {{ .Values.waleBackup.wabsPrefix }}
+        - name: WABS_ACCOUNT_NAME
+          value: {{ .Values.waleBackup.wabsAccountName }}
+        - name: WABS_ACCESS_KEY
+          value: {{ .Values.waleBackup.wabsAccessKey }}
+        - name: WABS_SAS_TOKEN
+          value: {{ .Values.waleBackup.wabsSasToken }}
+        - name: WALE_GS_PREFIX
+          value: {{ .Values.waleBackup.gsPrefix }}
+        {{- if .Values.waleBackup.googleApplicationCredentials }}
+        - name: GOOGLE_APPLICATION_CREDENTIALS
+          value: {{ .Values.waleBackup.googleApplicationCredentials }}
+        {{- end }}
+        - name: WALE_SWIFT_PREFIX
+          value: {{ .Values.waleBackup.swiftPrefix }}
+        - name: SWIFT_AUTHURL
+          value: {{ .Values.waleBackup.swiftAuthUrl }}
+        - name: SWIFT_TENANT
+          value: {{ .Values.waleBackup.swiftTenant }}
+        - name: SWIFT_USER
+          value: {{ .Values.waleBackup.swiftUser }}
+        - name: SWIFT_PASSWORD
+          value: {{ .Values.waleBackup.swiftPassword }}
+        - name: SWIFT_AUTH_VERSION
+          value: {{ .Values.waleBackup.swiftAuthVersion }}
+        - name: SWIFT_ENDPOINT_TYPE
+          value: {{ .Values.waleBackup.swiftEndpointType }}
+{{- end }}
--- magda-postgres/migrate-and-start.sh
+++ magda-postgres/migrate-and-start.sh
@@ -1,11 +1,58 @@
-#!/bin/sh
+#!/bin/bash
+BACKUP_COMMAND_LINE_ARGS=()
+MEMORY_COMMAND_LINE_ARGS=()
+
+#https://wiki.postgresql.org/wiki/Tuning_Your_PostgreSQL_Server
+if [[ ! -z $MEMORY_LIMIT ]]; then
+    SHARED_BUFFERS=$(expr $MEMORY_LIMIT / 4)
+    EFFECTIVE_CACHE_SIZE=$(expr $MEMORY_LIMIT / 2)
+    MEMORY_COMMAND_LINE_ARGS=(
+        "-c" "shared_buffers=${SHARED_BUFFERS}KB" \
+        "-c" "effective_cache_size=${EFFECTIVE_CACHE_SIZE}KB" )
+fi
+
+if [[ ! -z "${BACKUP}" ]]; then
+    #https://github.com/wal-e/wal-e/issues/200
+    FIRST_RUN=true
+    periodically_backup () {
+        while true; do
+            sleep 45
+
+            local CURRENT_TIME=$(date +"%H:%M")
+            if [ "$CURRENT_TIME" == "$BACKUP_EXECUTION_TIME" ] || [ $FIRST_RUN == true ]; then
+                /usr/local/bin/wal-e backup-push "$PGDATA"
+                /usr/local/bin/wal-e delete --confirm retain 30
+                FIRST_RUN=false
+            fi
+        done
+    }
+
+    periodically_backup &
+fi
+
+if [[ "${BACKUP}" == "WAL" ]]; then
+    #TODO: This needs to have no apostrophes for the postgres thing and still have apostrophes for the pgctl thing
+    BACKUP_COMMAND_LINE_ARGS=(\
+        "-c" "wal_level=replica" \
+        "-c" "archive_mode=on" \
+        "-c" "archive_command='/usr/local/bin/wal-e wal-push $PGDATA/%p'" \
+        "-c" "archive_timeout=60")
+
+    echo "restore_command = '/usr/local/bin/wal-e wal-fetch %f %p'" > ${PGDATA}/recovery.conf
+fi
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+JOINED_MEMORY_COMMAND_LINE_ARGS=$(join_by " " "${MEMORY_COMMAND_LINE_ARGS[@]}")
+JOINED_BACKUP_COMMAND_LINE_ARGS=$(join_by " " "${BACKUP_COMMAND_LINE_ARGS[@]}")
+
 cd /flyway
 tar xzf flyway-commandline-4.2.0-linux-x64.tar.gz
 cd flyway-4.2.0
-PGUSER="${PGUSER:-postgres}" pg_ctl -D "$PGDATA" -o "-c listen_addresses='localhost'" -w start
+PGARGS="-c listen_addresses='localhost' -c max_prepared_transactions=0 $JOINED_MEMORY_COMMAND_LINE_ARGS $JOINED_BACKUP_COMMAND_LINE_ARGS"
+PGUSER="${PGUSER:-postgres}" pg_ctl -D "$PGDATA" -o "$PGARGS" -w start
 
 for d in /flyway/sql/*; do
-    if [ -d "$d" ]; then
+    if [[ -d "$d" ]]; then
         echo "Creating database $(basename "$d") (this will fail if it already exists; that's ok)"
         psql -U "${PGUSER:-postgres}" -c "CREATE DATABASE $(basename "$d") WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1;"
         echo "Migrating database $(basename "$d")"
@@ -15,47 +62,8 @@ done
 
 pg_ctl -D "$PGDATA" -m fast -w stop
 
-local BACKUP_COMMAND_LINE_ARGS=""
-local MEMORY_COMMAND_LINE_ARGS=""
-
-#https://wiki.postgresql.org/wiki/Tuning_Your_PostgreSQL_Server
-if [ -z "${MEMORY_LIMIT}" ]; then
-    local SHARED_BUFFERS=$(expr $MEMORY_LIMIT / 4)
-    local EFFECTIVE_CACHE_SIZE=$(expr $MEMORY_LIMIT / 2)
-    MEMORY_COMMAND_LINE_ARGS="\
-        -c shared_buffers=${SHARED_BUFFERS}KB \
-        -c effective_cache_size=${EFFECTIVE_CACHE_SIZE}KB"
-fi
-
-if [ -z "${BACKUP}" ]; then
-    #https://github.com/wal-e/wal-e/issues/200
-    local FIRST_RUN=true
-    periodically_backup () {
-        while true; do
-            sleep 45
-
-            local CURRENT_TIME=$(date +"%H:%M")
-            if [[ "$CURRENT_TIME" == "$BACKUP_EXECUTION_TIME" ]] || [$FIRST_RUN = true]; then
-                wal-e backup-push "$DATA_DIRECTORY"
-                wal-e delete --confirm retain 30
-                FIRST_RUN=false
-            fi
-        done
-    }
-
-    periodically_backup &
-
-fi
-
-if [[ "${BACKUP}" == "WAL" ]]; then
-    BACKUP_COMMAND_LINE_ARGS="-c wal_level = replica \
-        -c archive_mode = on \
-        -c archive_command = 'wal-e wal-push %p' \
-        -c archive_timeout = 60 \
-        -c restore_command 'wal-e wal-fetch %f %p'"
-fi
-
-
-exec "$@ \
-    $MEMORY_COMMAND_LINE_ARGS \
-    $BACKUP_COMMAND_LINE_ARGS"
+# Why this elaborate way of running the command? It lets us use the same string of PGARGS for running pg_ctl (above) as it does for postgres (below).
+# Without it we get all these issues with exec expecting an array of strings vs pg_ctl -o expecting one big string.
+COMMAND="$* $PGARGS"
+echo "Running command $COMMAND"
+exec echo "$COMMAND" | bash
