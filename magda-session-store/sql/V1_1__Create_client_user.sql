32ccb35de75179a617c1b9913b72335315a88f08
Alex Gilleran <alex.gilleran@nicta.com.au>
2017-10-16 10:38:30 +1100

    Made client get read/write priveliges on all dbs in combined-db.

--- magda-session-store/sql/V1_1__Create_client_user.sql
+++ magda-session-store/sql/V1_1__Create_client_user.sql
@@ -6,12 +6,12 @@ BEGIN
         FROM   pg_catalog.pg_user
         WHERE  usename = '${clientUserName}') THEN
             CREATE USER ${clientUserName} WITH PASSWORD '${clientPassword}';
-
-            GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO ${clientUserName};
-            GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO ${clientUserName};
-
-            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO ${clientUserName};
-            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO ${clientUserName};
    END IF;
 END
 $body$;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO ${clientUserName};
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO ${clientUserName};
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO ${clientUserName};
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO ${clientUserName};
