36d63cecdbc230126240515bc42fc38be784971c
chloe <chloeleichen@gmail.com>
2017-10-16 14:25:22 +1100

    a11y

--- magda-web-client/src/Search/Search.js
+++ magda-web-client/src/Search/Search.js
@@ -140,12 +140,13 @@ class Search extends Component {
                  />
                 }
             </div>
-            {searchText.length > 0 &&
-             !this.props.isFetching &&
-             !this.props.error && <div className='results-count col-sm-4'>{this.props.hitCount} results found</div>}
+
           </div>
           <div className='row'>
             <div className='col-sm-8'>
+                {searchText.length > 0 &&
+                 !this.props.isFetching &&
+                 !this.props.error && <div className='results-count'>{this.props.hitCount} results found</div>}
                 {searchText.length === 0 && <div>{this.renderSuggestions()}</div>}
                 {searchText.length > 0 &&
                  !this.props.isFetching &&
