b9e9bc857d929aec4410e5a6a6ecaaee058b62d3
Alex Gilleran <alex.gilleran@nicta.com.au>
2017-10-17 16:02:36 +1100

    Removed some commented out code from ES dockerfile.

--- magda-elastic-search/setup.sh
+++ magda-elastic-search/setup.sh
@@ -56,11 +56,3 @@ fi
 
 echo "Starting up"
 /run.sh
-	
-# echo http://nl.alpinelinux.org/alpine/edge/community >> /etc/apk/repositories && \
-#	apk --no-cache add shadow && \
-#	adduser -D -g '' elasticsearch && \
-
-#	chmod 700 /snapshots && \
-#	mkdir /snapshots/~ && \
-#	usermod -u 1000 elasticsearch && \
