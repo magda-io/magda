6af908fe2bd02ea8ebbb4540605a034a2fb3d3c3
Kevin Ring <kevin@kotachrome.com>
2017-10-18 21:56:55 +1100

    Use the right admin API for admin UI.

--- magda-web-admin/src/config.js
+++ magda-web-admin/src/config.js
@@ -1,13 +1,11 @@
 const fallbackApiHost = "http://magda-dev.terria.io/";
 
 const serverConfig = window.magda_server_config || {};
-const adminApiUrl = serverConfig.baseUrl
-const adminApiUrlFallback = "http://192.168.99.100:30100/api/v0/admin/"
 
 export const config = {
   appName: "data.gov.au",
   baseUrl: serverConfig.baseUrl || fallbackApiHost,
-  adminApiUrl: serverConfig.adminApiUrl || adminApiUrlFallback,
+  adminApiUrl: serverConfig.adminApiBaseUrl || fallbackApiHost + "api/v0/admin/",
   authApiUrl: serverConfig.authApiBaseUrl || fallbackApiHost + "api/v0/auth/",
   appTitle: 'magda web-admin',
 };
