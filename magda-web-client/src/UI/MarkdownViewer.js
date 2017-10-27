9e65613782bfdc12baa530c558af30c8315b9e57
chloe <chloeleichen@gmail.com>
2017-10-27 15:07:05 +1100

    update markdown treatment

--- magda-web-client/src/UI/MarkdownViewer.js
+++ magda-web-client/src/UI/MarkdownViewer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import MarkdownIt from 'markdown-it';
 import './MarkdownViewer.css';
-
+import defined from '../helpers/defined';
+var DOMPurify = require('dompurify/dist/purify');
 
 class MarkdownViewer extends React.Component  {
   constructor(props) {
@@ -10,6 +11,16 @@ class MarkdownViewer extends React.Component  {
  }
 
  componentDidMount(){
+   this.notifyParentOfContentLength();
+ }
+
+ componentWillReceiveProps(nextProps){
+   if(nextProps.markdown !== this.props.markdown){
+     this.notifyParentOfContentLength();
+   }
+ }
+
+ notifyParentOfContentLength(){
    const numberOfChildrenFromMarkdownAsHtml = this.markdown.children.length;
    if (this.props.updateContentLength){
      this.props.updateContentLength(numberOfChildrenFromMarkdownAsHtml);
@@ -17,11 +28,9 @@ class MarkdownViewer extends React.Component  {
  }
 
  render(){
-   const md = new MarkdownIt();
-   const htmlcontent = md.render(this.props.markdown);
-   let markdown = {__html: htmlcontent};
+   let markdown = {__html: markdownToHtml(this.props.markdown)};
    return(
-      <div className='markdown' dangerouslySetInnerHTML={markdown} ref={markdown => this.markdown = markdown}/>
+      <div className='markdown' dangerouslySetInnerHTML={markdown} ref={markdown => this.markdown = markdown}></div>
    )
  }
 }
@@ -29,3 +38,31 @@ class MarkdownViewer extends React.Component  {
 MarkdownViewer.defaultProps = {markdown: ''};
 
 export default MarkdownViewer;
+
+const md = new MarkdownIt({
+    html: true,
+    linkify: true
+});
+
+const htmlRegex = /^\s*<[^>]+>/;
+
+
+function markdownToHtml(markdownString, allowUnsafeHtml, options) {
+    if (!defined(markdownString) || markdownString.length === 0) {
+        return markdownString;
+    }
+    // If the text looks like html, don't try to interpret it as Markdown because
+    // we'll probably break it in the process.
+    var unsafeHtml;
+    if (htmlRegex.test(markdownString)) {
+        unsafeHtml = markdownString;
+    } else {
+        // Note this would wrap non-standard tags such as <collapsible>hi</collapsible> in a <p></p>, which is bad.
+        unsafeHtml = md.render(markdownString);
+    }
+    if (allowUnsafeHtml) {
+        return unsafeHtml;
+    } else {
+        return DOMPurify.sanitize(unsafeHtml, options);
+    }
+}
