385c93cf23c810c3471fcac11ebdb4d3459cbad4
Kevin Ring <kevin@kotachrome.com>
2017-11-23 11:31:51 +1100

    Fix typo, use proper URL.

--- magda-web-client/src/index.js
+++ magda-web-client/src/index.js
@@ -110,7 +110,7 @@ ReactDOM.render(
           <Route path={`page/:id`} key={item.path} component={item.component} />
         )}
         <Redirect from="/about" to="page/about" />
-        <Route path="preivew/dataset/:datasetpreviewjson" component={DatasetSummary}/>
+        <Route path="preview/dataset/:datasetpreviewjson" component={DatasetSummary}/>
 
       </Route>
     </Router>
