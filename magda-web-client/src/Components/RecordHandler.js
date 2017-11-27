ddb07b44511483da4a7cbc86d966d053cf286235
chloe <chloeleichen@gmail.com>
2017-11-27 11:24:56 +1100

    progress bar when loading dataset, and handle 404

--- magda-web-client/src/Components/RecordHandler.js
+++ magda-web-client/src/Components/RecordHandler.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import { connect } from 'react-redux';
+import ProgressBar from '../UI/ProgressBar';
 import ReactDocumentTitle from 'react-document-title';
 import { bindActionCreators } from 'redux';
 import { fetchDatasetFromRegistry, fetchDistributionFromRegistry } from '../actions/recordActions';
@@ -8,6 +9,7 @@ import Tabs from '../UI/Tabs';
 import {config} from '../config';
 import ErrorHandler from './ErrorHandler';
 import CustomIcons from '../UI/CustomIcons';
+import RouteNotFound from './RouteNotFound';
 import type {StateRecord } from '../types';
 import type { ParsedDataset, ParsedDistribution } from '../helpers/record';
 import {
@@ -65,89 +67,98 @@ class RecordHandler extends React.Component {
     const publisherLogo = (this.props.dataset.publisher && this.props.dataset.publisher['aspects']['organization-details']) ? this.props.dataset.publisher['aspects']['organization-details']['imageUrl'] : '';
     const publisherId = this.props.dataset.publisher ? this.props.dataset.publisher.id : null;
     const distributionIdAsUrl = this.props.match.params.distributionId ? encodeURIComponent(this.props.match.params.distributionId) : '';
-     if (this.props.match.params.distributionId && !this.props.distributionIsFetching){
-       if(this.props.distributionFetchError){
-         return <ErrorHandler error={this.props.distributionFetchError}/>;
-       }
-       const tabList = [
-         {id: 'details', name: 'Details', isActive: true},
-         {id: 'preview', name: 'Preview', isActive: true}
-       ];
-       const baseUrlDistribution = `/dataset/${encodeURIComponent(this.props.match.params.datasetId)}/distribution/${distributionIdAsUrl}`
-      return (
-        <div>
-          <div className='container'>
-            {this.renderBreadCrumbs(this.props.dataset, this.props.distribution)}
-              <div className='media'>
-                <div className='media-left'>
-                  <CustomIcons imageUrl={publisherLogo} name={publisherName}/>
-                </div>
-                <div className='media-body'>
-                  <h1>{this.props.distribution.title}</h1>
-                  <div className='publisher'>{publisherName}</div>
-                  <div className='updated-date'>Updated {this.props.distribution.updatedDate}</div>
-                </div>
-              </div>
-            </div>
+    if(this.props.match.params.distributionId){
+      if(this.props.distributionIsFetching){
+        return <ProgressBar/>
+      } else{
+        if(this.props.distributionFetchError){
+          return <ErrorHandler error={this.props.distributionFetchError}/>;
+        }
+        const tabList = [
+          {id: 'details', name: 'Details', isActive: true},
+          {id: 'preview', name: 'Preview', isActive: true}
+        ];
+        const baseUrlDistribution = `/dataset/${encodeURIComponent(this.props.match.params.datasetId)}/distribution/${distributionIdAsUrl}`
+       return (
+         <div>
+           <div className='container'>
+             {this.renderBreadCrumbs(this.props.dataset, this.props.distribution)}
+               <div className='media'>
+                 <div className='media-left'>
+                   <CustomIcons imageUrl={publisherLogo} name={publisherName}/>
+                 </div>
+                 <div className='media-body'>
+                   <h1>{this.props.distribution.title}</h1>
+                   <div className='publisher'>{publisherName}</div>
+                   <div className='updated-date'>Updated {this.props.distribution.updatedDate}</div>
+                 </div>
+               </div>
+             </div>
 
-            <Tabs list={tabList} baseUrl={baseUrlDistribution}/>
-            <div className='tab-content'>
-              <Switch>
-                <Route path='/dataset/:datasetId/distribution/:distributionId/details' component={DistributionDetails} />
-                <Route path='/dataset/:datasetId/distribution/:distributionId/preview' component={DistributionPreview} />
-                <Redirect from='/dataset/:datasetId/distribution/:distributionId' to={`${baseUrlDistribution}/details`} />
-              </Switch>
-            </div>
-            </div>
-      )
-    } else if(this.props.match.params.datasetId && !this.props.datasetIsFetching){
-      if(this.props.datasetFetchError){
-        return <ErrorHandler error={this.props.datasetFetchError}/>;
+             <Tabs list={tabList} baseUrl={baseUrlDistribution}/>
+             <div className='tab-content'>
+               <Switch>
+                 <Route path='/dataset/:datasetId/distribution/:distributionId/details' component={DistributionDetails} />
+                 <Route path='/dataset/:datasetId/distribution/:distributionId/preview' component={DistributionPreview} />
+                 <Redirect from='/dataset/:datasetId/distribution/:distributionId' to={`${baseUrlDistribution}/details`} />
+               </Switch>
+             </div>
+             </div>
+       )
       }
-      const datasetTabs = [
-        {id: 'details', name: 'Details', isActive: true},
-        {id:  'discussion', name: 'Discussion', isActive: !config.disableAuthenticationFeatures},
-        {id: 'publisher', name: 'About ' + publisherName, isActive: publisherId},
-      ];
-
-      const baseUrlDataset = `/dataset/${encodeURIComponent(this.props.match.params.datasetId)}`;
-
-      return (
-        <div>
-            <div className='container media'>
-              {this.renderBreadCrumbs(this.props.dataset)}
-              <div className='media-left'>
-                <CustomIcons imageUrl={publisherLogo} name={publisherName}/>
-              </div>
-               <div className='media-body'>
-                  <h1>{this.props.dataset.title}</h1>
-                  <div className='publisher'>{publisherName}</div>
-                  <div className='updated-date'>Updated {this.props.dataset.updatedDate}</div>
-              </div>
-            </div>
-
-            <Tabs list={datasetTabs} baseUrl={baseUrlDataset}/>
-            <div className='tab-content'>
-              <Switch>
-                <Route path='/dataset/:datasetId/details' component={DatasetDetails} />
-                <Route path='/dataset/:datasetId/discussion' component={DatasetDiscussion} />
-                <Route path='/dataset/:datasetId/publisher' component={DatasetPublisher} />
-                <Redirect exact from='/dataset/:datasetId' to={`${baseUrlDataset}/details`} />
-              </Switch>
-            </div>
-        </div>
-      );
     }
 
+     else if(this.props.match.params.datasetId){
+       if(this.props.datasetIsFetching){
+         return <ProgressBar/>
+       } else{
+         if(this.props.datasetFetchError){
+           return <ErrorHandler error={this.props.datasetFetchError}/>;
+         }
+         const datasetTabs = [
+           {id: 'details', name: 'Details', isActive: true},
+           {id:  'discussion', name: 'Discussion', isActive: !config.disableAuthenticationFeatures},
+           {id: 'publisher', name: 'About ' + publisherName, isActive: publisherId},
+         ];
+
+         const baseUrlDataset = `/dataset/${encodeURIComponent(this.props.match.params.datasetId)}`;
+
+         return (
+           <div>
+               <div className='container media'>
+                 {this.renderBreadCrumbs(this.props.dataset)}
+                 <div className='media-left'>
+                   <CustomIcons imageUrl={publisherLogo} name={publisherName}/>
+                 </div>
+                  <div className='media-body'>
+                     <h1>{this.props.dataset.title}</h1>
+                     <div className='publisher'>{publisherName}</div>
+                     <div className='updated-date'>Updated {this.props.dataset.updatedDate}</div>
+                 </div>
+               </div>
+
+               <Tabs list={datasetTabs} baseUrl={baseUrlDataset}/>
+               <div className='tab-content'>
+                 <Switch>
+                   <Route path='/dataset/:datasetId/details' component={DatasetDetails} />
+                   <Route path='/dataset/:datasetId/discussion' component={DatasetDiscussion} />
+                   <Route path='/dataset/:datasetId/publisher' component={DatasetPublisher} />
+                   <Redirect exact from='/dataset/:datasetId' to={`${baseUrlDataset}/details`} />
+                 </Switch>
+               </div>
+           </div>
+         );
+       }
+    }
+    return <RouteNotFound/>
   }
 
   render() {
     const title = this.props.match.params.distributionId ? this.props.distribution.title : this.props.dataset.title;
-    console.log(this.props.match);
     return (
       <ReactDocumentTitle title={title + '|' + config.appName}>
         <div>
-            {!this.props.isFetching && this.renderByState()}
+            {this.renderByState()}
         </div>
       </ReactDocumentTitle>
     );
