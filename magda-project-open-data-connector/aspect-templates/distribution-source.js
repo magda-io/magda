5732a66e8d4dff0b5aace82c650a78fba930450b
Kevin Ring <kevin@kotachrome.com>
2017-12-07 17:13:13 +1100

    Namespaced IDs for data.json.

--- magda-project-open-data-connector/aspect-templates/distribution-source.js
+++ magda-project-open-data-connector/aspect-templates/distribution-source.js
@@ -3,5 +3,6 @@ var projectOpenData = libraries.projectOpenData;
 return {
     type: 'project-open-data-distribution',
     url: projectOpenData.url,
+    id: projectOpenData.id,
     name: projectOpenData.name
 };
--- magda-project-open-data-connector/aspect-templates/organization-source.js
+++ magda-project-open-data-connector/aspect-templates/organization-source.js
@@ -3,5 +3,6 @@ var projectOpenData = libraries.projectOpenData;
 return {
     type: 'project-open-data-organization',
     url: projectOpenData.url,
+    id: projectOpenData.id,
     name: projectOpenData.name
 };
