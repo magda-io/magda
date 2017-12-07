b764cb91fc2ad84632d6611b5bc31040d75c89b0
chloe <chloeleichen@gmail.com>
2017-12-07 14:43:44 +1100

    add magda ui

--- magda-web-client/src/stories/index.js
+++ magda-web-client/src/stories/index.js
@@ -4,6 +4,8 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+import OverviewBox from '../UI/OverviewBox';
+
 import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
@@ -11,3 +13,6 @@ storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo(
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('dataset', module)
+    .add('overview box', () => <OverviewBox content={"## heading \n some content"}/>)
