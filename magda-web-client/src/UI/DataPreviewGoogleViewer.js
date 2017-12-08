6e7287ff796a2bae379da0a243b4befbd8e6d7dc
chloe <chloeleichen@gmail.com>
2017-12-08 11:21:06 +1100

    add preview

--- magda-web-client/src/UI/DataPreviewGoogleViewer.js
+++ magda-web-client/src/UI/DataPreviewGoogleViewer.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function DataPreviewGoogleViewer(props) {
-  console.log(props.data.data)
   return (
       <div className='data-preview-json'>
       <iframe title='google doc viewer' allowFullScreen="" height="600px" scrolling="auto" src={`https://docs.google.com/viewer?embedded=true&toolbar=hide&url=${props.data.data}`} width="100%"></iframe>
--- magda-web-client/src/stories/index.js
+++ magda-web-client/src/stories/index.js
@@ -4,15 +4,18 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+import CustomIcons, {iconTypes} from '../UI/CustomIcons';
+import DataPreviewGoogleViewer from '../UI/DataPreviewGoogleViewer';
+
 import OverviewBox from '../UI/OverviewBox';
 
-import { Button, Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
-
-storiesOf('dataset', module)
+storiesOf('Dataset', module)
     .add('overview box', () => <OverviewBox content={"## heading \n some content"}/>)
+    .add('DataPreviewGoogleViewer', () => <DataPreviewGoogleViewer data={{data: "http://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf"}}/>)
+
+
+
+iconTypes.map(iconname =>
+  storiesOf('Icons', module)
+      .add(iconname, () => <CustomIcons name={iconname}/>)
+)
