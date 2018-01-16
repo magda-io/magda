c595b936481a17e4410041c7372681042aab0b22
chloe <chloeleichen@gmail.com>
2018-01-16 11:46:59 +1100

    location filter

--- magda-web-client/src/Components/SearchFacets/RegionMap.js
+++ magda-web-client/src/Components/SearchFacets/RegionMap.js
@@ -74,15 +74,15 @@ class RegionMap extends Component {
     generateStyle(region) {
         return (feature) => {
           return{
-            color: (region === this.getID(feature)) ? 'rgba(0, 181, 255, 0.6)' : 'rgba(0,0,0,0)',
+            color: (region === this.getID(feature)) ? 'rgba(245, 88, 96, 0.6)' : 'rgba(0,0,0,0)',
             outline: {
                 color: '#ddd',
                 size: 1
             },
             selected: {
-                color: (region === this.getID(feature)) ? 'rgba(0, 181, 255, 0.6)' : 'rgba(0,0,0,0)',
+                color: (region === this.getID(feature)) ? 'rgba(245, 88, 96, 0.6)' : 'rgba(0,0,0,0)',
                 outline: {
-                    color: '#00B5FF'
+                    color: '#ffffff'
                 }
             }
         }};
