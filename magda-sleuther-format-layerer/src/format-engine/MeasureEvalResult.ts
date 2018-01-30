37bd6e8b0bb513f4d4916c92b41cf0f433a5d31e
Tristan Berry <nextan.TB@gmail.com>
2018-01-31 07:47:56 +1100

    removed all of the format sleuther's files from this branch

--- magda-sleuther-format-layerer/src/format-engine/MeasureEvalResult.ts
+++
@@ -1,8 +0,0 @@
-import { SelectedFormat } from "../format-engine/formats";
-import { Record } from "../../../magda-typescript-common/dist/generated/registry/api";
-
-export default interface MeasureEvalResult {
-    format: SelectedFormat;
-    absConfidenceLevel: number;
-    distribution: Record
-}
--- magda-sleuther-format-layerer/src/format-engine/measures/MeasureResult.ts
+++
@@ -1,9 +0,0 @@
-import { SelectedFormat } from "../formats";
-import ProbeResult from "./ProbeResult";
-import { Record } from "@magda/typescript-common/dist/generated/registry/api";
-
-export default interface MeasureResult {
-    formats: SelectedFormat[];
-    state?: ProbeResult;
-    distribution: Record;
-}
