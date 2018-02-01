e039ace4e5e7c53632d2e3ad48b79e59344ea063
Tristan Berry <nextan.TB@gmail.com>
2018-02-02 09:33:59 +1100

    added error handling to onRecordFound

--- magda-sleuther-format/src/onRecordFound.ts
+++ magda-sleuther-format/src/onRecordFound.ts
@@ -3,7 +3,7 @@
 import Registry from "../../magda-typescript-common/dist/registry/AuthorizedRegistryClient";
 import { Record } from "../../magda-typescript-common/dist/generated/registry/api";
 import { FormatAspect } from "./formatAspectDef";
-import unionToThrowable from "../../magda-typescript-common/dist/util/unionToThrowable"
+import unionToThrowable from "../../magda-typescript-common/dist/util/unionToThrowable";
 
 import getDcatMeasureResult from "./format-engine/measures/dcatFormatMeasure";
 import getExtensionMeasureResult from "./format-engine/measures/downloadExtensionMeasure";
@@ -13,7 +13,7 @@ import getDcatProcessedData from "./format-engine/measures/processed-functions/d
 import getDownloadProcessedData from "./format-engine/measures/processed-functions/downloadProcessedFns";
 import getExtensionProcessedData from "./format-engine/measures/processed-functions/extensionProcessedFns";
 
-import  getBestMeasureResult  from "./format-engine/measureEvaluatorByHierarchy"
+import getBestMeasureResult from "./format-engine/measureEvaluatorByHierarchy";
 import MeasureEvaluationSet from "./format-engine/measures/MeasureEvaluationSet";
 import MeasureEvalResult from "./format-engine/MeasureEvalResult";
 let synonymObject = require("./format-engine/synonyms.json");
@@ -34,37 +34,57 @@ export default async function onRecordFound(
     console.log("gott after distribution nul check");
 
     // 2D array: 1 row per distribution
-    const retrievedEvalSets: MeasureEvaluationSet[][] = distributions.map(function (distribution) {
-        const dcatSet: MeasureEvaluationSet = {
-            measureResult: getDcatMeasureResult(distribution, synonymObject),
-            getProcessedData: getDcatProcessedData
-        }
-        const extensionSet: MeasureEvaluationSet = {
-            measureResult: getExtensionMeasureResult(distribution, synonymObject),
-            getProcessedData: getExtensionProcessedData
-        }
-        const downloadSet: MeasureEvaluationSet = {
-            measureResult: getDownloadMeasureResult(distribution, synonymObject),
-            getProcessedData: getDownloadProcessedData
-        }
+    const retrievedEvalSets: MeasureEvaluationSet[][] = distributions.map(
+        function(distribution) {
+            const dcatSet: MeasureEvaluationSet = {
+                measureResult: getDcatMeasureResult(
+                    distribution,
+                    synonymObject
+                ),
+                getProcessedData: getDcatProcessedData
+            };
+            const extensionSet: MeasureEvaluationSet = {
+                measureResult: getExtensionMeasureResult(
+                    distribution,
+                    synonymObject
+                ),
+                getProcessedData: getExtensionProcessedData
+            };
+            const downloadSet: MeasureEvaluationSet = {
+                measureResult: getDownloadMeasureResult(
+                    distribution,
+                    synonymObject
+                ),
+                getProcessedData: getDownloadProcessedData
+            };
 
-        return [dcatSet, extensionSet, downloadSet];
+            return [dcatSet, extensionSet, downloadSet];
+        }
+    );
 
-    });
-
-    const bestFormatResults: MeasureEvalResult[] = retrievedEvalSets.map(evalSetsPerDist =>
-        getBestMeasureResult(evalSetsPerDist)
+    const bestFormatResults: MeasureEvalResult[] = retrievedEvalSets.map(
+        evalSetsPerDist => getBestMeasureResult(evalSetsPerDist)
     );
 
     bestFormatResults.forEach(function(formatResult) {
-        recordFormatAspect(
-            registry,
-            formatResult.distribution,
-            {
+        if (!formatResult) {
+            console.log("encountered a null format result");
+        } else {
+            if (!formatResult.distribution) {
+                throw new Error("the distribution is null");
+            }
+            if (!(formatResult.distribution instanceof Record)) {
+                throw new Error(
+                    "something happened with the measurements and evaluator and now the distribution isn't of type record:" +
+                        formatResult.distribution
+                );
+            }
+
+            recordFormatAspect(registry, formatResult.distribution, {
                 format: formatResult.format.format,
                 confidenceLevel: formatResult.absConfidenceLevel
-            }
-        )
+            });
+        }
     });
 
     return Promise.resolve();
