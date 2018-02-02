5146d811dcff05e77c7692a52a110507c1cd51c3
Jacky Jiang <jacky.jiang@data61.csiro.au>
2018-02-02 13:20:26 +1100

    make `options` `any` type

--- magda-typescript-common/src/test/fakeArgv.ts
+++ magda-typescript-common/src/test/fakeArgv.ts
@@ -1,14 +1,6 @@
 import { Arguments } from "yargs";
 
-export default function fakeArgv(options: {
-    internalUrl?: string;
-    registryUrl?: string;
-    jwtSecret?: string;
-    userId?: string;
-    listenPort?: number;
-    dbHost?: string;
-    dbPort?: number;
-}): Arguments {
+export default function fakeArgv(options: any): Arguments {
     return {
         ...options,
         $0: "",
