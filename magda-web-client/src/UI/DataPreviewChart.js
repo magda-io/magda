a9f7ef096325b78b0224be4a7e7d07aa389dc07d
chloe <chloeleichen@gmail.com>
2018-02-15 17:19:42 +1100

    vis scuffolding

--- magda-web-client/src/UI/DataPreviewChart.js
+++ magda-web-client/src/UI/DataPreviewChart.js
@@ -2,6 +2,7 @@ import 'es6-symbol/implement';
 import React, { Component } from 'react';
 import Tabs from 'muicss/lib/react/tabs';
 import Tab from 'muicss/lib/react/tab';
+import ChartConfig from './ChartConfig';
 
 
 const VEGAMARK = ['area', 'bar', 'circle', 'line', 'point', 'rect', 'square', 'text', 'tick'];
@@ -10,7 +11,9 @@ const DATATYPE = ['quantitative', 'temporal', 'ordinal', 'nominal'];
 class DataPreviewChart extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      previewState: 'ready'
+    }
   }
 
   onChange(i, value, tab, ev) {
@@ -22,7 +25,10 @@ class DataPreviewChart extends Component {
   }
 
   renderChart(){
-    
+    return (<div className='mui-row'>
+              <div className='mui-col-sm-6'>Chart</div>
+              <div className='mui-col-sm-6'><ChartConfig/></div>
+            </div>)
   }
 
   renderTable(){
@@ -30,12 +36,18 @@ class DataPreviewChart extends Component {
   }
 
   render(){
-    return (
-      <Tabs onChange={this.onChange} defaultSelectedIndex={1}>
-        <Tab value="pane-1" label="Chart" onActive={this.onActive}>{this.renderChart()}</Tab>
-        <Tab value="pane-2" label="Table">{this.renderTable()}</Tab>
-      </Tabs>
-    )
+    if(this.state.previewState === 'ready'){
+      return (
+        <Tabs onChange={this.onChange} defaultSelectedIndex={0}>
+          <Tab value="pane-1" label="Chart" onActive={this.onActive}>{this.renderChart()}</Tab>
+          <Tab value="pane-2" label="Table">{this.renderTable()}</Tab>
+        </Tabs>
+      )
+    } else if(this.state.previewState === 'loading'){
+      return <div>Preview is loading</div>
+    } else {
+      return <div>No preview available</div>
+    }
   }
 }
 
