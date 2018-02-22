801ea70742e6ccb6ff10e47ac56e4eb9ac88993f
Alex Gilleran <alex.gilleran@nicta.com.au>
2018-02-23 10:29:45 +1100

    Prettified everything.

--- scripts/node-js-ci.js
+++ scripts/node-js-ci.js
@@ -24,35 +24,35 @@ const lernaJson = require("../lerna.json");
 const webPackages = ["@magda/web-client", "@magda/preview-map"];
 
 const jsPackages = lernaJson.packages
-  .filter(function(packagePath) {
-    return !fse.existsSync(path.resolve(packagePath, "build.sbt"));
-  })
-  .map(packagePath => {
-    const packageJson = require(path.resolve(packagePath, "package.json"));
-    return packageJson.name;
-  })
-  .filter(packageName => webPackages.indexOf(packageName) === -1);
+    .filter(function(packagePath) {
+        return !fse.existsSync(path.resolve(packagePath, "build.sbt"));
+    })
+    .map(packagePath => {
+        const packageJson = require(path.resolve(packagePath, "package.json"));
+        return packageJson.name;
+    })
+    .filter(packageName => webPackages.indexOf(packageName) === -1);
 
 const commands = ["bootstrap", "run build", "run test"];
 
 commands.forEach(command => {
-  const result = childProcess.spawnSync(
-    "lerna",
-    [
-      ...jsPackages.map(package => "--scope " + package),
-      "--concurrency",
-      "4",
-      command
-    ],
-    {
-      stdio: ["pipe", "inherit", "inherit"],
-      shell: true
-    }
-  );
+    const result = childProcess.spawnSync(
+        "lerna",
+        [
+            ...jsPackages.map(package => "--scope " + package),
+            "--concurrency",
+            "4",
+            command
+        ],
+        {
+            stdio: ["pipe", "inherit", "inherit"],
+            shell: true
+        }
+    );
 
-  if (result.status > 0) {
-    process.exit(result.status);
-  }
+    if (result.status > 0) {
+        process.exit(result.status);
+    }
 });
 
 process.exit(0);
