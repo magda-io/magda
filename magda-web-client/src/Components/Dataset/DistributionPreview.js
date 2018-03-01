cf61687cfef25015c4cb18d2024d471c6d31e98e
Chloe Chen <chloeleichen@gmail.com>
2018-03-01 13:52:21 +1100

    basic datavis

--- magda-web-client/src/Components/Dataset/DistributionPreview.js
+++ magda-web-client/src/Components/Dataset/DistributionPreview.js
@@ -1,20 +1,88 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import DataPreviewTable from "../../UI/DataPreviewTable";
+import DataPreviewTextBox from "../../UI/DataPreviewTextBox";
+import DataPreviewGoogleViewer from "../../UI/DataPreviewGoogleViewer";
+import DataPreviewJson from "../../UI/DataPreviewJson";
+import DataPreviewVis from "../../UI/DataPreviewVis";
+import type { PreviewData } from "../../helpers/previewData";
+import { fetchPreviewData } from "../../actions/previewDataActions";
+import ProgressBar from "../../UI/ProgressBar";
+import News from "../../UI/News";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+
 
 class DistributionPreview extends Component {
-  render(){
-    return (<div className='dataset-preview container'>
-              render dataset distribution preview
-            </div>)
+  componentWillMount() {
+        this.props.fetchPreviewData(this.props.distribution);
   }
+
+  componentWillReceiveProps(nextProps) {
+        if (
+            nextProps.distribution.downloadURL &&
+            nextProps.distribution.downloadURL !==
+                this.props.distribution.downloadURL
+        ) {
+            this.props.fetchPreviewData(this.props.distribution);
+        }
+  }
+
+  visualisable() {
+       return !this.props.isFetching && !this.props.error && this.props.data;
+   }
+
+  renderByState(previewData) {
+      return <DataPreviewGoogleViewer data={previewData} />;
+  }
+
+
+  render() {
+        const url = this.props.url;
+        return (
+            <div className="data-previewer">
+                <h3 className="section-heading">
+                    <a href={url} target="_blank">
+                        {url && url.substring(url.lastIndexOf("/") + 1)}
+                    </a>
+                </h3>
+                {this.props.error && <div>{this.props.error}</div>}
+                {this.props.isFetching && <ProgressBar />}
+                {this.props.data &&
+                    this.props.distribution.identifier &&
+                    this.props.data[this.props.distribution.identifier] &&
+                    this.renderByState(
+                        this.props.data[this.props.distribution.identifier]
+                    )}
+            </div>
+        );
+    }
 }
 
 function mapStateToProps(state) {
   const distribution =state.record.distribution;
-  return {
-    distribution
-  };
+  const previewData = state.previewData;
+  const data = previewData.previewData;
+  const isFetching = previewData.isFetching;
+  const error = previewData.error;
+  const url = previewData.url;
+    return {
+        distribution,
+        data,
+        isFetching,
+        error,
+        url
+    };
 }
 
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators(
+        {
+            fetchPreviewData: fetchPreviewData
+        },
+        dispatch
+    );
+};
 
-export default connect(mapStateToProps)(DistributionPreview);
+
+export default connect(mapStateToProps, mapDispatchToProps)(DistributionPreview);
