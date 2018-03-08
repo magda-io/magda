97708908bc9fdaa6b5561ed6a87879e68d59e2ab
Jacky Jiang <jacky.jiang@data61.csiro.au>
2018-03-08 12:11:47 +1100

    Displaying different icons for different data type on dataset page. This commit didn't use result from format sleuther and we will change this once format sleuther is ready to use

--- magda-web-client/src/Components/DistributionRow.js
+++ magda-web-client/src/Components/DistributionRow.js
@@ -4,9 +4,70 @@ import type { ParsedDistribution } from "../helpers/record";
 import { Link } from "react-router-dom";
 import Button from "muicss/lib/react/button";
 import "./DistributionRow.css";
-import formatIcon from "../assets/format-passive-dark.svg";
+import defaultFormatIcon from "../assets/format-passive-dark.svg";
 import downloadIcon from "../assets/download.svg";
 import newTabIcon from "../assets/newtab.svg";
+
+const formatIcons = {
+    default: defaultFormatIcon
+};
+const dataFormatCategories = [
+    "api",
+    "archive",
+    "document",
+    "gis",
+    "html",
+    "image-raster",
+    "image-vector",
+    "presentation",
+    "spreadsheet",
+    "tabular"
+];
+dataFormatCategories.forEach(item => {
+    formatIcons[item] = require(`../assets/data-types/${item}.svg`);
+});
+const CategoryDetermineConfigItems = [
+    {
+        regex: /wfs|wms|geojson|kml|kmz|shp|gdb|csv-geo-au|mpk|ArcGIS/,
+        category: "gis"
+    },
+    {
+        regex: /api|webservice| web service/,
+        category: "api"
+    },
+    {
+        regex: /zip|7z|rar|arj/,
+        category: "archive"
+    },
+    {
+        regex: /doc|pdf|docx|txt|plaintext/,
+        category: "document"
+    },
+    {
+        regex: /html|htm|web page|web site/,
+        category: "html"
+    },
+    {
+        regex: /jpg|gif|jpeg/,
+        category: "image-raster"
+    },
+    {
+        regex: /svg|png/,
+        category: "image-vector"
+    },
+    {
+        regex: /ppt|pptx/,
+        category: "presentation"
+    },
+    {
+        regex: /xlsx|xsl/,
+        category: "spreadsheet"
+    },
+    {
+        regex: /csv|tab/,
+        category: "tabular"
+    }
+];
 
 export type PropType = {
     datasetId: string,
@@ -21,6 +82,21 @@ class DistributionRow extends Component {
         };
     }
 
+    determineFormatIcon() {
+        let format = this.props.distribution.format;
+        if (!format) return defaultFormatIcon;
+        format = format.trim().toLowerCase();
+        let matchedCategory = "default";
+        for (let i = 0; i < CategoryDetermineConfigItems.length; i++) {
+            let config = CategoryDetermineConfigItems[i];
+            if (format.match(config.regex)) {
+                matchedCategory = config.category;
+                break;
+            }
+        }
+        return formatIcons[matchedCategory];
+    }
+
     render() {
         const { datasetId, distribution } = this.props;
         const distributionLink = `/dataset/${encodeURIComponent(
@@ -32,7 +108,7 @@ class DistributionRow extends Component {
                 <div className="mui-col-sm-1">
                     <img
                         className="format-icon"
-                        src={formatIcon}
+                        src={this.determineFormatIcon()}
                         alt="format icon"
                     />
                 </div>
@@ -48,8 +124,8 @@ class DistributionRow extends Component {
                             (typeof distribution.license === "string"
                                 ? distribution.license
                                 : distribution.license.name
-                                    ? distribution.license.name
-                                    : "")}
+                                  ? distribution.license.name
+                                  : "")}
                     </div>
                 </div>
                 <div className="mui-col-sm-3 button-area">
