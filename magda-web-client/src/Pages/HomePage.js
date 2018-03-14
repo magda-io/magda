2f523e470c11efbbb2a83b33aca5db2d075487b0
Jacky Jiang <jacky.jiang@data61.csiro.au>
2018-03-14 17:10:02 +1100

    Add background images

--- magda-web-client/src/Pages/HomePage.js
+++ magda-web-client/src/Pages/HomePage.js
@@ -4,15 +4,44 @@ import { withRouter } from "react-router-dom";
 import Header from "../Components/Header/Header";
 import SearchBox from "../Components/SearchBox/SearchBox";
 import Home from "../Components/Home";
-//import { config } from "../../config.js";
+import { config } from "../config.js";
+import "./HomePage.css";
+
+const getBgImg=()=>{
+    if(!config.homePageConfig || !config.homePageConfig.backgroundImageUrls) return null;
+    const backgroundImageUrls = config.homePageConfig.backgroundImageUrls;
+    if(!backgroundImageUrls.length) return null;
+    const baseUrl = config.homePageConfig.baseUrl ? config.homePageConfig.baseUrl : "";
+    const imgInfoItems = backgroundImageUrls.map(item=>{
+        let width;
+        try{
+            width = parseInt(item.replace(/[^\d]/g,""),10);
+            if(isNaN(width)) width=0;
+        }catch(e){
+            width=0;
+        }
+        return {
+            url : baseUrl+item,
+            width
+        }
+    });
+    const srcset = imgInfoItems.filter(item=>item.width).map(item=>`${item.url} ${item.width}w`).join(", ");
+    let sizes;
+    if(imgInfoItems.length<=1) sizes = `${imgInfoItems[0].width}px`;
+    else sizes = imgInfoItems.slice(1).filter(item=>item.width).map(item=>`(min-width: ${item.width}px) ${item.width}px`).join(", ")+`, ${imgInfoItems[0].width}px`;
+    return <img alt="background image" className="homepage-background-img" src={imgInfoItems[0].url} sizes={sizes} srcSet={srcset} />;
+};
 
 const HomePage = withRouter(({ location }) => {
     return (
-        <Container className="app-container">
-            <Header/>
-            <SearchBox />
-            <Home location={location} />
-        </Container>
+        <div className="homepage-app-container">
+            {getBgImg()}
+            <Container className="app-container">
+                <Header/>
+                <SearchBox />
+                <Home location={location} />
+            </Container>
+        </div>
     );
 });
 
--- magda-web-client/src/homepage-config.js
+++ magda-web-client/src/homepage-config.js
@@ -3,13 +3,16 @@
  * Should be hosted at either:
  * https://s3-ap-southeast-2.amazonaws.com/data-gov-au-frontpage/development/ (for development)
  * or https://s3-ap-southeast-2.amazonaws.com/data-gov-au-frontpage/production/ (for production)
+ * backgroundImageUrl must contain image width as part of url /name. e.g. twenty-1440w.jpg (image width is 1440px)
  */
 window.magda_client_homepage_config = {
     "baseUrl": "https://s3-ap-southeast-2.amazonaws.com/data-gov-au-frontpage/development/",
     "backgroundImageUrls":[
-        "bg-1440.jpg",
-        "bg-800.jpg",
-        "bg-640.jpg"
+        "twenty-1440w.jpg", //--- the first one will be used as default image
+        "twenty-720w.jpg",
+        "twenty-1080w.jpg",
+        "twenty-2160w.jpg",
+        "twenty-2880w.jpg"
     ],
     "stories":[
         "story1.md",
