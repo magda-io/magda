e27cc77b358e2b017af795a28366a1e491e9ede1
Jacky Jiang <jacky.jiang@data61.csiro.au>
2018-03-15 12:23:49 +1100

    Remove unused home

--- magda-web-client/src/Pages/HomePage.js
+++ magda-web-client/src/Pages/HomePage.js
@@ -3,7 +3,6 @@ import Container from "muicss/lib/react/container";
 import { withRouter } from "react-router-dom";
 import Header from "../Components/Header/Header";
 import SearchBox from "../Components/SearchBox/SearchBox";
-import Home from "../Components/Home";
 import { config } from "../config.js";
 import "./HomePage.css";
 
