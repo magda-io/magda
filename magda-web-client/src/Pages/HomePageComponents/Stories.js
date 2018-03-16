141db9292302d538d68403b906d3fc673b0d9cc8
Jacky Jiang <jacky.jiang@data61.csiro.au>
2018-03-16 17:08:13 +1100

    Home Page Stories Mobile Version

--- magda-web-client/src/Pages/HomePageComponents/Stories.js
+++ magda-web-client/src/Pages/HomePageComponents/Stories.js
@@ -1,31 +1,54 @@
 import React from "react";
 import StoryBox from "./StoryBox";
+import { Medium, Small } from "../../UI/Responsive";
+import "./Stories.css";
 
 const Stories = () => {
     return (
         <div className="homepage-stories">
-            <div className="mui-row">
-                <div className="mui-col-md-4">
-                    <StoryBox idx={0} className="story-box-1"/>
+            <Small>
+                <div className="mui-row">
+                    <div className="mui-col-md-12">
+                        <StoryBox idx={0} className="story-box-1" />
+                        <StoryBox idx={1} className="story-box-2" />
+                        <StoryBox idx={2} className="story-box-3" />
+                        <StoryBox idx={3} className="story-box-4" />
+                        <StoryBox idx={4} className="story-box-5" />
+                        <StoryBox idx={5} className="story-box-6" />
+                    </div>
                 </div>
-                <div className="mui-col-md-4">
-                    <StoryBox idx={1} className="story-box-2" />
-                    <StoryBox idx={2} className="story-box-3" />
-                </div>
-                <div className="mui-col-md-4">
-                    <StoryBox idx={3} className="story-box-4" />
-                </div>
-            </div>
-            <div className="mui-row">
-                <div className="mui-col-md-6">
-                    <StoryBox idx={4} className="story-box-5" />
-                </div>
-                <div className="mui-col-md-6">
-                    <StoryBox idx={5} className="story-box-6" />
-                </div>
-            </div>
+            </Small>
+            <Medium>
+                <table className="table-container">
+                    <tbody>
+                        <tr>
+                            <td className="row-1-col-left">
+                                <StoryBox idx={0} className="story-box-1" />
+                            </td>
+                            <td className="row-1-col-middle">
+                                <StoryBox idx={1} className="story-box-2" />
+                                <StoryBox idx={2} className="story-box-3" />
+                            </td>
+                            <td className="row-1-col-right">
+                                <StoryBox idx={3} className="story-box-4" />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className="table-container">
+                    <tbody>
+                        <tr>
+                            <td className="row-2-col-left">
+                                <StoryBox idx={4} className="story-box-5" />
+                            </td>
+                            <td className="row-2-col-right">
+                                <StoryBox idx={5} className="story-box-6" />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </Medium>
         </div>
     );
 };
-
 export default Stories;
