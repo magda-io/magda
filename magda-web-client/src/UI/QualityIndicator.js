42933bf013eb7a10c416815e0210c3d6463d265d
Adam Zerella <adam.zerella@gmail.com>
2018-03-22 16:49:15 +1100

    Modified <a> link to use native react-router link component
    Added alt help text

--- magda-web-client/src/UI/QualityIndicator.js
+++ magda-web-client/src/UI/QualityIndicator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./QualityIndicator.css";
 import helpIcon from "../assets/help-24.svg";
@@ -39,9 +40,9 @@ function QualityIndicator(props) {
                 ))}
             </span>&nbsp;
             <div className="tooltip">
-                <img src={helpIcon} alt=""></img>
+                <img src={helpIcon} alt="Help Link"></img>
                     <span className="tooltiptext">
-                        Calculated using the <a href="page/dataset-quality">Open Data scale</a>
+                        Calculated using the <Link to="page/dataset-quality">Open Data scale</Link>
                     </span>
                 
             </div>
