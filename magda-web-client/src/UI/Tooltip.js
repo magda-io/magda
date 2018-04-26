bdb1864d65009228ebd7206e73c629f52943627e
Adam Zerella <adam.zerella@gmail.com>
2018-04-26 11:33:57 +1000

    Refactored tooltip functionality

---
+++ magda-web-client/src/UI/Tooltip.js
@@ -0,0 +1,22 @@
+import "./Tooltip.css";
+import { Link } from "react-router-dom";
+import React from "react";
+import helpIcon from "../assets/help-24.svg";
+
+/**
+ * @description Return a information tooltip, on hover show calculation method.
+ * @returns { div }
+ */
+const Tooltip = () => {
+    return (
+        <div className="tooltip">
+            <img src={helpIcon} alt="Help Link" />
+            <span className="tooltiptext">
+                Calculated using the{" "}
+                <Link to="/page/dataset-quality">Open Data scale</Link>
+            </span>
+        </div>
+    );
+};
+
+export default Tooltip;
