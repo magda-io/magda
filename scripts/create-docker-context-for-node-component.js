4c8fa20eef63fe4152451f82c068b5d23d7f5ec3
Alex Gilleran <alex.gilleran@nicta.com.au>
2018-04-30 13:58:11 +1000

    Made docker build script default to tag auto

--- scripts/create-docker-context-for-node-component.js
+++ scripts/create-docker-context-for-node-component.js
@@ -16,7 +16,8 @@ const argv = yargs
         tag: {
             description:
                 'The tag to pass to "docker build".  This parameter is only used if --build is specified.  If the value of this parameter is `auto`, a tag name is automatically created from NPM configuration.',
-            type: "string"
+            type: "string",
+            default: "auto"
         },
         repository: {
             description:
