c275a0e2fb2a2b2a2446f1a34798400033601968
Alexander Sadleir <maxious@users.noreply.github.com>
2018-05-09 09:15:09 +1000

    Update Account.js

--- magda-web-client/src/Components/Account/Account.js
+++ magda-web-client/src/Components/Account/Account.js
@@ -47,9 +47,7 @@ class Account extends React.Component {
 }
 
 function mapStateToProps(state) {
-    let {
-        userManagement: { user, providers, providersError }
-    } = state;
+    let { userManagement: { user, providers, providersError } } = state;
 
     return {
         user,
