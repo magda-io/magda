b3dab83db613f80761703dfd03e7b11c4710c8f7
Adam Zerella <adamzerella@users.noreply.github.com>
2018-06-05 16:55:26 +1000

    Added comments to divider component

--- magda-web-client/src/UI/Divider.js
+++ magda-web-client/src/UI/Divider.js
@@ -2,6 +2,10 @@ import React from "react";
 
 import "./Divider.css";
 
+/**
+ * Return a divider with left and right padding, e.g abc | def
+ * @returns { div } - div with character '|'
+ */
 const Divider = () => {
     return <div className="divider">|</div>;
 };
