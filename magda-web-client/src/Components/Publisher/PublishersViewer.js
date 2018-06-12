1ff4e7a05c7a32893e22c94db81745c0276ae29b
chloe <chloeleichen@gmail.com>
2018-06-12 12:15:20 +1000

    org search box

--- magda-web-client/src/Components/Publisher/PublishersViewer.js
+++ magda-web-client/src/Components/Publisher/PublishersViewer.js
@@ -15,8 +15,16 @@ import reduce from "lodash/reduce";
 import findIndex from "lodash/findIndex";
 import trim from "lodash/trim";
 import "./PublishersViewer.css";
+import search from "../../assets/search-dark.svg";
 
 class PublishersViewer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchText: ""
+        };
+        this.onUpdateSearchText = this.onUpdateSearchText.bind(this);
+    }
     componentWillMount() {
         this.props.fetchPublishersIfNeeded(getPageNumber(this.props) || 1);
     }
@@ -35,6 +43,12 @@ class PublishersViewer extends Component {
                     page: i
                 })
             )
+        });
+    }
+
+    onUpdateSearchText(e) {
+        this.setState({
+            searchText: e.target.value
         });
     }
 
@@ -95,7 +109,7 @@ class PublishersViewer extends Component {
             return <ErrorHandler error={this.props.error} />;
         } else {
             return (
-                <div className="col-sm-8">
+                <div>
                     {sortBy(this.mergedPublishers(), [
                         function(o) {
                             return o.name.toLowerCase();
@@ -106,14 +120,37 @@ class PublishersViewer extends Component {
         }
     }
 
+    renderSearchBar() {
+        return (
+            <div className="organization-search">
+                <label htmlFor="organization-search" className="sr-only">
+                    Search for organisations
+                </label>
+                <input
+                    className="au-text-input au-text-input--block"
+                    name="organization-search"
+                    id="organization-search"
+                    type="text"
+                    value={this.searchText}
+                    placeholder="Search for organisations"
+                    onChange={this.onUpdateSearchText}
+                />
+                <img className="search-icon" src={search} alt="search" />
+            </div>
+        );
+    }
+
     render() {
         return (
             <ReactDocumentTitle title={"Organisations | " + config.appName}>
                 <div className="publishers-viewer">
                     <h1>Organisations</h1>
                     <div className="row">
-                        {!this.props.isFetching && this.renderContent()}
+                        <div className="col-sm-8">
+                            {!this.props.isFetching && this.renderContent()}
+                        </div>
                         {this.props.isFetching && <ProgressBar />}
+                        <div className="col-sm-4">{this.renderSearchBar()}</div>
                     </div>
                 </div>
             </ReactDocumentTitle>
