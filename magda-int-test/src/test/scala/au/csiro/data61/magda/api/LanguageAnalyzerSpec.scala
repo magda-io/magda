e8a75629d27ecb8f94c5192d6b79af336fb57978
Alex Gilleran <alex.gilleran@nicta.com.au>
2018-06-26 18:44:05 +1000

    Got all the tests working I think.

--- magda-int-test/src/test/scala/au/csiro/data61/magda/api/LanguageAnalyzerSpec.scala
+++ magda-int-test/src/test/scala/au/csiro/data61/magda/api/LanguageAnalyzerSpec.scala
@@ -100,7 +100,7 @@ class LanguageAnalyzerSpec extends BaseSearchApiSpec {
         val result = responseAs[FacetSearchResult]
         val formats = termExtractor(dataSet)
 
-        withClue(s"format: ${formatName} options ${result.options}") {
+        withClue(s"term: ${formatName} formats: ${formats} options ${result.options}") {
           result.options.exists(value =>
             formats.exists(format =>
               value.value.equalsIgnoreCase(format))) should be(true)
@@ -113,48 +113,95 @@ class LanguageAnalyzerSpec extends BaseSearchApiSpec {
 
   def isAStopWord(term: String) = Generators.luceneStopWords.exists(stopWord => term.trim.equalsIgnoreCase(stopWord))
 
+  def getAllSlices(terms: Seq[String]) = for {
+    start <- 0 to terms.length - 1
+    len <- 1 to (terms.length - start)
+    combinations = terms.drop(start).take(len)
+  } yield combinations
+
   def testLanguageFieldSearch(outerTermExtractor: DataSet => Seq[String], test: (DataSet, String, Route, List[(DataSet, String)]) => Unit, keepOrder: Boolean = false) = {
     it("when searching for it directly") {
-      def innerTermExtractor(dataSet: DataSet) = outerTermExtractor(dataSet).flatMap(MagdaMatchers.tokenize)
+      def innerTermExtractor(dataSet: DataSet) = if (keepOrder) {
+        outerTermExtractor(dataSet).map(MagdaMatchers.tokenize).map(_.mkString(" "))
+      } else {
+        outerTermExtractor(dataSet).flatMap(MagdaMatchers.tokenize)
+      }
 
       doTest(innerTermExtractor, keepOrder)
     }
 
     it(s"regardless of pluralization/depluralization") {
 
-      def innerTermExtractor(dataSet: DataSet) = outerTermExtractor(dataSet)
-        .flatMap(MagdaMatchers.tokenize)
-        .view
-        .map(_.trim)
-        .filterNot(_.contains("."))
-        .filterNot(_.contains("'"))
-        .filterNot(_.toLowerCase.endsWith("ss"))
-        .filterNot(x => x.equalsIgnoreCase("and") || x.equalsIgnoreCase("or"))
-        .filterNot(_.isEmpty)
-        .filterNot(term => term.toLowerCase.endsWith("e") ||
-          term.toLowerCase.endsWith("ies") ||
-          term.toLowerCase.endsWith("es") ||
-          term.toLowerCase.endsWith("y")) // This plays havoc with pluralization because when you add "s" to it, ES chops off the "es at the end
-        .filterNot(isAStopWord)
-        .flatMap {
-          case term if term.last.toLower.equals('s') =>
-            val depluralized = term.take(term.length - 1)
-            if (MagdaMatchers.porterStem(term) == depluralized) {
-              Some(depluralized)
-            } else None
-          case term =>
-            val pluralized = term + "s"
-            if (MagdaMatchers.porterStem(pluralized) == term) {
-              Some(pluralized)
-            } else None
-        }
-        .filterNot(isAStopWord)
+      def innerTermExtractor(dataSet: DataSet) = if (keepOrder) {
+
+        outerTermExtractor(dataSet)
+          .map(term =>
+            MagdaMatchers.tokenize(term).map(_.trim))
+          .filterNot(terms =>
+            terms.exists(term =>
+              term.contains(".") ||
+                term.contains("'") ||
+                term.toLowerCase.endsWith("ss") ||
+                term.toLowerCase.endsWith("e") ||
+                term.toLowerCase.endsWith("ies") ||
+                term.toLowerCase.endsWith("es") ||
+                term.toLowerCase.endsWith("y") ||
+                isAStopWord(term)))
+          .map(terms => terms.flatMap {
+            case term if term.last.toLower.equals('s') =>
+              val depluralized = term.take(term.length - 1)
+              if (MagdaMatchers.porterStem(term) == depluralized) {
+                Some(depluralized)
+              } else None
+            case term =>
+              val pluralized = term + "s"
+              if (MagdaMatchers.porterStem(pluralized) == term) {
+                Some(pluralized)
+              } else None
+          })
+          .filterNot(terms => terms.exists(term => isAStopWord(term)))
+          .map(_.mkString(" "))
+      } else {
+        outerTermExtractor(dataSet)
+          .flatMap(MagdaMatchers.tokenize)
+          .view
+          .map(_.trim)
+          .filterNot(_.contains("."))
+          .filterNot(_.contains("'"))
+          .filterNot(_.toLowerCase.endsWith("ss"))
+          .filterNot(x => x.equalsIgnoreCase("and") || x.equalsIgnoreCase("or"))
+          .filterNot(_.isEmpty)
+          .filterNot(term => term.toLowerCase.endsWith("e") ||
+            term.toLowerCase.endsWith("ies") ||
+            term.toLowerCase.endsWith("es") ||
+            term.toLowerCase.endsWith("y")) // This plays havoc with pluralization because when you add "s" to it, ES chops off the "es at the end
+          .filterNot(isAStopWord)
+          .flatMap {
+            case term if term.last.toLower.equals('s') =>
+              val depluralized = term.take(term.length - 1)
+              if (MagdaMatchers.porterStem(term) == depluralized) {
+                Some(depluralized)
+              } else None
+            case term =>
+              val pluralized = term + "s"
+              if (MagdaMatchers.porterStem(pluralized) == term) {
+                Some(pluralized)
+              } else None
+          }
+          .filterNot(isAStopWord)
+      }
 
       doTest(innerTermExtractor, keepOrder)
     }
 
     def doTest(innerTermExtractor: DataSet => Seq[String], keepOrder: Boolean) = {
-      def getIndividualTerms(terms: Seq[String]) = terms.flatMap(MagdaMatchers.tokenize)
+      def getIndividualTerms(terms: Seq[String]) = terms.map(MagdaMatchers.tokenize)
+
+      /** Checks that there's at least one searchable term in this seq of strings */
+      def check = (list: Seq[String]) =>
+        list.forall(_.length > 2) &&
+          list.exists(term => !Seq("and", "or").contains(term.trim.toLowerCase)) &&
+          list.exists(!isAStopWord(_))
 
       val indexAndTermsGen = smallIndexGen.flatMap {
         case (indexName, dataSetsRaw, routes) ⇒
@@ -164,25 +211,15 @@ class LanguageAnalyzerSpec extends BaseSearchApiSpec {
             val rawTerms = getIndividualTerms(innerTermExtractor(dataSet))
 
             val termGen = if (keepOrder) {
-              /** Checks that there's at least one searchable term in this seq of strings */
-              def check = (list: Seq[String]) =>
-                list.exists(_.length > 2) &&
-                  list.exists(term => !Seq("and", "or").contains(term.trim.toLowerCase)) &&
-                  list.exists(!isAStopWord(_))
-
-              val x = for {
-                len <- 1 to rawTerms.length
-                combinations <- rawTerms.s len
-              } yield combinations
-
-              val validCombinations = x.filter(check)
+              val combinations = rawTerms.flatMap(getAllSlices)
+              val validCombinations = combinations.filter(check)
 
               // Make sure there's _some_ sublist that can be successfully searched - if so try to generate one, otherwise return none
               if (!validCombinations.isEmpty) {
                 Some(Gen.oneOf(validCombinations))
               } else None
             } else {
-              val terms = rawTerms
+              val terms = rawTerms.flatten
                 .filter(_.length > 2)
                 .filterNot(term => Seq("and", "or", "").contains(term.trim.toLowerCase))
                 .filterNot(isAStopWord)
@@ -207,10 +244,16 @@ class LanguageAnalyzerSpec extends BaseSearchApiSpec {
           combinedDataSetAndTermGen.map((indexName, _, routes))
       }
 
-      // We don't want to shrink this kind of tuple at all ever.
       implicit def dataSetStringShrinker(implicit s: Shrink[DataSet], s1: Shrink[Seq[String]]): Shrink[(DataSet, String)] = Shrink[(DataSet, String)] {
         case (dataSet, string) =>
-          val shrunk = string.split("\\s").filter(_ != string)
+          val seq = MagdaMatchers.tokenize(string)
+          val x = for {
+            start <- 0 to seq.length - 1
+            len <- 1 to (seq.length - start)
+            combinations = seq.drop(start).take(len) if !combinations.equals(seq)
+          } yield combinations
+
+          val shrunk = x.map(_.mkString(" "))
 
           logger.error("Shrinking " + string + " to " + shrunk)
 
