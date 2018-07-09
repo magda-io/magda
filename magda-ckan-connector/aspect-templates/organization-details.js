1155d63762f4c3dda94d6fb9537207f3ff5c42bf
Jacky Jiang <jacky.jiang@data61.csiro.au>
2018-07-09 17:26:18 +1000

    CKan connector: add telephone, email & website fields

--- magda-ckan-connector/aspect-templates/organization-details.js
+++ magda-ckan-connector/aspect-templates/organization-details.js
@@ -1,6 +1,29 @@
+let telephone = null;
+let website = null;
+let email = null;
+
+if(organization && organization.extras && organization.extras.length){
+    organization.extras.forEach(item=>{
+        switch(item["key"]){
+            case "email" : 
+                email = item["value"];
+                break;
+            case "telephone" : 
+                telephone = item["value"];
+                break;
+            case "website" : 
+                website = item["value"];
+                break;
+        }
+    });
+}
+
 return {
     name: organization.name,
     title: organization.title,
     description: organization.description,
-    imageUrl: organization.image_display_url || organization.image_url
+    imageUrl: organization.image_display_url || organization.image_url,
+    telephone,
+    email,
+    website
 };
