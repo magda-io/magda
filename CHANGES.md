1ae1d8a487bcd48a60351ccddf60c560e56fe17e
Alex Sadleir <alex.sadleir@digital.gov.au>
2018-07-10 15:23:57 +1000

    Updated header logo to be correct branding type

--- CHANGES.md
+++ CHANGES.md
@@ -34,6 +34,7 @@
 ## 0.0.44
 
 -   Added more analytics events for Downloads/Views by Organisation, Search result click and Dataset request/feedback
+-   Updated header logo to be correct branding type
 -   Removed `button` element for distribution download link on dataset page.
 -   Updated documentation for setting up `Docker Edge` for Kubernetes.
 -   Modified error message text and omitted homepage articles if search error occurs.
--- magda-web-client/src/Components/Header/Header.js
+++ magda-web-client/src/Components/Header/Header.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Medium, Small } from "../../UI/Responsive";
 import { Link } from "react-router-dom";
 import govtLogo from "../../assets/au-govt-logo.svg";
+import dgaLogo from "../../assets/dga-logo.svg";
 import HeaderNav from "./HeaderNav";
 import HeaderMobile from "./HeaderMobile";
 import "./Header.css";
+import { config } from "../../config";
 
 const Header = props => {
     return (
@@ -15,7 +17,19 @@ const Header = props => {
             <Medium>
                 <div className="desktop-nav container">
                     <Link to="/" className="logo">
-                        <img src={govtLogo} alt="Coat of Arms" />
+                        <div>
+                            <img
+                                src={govtLogo}
+                                height={70}
+                                alt="Coat of Arms"
+                            />
+                            <img
+                                className="site-logo"
+                                src={dgaLogo}
+                                height={70}
+                                alt={config.appName}
+                            />
+                        </div>
                     </Link>
                     <HeaderNav />
                 </div>
