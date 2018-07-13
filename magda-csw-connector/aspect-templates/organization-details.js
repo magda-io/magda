8b009c40d651e7d272c3521742f2fc5c5e18b6ed
Jacky Jiang <jacky.jiang@data61.csiro.au>
2018-07-13 11:17:16 +1000

    correct var definition: unnecessary let should be consts Remove duplicate open project connector debug setting

--- magda-csw-connector/aspect-templates/organization-details.js
+++ magda-csw-connector/aspect-templates/organization-details.js
@@ -1,13 +1,37 @@
 const name = transformer.getNameFromJsonOrganization(organization);
 const jsonpath = libraries.jsonpath;
-let phone = jsonpath.value(organization,"$.contactInfo[*].CI_Contact[*].phone[*].CI_Telephone[*].voice[0].CharacterString[0]._");
-let website = jsonpath.value(organization,"$.contactInfo[*].CI_Contact[*].onlineResource[*].CI_OnlineResource[*].linkage[*].URL[0]._");
-let email = jsonpath.value(organization,"$.contactInfo[*].CI_Contact[*].address[*].CI_Address[*].electronicMailAddress[*].CharacterString[0]._");
-let addrStreet = jsonpath.value(organization,"$.contactInfo[*].CI_Contact[*].address[*].CI_Address[*].deliveryPoint[*].CharacterString[0]._");
-let addrSuburb = jsonpath.value(organization,"$.contactInfo[*].CI_Contact[*].address[*].CI_Address[*].city[*].CharacterString[0]._");
-let addrState = jsonpath.value(organization,"$.contactInfo[*].CI_Contact[*].address[*].CI_Address[*].administrativeArea[*].CharacterString[0]._");
-let addrPostCode = jsonpath.value(organization,"$.contactInfo[*].CI_Contact[*].address[*].CI_Address[*].postalCode[*].CharacterString[0]._");
-let addrCountry =jsonpath.value(organization,"$.contactInfo[*].CI_Contact[*].address[*].CI_Address[*].country[*].Country[0]._");
+const phone = jsonpath.value(
+    organization,
+    "$.contactInfo[*].CI_Contact[*].phone[*].CI_Telephone[*].voice[0].CharacterString[0]._"
+);
+const website = jsonpath.value(
+    organization,
+    "$.contactInfo[*].CI_Contact[*].onlineResource[*].CI_OnlineResource[*].linkage[*].URL[0]._"
+);
+const email = jsonpath.value(
+    organization,
+    "$.contactInfo[*].CI_Contact[*].address[*].CI_Address[*].electronicMailAddress[*].CharacterString[0]._"
+);
+const addrStreet = jsonpath.value(
+    organization,
+    "$.contactInfo[*].CI_Contact[*].address[*].CI_Address[*].deliveryPoint[*].CharacterString[0]._"
+);
+const addrSuburb = jsonpath.value(
+    organization,
+    "$.contactInfo[*].CI_Contact[*].address[*].CI_Address[*].city[*].CharacterString[0]._"
+);
+const addrState = jsonpath.value(
+    organization,
+    "$.contactInfo[*].CI_Contact[*].address[*].CI_Address[*].administrativeArea[*].CharacterString[0]._"
+);
+const addrPostCode = jsonpath.value(
+    organization,
+    "$.contactInfo[*].CI_Contact[*].address[*].CI_Address[*].postalCode[*].CharacterString[0]._"
+);
+const addrCountry = jsonpath.value(
+    organization,
+    "$.contactInfo[*].CI_Contact[*].address[*].CI_Address[*].country[*].Country[0]._"
+);
 
 const data = {
     name: name,
@@ -24,8 +48,8 @@ const data = {
     website
 };
 
-Object.keys(data).forEach(key=>{
-    if(!data[key]) delete data[key];
+Object.keys(data).forEach(key => {
+    if (!data[key]) delete data[key];
 });
 
 return data;
