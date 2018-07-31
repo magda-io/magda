30650fd7a95dad0be39efa99240dcb57e1d5b838
Jacky Jiang <jacky.jiang@data61.csiro.au>
2018-07-31 10:00:37 +1000

    Add customised error page for 500

--- magda-web-client/src/Components/ErrorPage/index.js
+++ magda-web-client/src/Components/ErrorPage/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import * as queryString from "query-string";
 import ErrorHandler from "../ErrorHandler";
 import Error404 from "./Error404";
+import Error500 from "./Error500";
 
 export default function ErrorPage({ location }) {
     const query = queryString.parse(location.search);
@@ -9,6 +10,8 @@ export default function ErrorPage({ location }) {
     switch (errorCode) {
         case "404":
             return <Error404 errorData={query} />;
+        case "500":
+            return <Error500 errorData={query} />;
         default:
             return (
                 <ErrorHandler
