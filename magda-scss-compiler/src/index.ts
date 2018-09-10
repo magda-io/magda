155e8ca89c28ac9a9d9ebd844c06cab0d47b019a
Jacky Jiang <jacky.jiang@data61.csiro.au>
2018-09-10 14:50:58 +1000

    scss compiler in progress

--- magda-scss-compiler/src/index.ts
+++ magda-scss-compiler/src/index.ts
@@ -1,6 +1,6 @@
 import * as yargs from "yargs";
 import * as path from "path";
-import compile from "./compile";
+//import compile from "./compile";
 
 const argv = yargs
     .config()
@@ -9,7 +9,28 @@ const argv = yargs
         describe: "SCSS vars to override. Expect JSON format string.",
         type: "string",
         default: ""
+    })
+    .option("contentApiUrl", {
+        describe: "The base URL of the content API.",
+        type: "string",
+        default: "http://localhost:6119/v0"
+    })
+    .option("jwtSecret", {
+        describe: "The shared secret for intra-network communication",
+        type: "string",
+        demand: true,
+        default:
+            process.env.JWT_SECRET || process.env.npm_package_config_jwtSecret
+    })
+    .option("userId", {
+        describe:
+            "The user id to use when making authenticated requests to the registry",
+        type: "string",
+        demand: true,
+        default: process.env.USER_ID || process.env.npm_package_config_userId
     }).argv;
+
+console.log(argv);
 
 const clientRoot = path.resolve(
     require.resolve("@magda/web-client/package.json"),
@@ -30,12 +51,13 @@ try {
 }
 
 try {
-    compile(clientRoot, argv.scssVars)
+    console.log(clientRoot);
+    /*compile(clientRoot, argv.scssVars)
         .then(result => {
             console.log(result);
         })
         .catch(e => {
             console.error("Error happend when compile SCSS: ", e);
             process.exit(1);
-        });
+        });*/
 } catch (e) {}
---
+++ magda-scss-compiler/src/saveToContentApi.ts
@@ -0,0 +1,9 @@
+import buildJwt from "@magda/typescript-common/dist/session/buildJwt";
+import * as request from "request";
+
+const saveToContentApi = (cssContent, contentApiUrl, userId, jwtSecret) => {
+    return new Promise((resolve, reject) => {});
+    request(contentApiUrl);
+};
+
+export default saveToContentApi;
