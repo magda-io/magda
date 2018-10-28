c2cda843f72c05522087ce332f01851229377ee3
Alex Gilleran <AlexGilleran@users.noreply.github.com>
2018-10-29 10:51:26 +1100

    Update contentReducer.js

--- magda-web-client/src/reducers/contentReducer.js
+++ magda-web-client/src/reducers/contentReducer.js
@@ -77,7 +77,6 @@ function parseContent(content) {
     let highlights = {};
     let stories = {};
     const lang = "en";
-    let strings = Object.assign(defaultStrings);
     let strings = Object.assign({}, defaultStrings);
     for (const item of content) {
         if (item.id === "home/tagline/desktop") {
