8f84f35c387b175d6fa860efc481ffdae0a33942
Nahid Akbar <nahid.akbar@data61.csiro.au>
2018-11-05 16:46:15 +1100

    save

--- magda-migrator-authorization-db/sql/V2__refactor.sql
+++ magda-migrator-authorization-db/sql/V2__refactor.sql
@@ -2,22 +2,47 @@
 -- Make new table changes
 -------------------------------
 
+CREATE TYPE version AS (
+    id       bigserial,
+    time     timestamptz DEFAULT NOW(),
+    lastId   bigint,
+    user     uuid NOT NULL,
+    deleted  boolean DEFAULT FALSE
+);
+
+
 CREATE TABLE public.policy
 (
     id character varying(200) NOT NULL CHECK (id ~* '^[A-Z0-9_-]+$'),
     description text,
     statements jsonb NOT NULL,
-    owner uuid,
-    CONSTRAINT policy_pkey PRIMARY KEY (id)
+    -- history
+    serial       bigserial,
+    time         timestamptz NOT NULL DEFAULT NOW(),
+    nextSerial   bigint DEFAULT -1,
+    "user"       uuid NOT NULL,
+    deleted      boolean NOT NULL DEFAULT FALSE,
+  	"comments"   text DEFAULT NULL
+)
+WITH (
+    OIDS = FALSE
 )
 TABLESPACE pg_default;
 
+-- partial index to get latest state
+CREATE INDEX policy_id_latest
+	ON public.policy (id)
+  WHERE nextSerial = -1;
+
 CREATE TABLE public.group
 (
-    id character varying(200) NOT NULL CHECK (id ~* '^[A-Z0-9_-]+$'),
+    id character varying(200) NOT NULL CHECK (id ~* '^[A-Za-z0-9_-]+$'),
     description text,
     policy character varying(200)[],
     CONSTRAINT group_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
 )
 TABLESPACE pg_default;
 
@@ -34,7 +59,7 @@ ALTER TABLE public.users
 -- Migrate old data
 -------------------------------
 
-INSERT INTO public.policy(id, description, statements, owner)
+INSERT INTO public.policy(id, description, statements, "user")
 VALUES ('ALL', 'Full System Read and Write', '[
     {
         "resource": ["*"],
