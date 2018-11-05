20e815293da8dd69153f060a799b074e47831c9a
Nahid Akbar <nahid.akbar@data61.csiro.au>
2018-11-05 12:57:32 +1100

    save

---
+++ magda-migrator-authorization-db/sql/V2__refactor.sql
@@ -0,0 +1,33 @@
+CREATE EXTENSION IF NOT EXISTS "postgres-json-schema";
+
+CREATE TABLE public.policy
+(
+    id character varying(200) NOT NULL,
+    description text,
+    statements jsonb NOT NULL CHECK(validate_json_schema($$
+    {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "resource": {
+            "type": "array",
+            "items": {
+              "type": "string"
+            }
+          },
+          "action": {
+            "type": "array",
+            "items": {
+              "type": "string"
+            }
+          },
+          "allow": {
+            "type": "boolean"
+          }
+        }
+      }
+    }$$, statements)),
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+TABLESPACE pg_default;
