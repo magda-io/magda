4c1af78d2dfb3991f6071d185a5c87f1b072e37e
Nahid Akbar <nahid.akbar@data61.csiro.au>
2018-11-05 14:31:00 +1100

    save

--- magda-migrator-authorization-db/sql/V2__refactor.sql
+++ magda-migrator-authorization-db/sql/V2__refactor.sql
@@ -1,34 +1,69 @@
-CREATE EXTENSION IF NOT EXISTS "postgres-json-schema";
+-------------------------------
+-- Make new table changes
+-------------------------------
 
 CREATE TABLE public.policy
 (
-    id character varying(200) NOT NULL,
+    id character varying(200) NOT NULL CHECK (id ~* '^[A-Z0-9_-]+$'),
     description text,
-    statements jsonb NOT NULL CHECK(validate_json_schema($$
-    {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "resource": {
-            "type": "array",
-            "items": {
-              "type": "string"
-            }
-          },
-          "action": {
-            "type": "array",
-            "items": {
-              "type": "string"
-            }
-          },
-          "allow": {
-            "type": "boolean"
-          }
-        }
-      }
-    }$$, statements)),
+    statements jsonb NOT NULL,
     owner uuid,
-    CONSTRAINT users_pkey PRIMARY KEY (id)
+    CONSTRAINT policy_pkey PRIMARY KEY (id)
 )
 TABLESPACE pg_default;
+
+CREATE TABLE public.group
+(
+    id character varying(200) NOT NULL CHECK (id ~* '^[A-Z0-9_-]+$'),
+    description text,
+    policy character varying(200)[],
+    CONSTRAINT group_pkey PRIMARY KEY (id)
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.users
+  ADD policy character varying(200)[],
+  ADD groups character varying(200)[];
+
+-------------------------------
+-- Insert new data
+-------------------------------
+
+
+-------------------------------
+-- Migrate old data
+-------------------------------
+
+INSERT INTO public.policy(id, description, statements, owner)
+VALUES ('ALL', 'Full System Read and Write', '[
+    {
+        "resource": ["*"],
+        "action": ["create", "read", "update", "delete"],
+        "allow": true
+    }
+]', '00000000-0000-4000-8000-000000000000');
+
+  -- CHECK(validate_json_schema($$
+  -- {
+  --   "type": "array",
+  --   "items": {
+  --     "type": "object",
+  --     "properties": {
+  --       "resource": {
+  --         "type": "array",
+  --         "items": {
+  --           "type": "string"
+  --         }
+  --       },
+  --       "action": {
+  --         "type": "array",
+  --         "items": {
+  --           "type": "string"
+  --         }
+  --       },
+  --       "allow": {
+  --         "type": "boolean"
+  --       }
+  --     }
+  --   }
+  -- }$$, statements))
