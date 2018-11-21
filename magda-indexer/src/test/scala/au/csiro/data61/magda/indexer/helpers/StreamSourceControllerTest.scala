3cae06c0f6a49d2fe3551f93b928db8c1b32b2e1
Mike Wu <mike.wu@data61.csiro.au>
2018-11-22 09:08:42 +1100

    wip

--- magda-indexer/src/test/scala/au/csiro/data61/magda/indexer/helpers/StreamSourceControllerTest.scala
+++ magda-indexer/src/test/scala/au/csiro/data61/magda/indexer/helpers/StreamSourceControllerTest.scala
@@ -10,17 +10,16 @@ import org.scalatest.{AsyncFlatSpec, _}
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
-class StreamSourceControllerTest extends AsyncFlatSpec with Matchers
-  with BeforeAndAfterAll with BeforeAndAfterEach {
+class StreamSourceControllerTest extends AsyncFlatSpec with Matchers with BeforeAndAfterEach {
 
   implicit val system: ActorSystem = ActorSystem("StreamSourceControllerTest")
   implicit val ec: ExecutionContextExecutor = system.dispatcher
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val config: Config = ConfigFactory.load()
 
-  var ssc: StreamSourceController = None.orNull
-  var actorRef: ActorRef = None.orNull
-  var source: Source[DataSet, NotUsed] = None.orNull
+  private var ssc: StreamSourceController = None.orNull
+  private var actorRef: ActorRef = None.orNull
+  private var source: Source[DataSet, NotUsed] = None.orNull
 
   private val dataSet1 =
     DataSet(identifier = "d1", catalog = Some("c"), quality = 1.0D, score = Some(1.0F))
@@ -44,39 +43,29 @@ class StreamSourceControllerTest extends AsyncFlatSpec with Matchers
     actorRef ! PoisonPill
   }
 
-  private def futureAssert(actualF: Future[Seq[DataSet]],
-                           expected: Seq[DataSet]): Future[Assertion] = {
-    actualF.map(actual => {
-      // This block might not be executed at all for some programming error.
-      // Print out for checking when debugging.
-      println(actual)
-      actual shouldEqual expected
-    })
+  "The stream source controller" should "fill the source after the stream starts" in {
+    // take(dataSets.size) in order to terminate the stream automatically
+    // so that actualDataSetsF can complete.
+    val actualDataSetsF: Future[Seq[DataSet]] = source.take(dataSets.size).runWith(Sink.seq)
+
+    // Fill the source.
+    dataSets.foreach(dataSet => actorRef ! dataSet)
+
+    actualDataSetsF.map(actual => actual shouldEqual dataSets)
   }
 
-  "The stream source controller" should "fill the source after the stream is alive" in {
-      // take(dataSets.size) in order to terminate the stream automatically
-      // so that actualDataSetsF can complete.
-      val actualDataSetsF: Future[Seq[DataSet]] = source.take(dataSets.size).runWith(Sink.seq)
-
-      // Fill the source.
-      dataSets.foreach(dataSet => actorRef ! dataSet)
-
-      futureAssert(actualDataSetsF, dataSets)
+  it should "fill the source before the stream starts" in {
+    dataSets.foreach(dataSet => actorRef ! dataSet)
+    val actualDataSetsF: Future[Seq[DataSet]] = source.take(dataSets.size).runWith(Sink.seq)
+    actualDataSetsF.map(actual => actual shouldEqual dataSets)
   }
 
-  it should "fill the source before the stream is alive" in {
-      dataSets.foreach(dataSet => actorRef ! dataSet)
-      val actualDataSetsF: Future[Seq[DataSet]] = source.take(dataSets.size).runWith(Sink.seq)
-      futureAssert(actualDataSetsF, dataSets)
-    }
-
-  it should "fill the source before and after the stream is alive" in {
-      dataSets.foreach(dataSet => actorRef ! dataSet)
-      val actualDataSetsF: Future[Seq[DataSet]] = source.take(2*dataSets.size).runWith(Sink.seq)
-      dataSets.foreach(dataSet => actorRef ! dataSet)
-      futureAssert(actualDataSetsF, dataSets ++ dataSets)
-    }
+  it should "fill the source before and after the stream starts" in {
+    dataSets.foreach(dataSet => actorRef ! dataSet)
+    val actualDataSetsF: Future[Seq[DataSet]] = source.take(2*dataSets.size).runWith(Sink.seq)
+    dataSets.foreach(dataSet => actorRef ! dataSet)
+    actualDataSetsF.map(actual => actual shouldEqual dataSets ++ dataSets)
+  }
 
   it should "be able to terminate the stream explicitly" in {
     // Fill the source.
@@ -85,18 +74,16 @@ class StreamSourceControllerTest extends AsyncFlatSpec with Matchers
     // Run the stream.
     val actualDataSetsF: Future[Seq[DataSet]] = source.runWith(Sink.seq)
 
-    // Fill more.
+    // Fill more to the source.
     dataSets.foreach(dataSet => actorRef ! dataSet)
 
-    // Give some time for actualDataSetsF to run. The future will complete when the stream
-    // is terminated by calling ssc.terminate().
-    // If not sleeping here, terminate() might be called before the stream gets chance to run,
-    // resulting in an empty actualDataSets next.
+    // No more data to fill the source, terminate the stream so that actualDataSetsF can complete.
+    // Call terminate() after some delay in order to give some time for actualDataSetsF to run.
+    // If calling terminate() without delay, the future may complete before the stream gets a
+    // chance to run, resulting in an empty actualDataSetsF.
     Thread.sleep(500)
-
-    // No more data to fill the source, terminate the stream so that
-    // actualDataSetsF then resultF can complete
     ssc.terminate()
-    futureAssert(actualDataSetsF, dataSets ++ dataSets)
+
+    actualDataSetsF.map(actual => actual shouldEqual dataSets ++ dataSets)
   }
 }
