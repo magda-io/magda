6b07983af14262f9935e55ae9e91851baca61a5f
Mike Wu <mike.wu@data61.csiro.au>
2018-11-22 14:58:48 +1100

    wip

--- magda-indexer/src/test/scala/au/csiro/data61/magda/indexer/helpers/StreamControllerTest.scala
+++ magda-indexer/src/test/scala/au/csiro/data61/magda/indexer/helpers/StreamControllerTest.scala
@@ -1,13 +1,20 @@
 package au.csiro.data61.magda.indexer.helpers
 
+import java.time.OffsetDateTime
+
+import akka.NotUsed
 import akka.actor.{ActorSystem, PoisonPill}
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Sink
+import akka.stream.scaladsl.{Sink, Source}
+import au.csiro.data61.magda.indexer.search.SearchIndexer
+import au.csiro.data61.magda.indexer.search.SearchIndexer.IndexResult
 import au.csiro.data61.magda.model.misc.DataSet
+import au.csiro.data61.magda.search.elasticsearch.Indices
 import com.typesafe.config.{Config, ConfigFactory}
 import org.scalatest.{AsyncFlatSpec, BeforeAndAfterEach, Matchers}
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.{ExecutionContextExecutor, Future, Promise}
 
 class StreamControllerTest extends AsyncFlatSpec with Matchers with BeforeAndAfterEach {
 
@@ -61,7 +68,7 @@ class StreamControllerTest extends AsyncFlatSpec with Matchers with BeforeAndAft
     }
   }
 
-  "The stream controller" should "control the stream flow" in {
+  "The stream controller" should "control the simple stream flow" in {
 
     // Create the stream source.
     ssc = new StreamSourceController()
@@ -91,5 +98,83 @@ class StreamControllerTest extends AsyncFlatSpec with Matchers with BeforeAndAft
     nextTokenOptionF.map(tokenOption => tokenOption shouldEqual tokenOption2)
 
     actualDataSetsF.flatMap(dataSets => dataSets shouldEqual dataSets1 ++ dataSets2)
+  }
+
+  class MockIndexer(streamController: StreamController) extends SearchIndexer{
+    private var dataSetCount = 0
+    private var batchCount = 0
+    private val batchSize = 2
+    private val batchNum = 2
+    private val buffer = new ListBuffer[Promise[Unit]]()
+    override def index(source: Source[DataSet, NotUsed]): Future[SearchIndexer.IndexResult] = {
+      val indexResults = source
+        .map(dataSet => {
+          println(s"Received: $dataSet")
+          (dataSet.uniqueId, index(dataSet))
+         })
+        .runWith(Sink.fold(Future(SearchIndexer.IndexResult(0, Seq()))) {
+          case (combinedResultFuture, (thisResultIdentifier, thisResultFuture)) =>
+            if (batchCount < batchNum) {
+              println("Request next batch")
+              streamController.next(Some(""), 2)
+            }
+            else {
+              println("Terminate the stream")
+              streamController.next(None, 2)
+            }
+
+            Future(SearchIndexer.IndexResult(0, Seq()))
+//            combinedResultFuture.flatMap { combinedResult =>
+//              thisResultFuture.map { _ =>
+//
+//                combinedResult.copy(successes = combinedResult.successes + 1)
+//              }
+//            }
+      })
+
+      indexResults.flatMap(identity)
+    }
+
+    private def index(dataSet: DataSet, promise: Promise[Unit] = Promise[Unit]): Future[Unit] = {
+      dataSetCount += 1
+      buffer.append(promise)
+
+      // Simulate batch processing
+      if (dataSetCount % batchSize == 0) {
+        batchCount += 1
+        buffer.toList.foreach(promise => promise.future.flatMap(_ => Future.successful("Done")))
+        buffer.clear()
+      }
+
+      promise.future
+    }
+
+    override def delete(identifiers: Seq[String]): Future[Unit] = ???
+
+    override def snapshot(): Future[Unit] = ???
+
+    override def ready: Future[Unit] = ???
+
+    override def trim(before: OffsetDateTime): Future[Unit] = ???
+
+    override def isEmpty(index: Indices.Index): Future[Boolean] = ???
+  }
+
+  "The stream controller" should "control the stream flow" in {
+
+    // Create the stream source.
+    ssc = new StreamSourceController()
+    val (_, source) = ssc.refAndSource
+
+    val mockRegistryInterface = new MockRegistryInterface()
+    sc = new StreamController(mockRegistryInterface, ssc)
+    sc.start(50)
+    val mockIndexer = new MockIndexer(sc)
+
+    // Start the stream.
+    val indexResultF: Future[SearchIndexer.IndexResult] = mockIndexer.index(source)
+
+    indexResultF.map(indexResult => indexResult shouldEqual IndexResult(0,List()))
+
   }
 }
