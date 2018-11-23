d98534e498d18d8f2f75f05deb7a5dcc42675cee
Mike Wu <mike.wu@data61.csiro.au>
2018-11-23 12:43:36 +1100

    Improved StreamSourceController.

--- magda-indexer/src/main/scala/au/csiro/data61/magda/indexer/helpers/StreamSourceController.scala
+++ magda-indexer/src/main/scala/au/csiro/data61/magda/indexer/helpers/StreamSourceController.scala
@@ -3,8 +3,11 @@ package au.csiro.data61.magda.indexer.helpers
 import akka.NotUsed
 import akka.actor.{ActorRef, ActorSystem, Scheduler}
 import akka.event.Logging
+import akka.stream._
+import akka.stream.impl.Stages.DefaultAttributes
+import akka.stream.impl.fusing.GraphStages.SimpleLinearGraphStage
 import akka.stream.scaladsl.{Keep, Sink, Source}
-import akka.stream.{KillSwitches, Materializer, OverflowStrategy}
+import akka.stream.stage.{GraphStageLogic, InHandler, OutHandler}
 import au.csiro.data61.magda.model.misc.DataSet
 import com.typesafe.config.Config
 import org.reactivestreams.Publisher
@@ -19,23 +22,44 @@ class StreamSourceController(bufferSize: Int)(implicit val system: ActorSystem,
   implicit val ec: ExecutionContextExecutor = system.dispatcher
   implicit val scheduler: Scheduler = system.scheduler
 
-  private val sharedKillSwitch = KillSwitches.shared("my-kill-switch")
   private val (ref, source) = createStreamSource()
+  val NO_MORE_DATASETS: String = "Done"
+
+  final case class CheckEnd[Object]() extends SimpleLinearGraphStage[Object] {
+    override def initialAttributes: Attributes = DefaultAttributes.take
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+      new GraphStageLogic(shape) with InHandler with OutHandler {
+        override def onPush(): Unit = {
+          val x = grab(in)
+          if (x == NO_MORE_DATASETS)
+            completeStage()
+          else
+            push(out, x)
+        }
+
+        override def onPull(): Unit = {
+          pull(in)
+        }
+
+       setHandlers(in, out, this)
+      }
+
+    override def toString: String = "Check"
+  }
 
   private def createStreamSource(): (ActorRef, Source[DataSet, NotUsed]) = {
     val (ref: ActorRef, publisher: Publisher[DataSet]) =
-      Source.actorRef[DataSet](bufferSize = bufferSize, OverflowStrategy.fail)
+      Source.actorRef[DataSet](bufferSize = bufferSize+1, OverflowStrategy.fail)
         .toMat(Sink.asPublisher(false))(Keep.both).run()
 
-    val source = Source.fromPublisher(publisher)
-
-    val source2: Source[DataSet, NotUsed] = source.via(sharedKillSwitch.flow)
-
+    val source: Source[DataSet, NotUsed] = Source.fromPublisher(publisher)
+    val source2: Source[DataSet, NotUsed] = source.via(CheckEnd.apply())
     (ref, source2)
   }
 
   def terminate(): Unit = {
-    sharedKillSwitch.shutdown()
+    ref ! NO_MORE_DATASETS
   }
 
   def fillSource(dataSets: Seq[DataSet]): Unit = {
@@ -48,5 +72,4 @@ class StreamSourceController(bufferSize: Int)(implicit val system: ActorSystem,
   def refAndSource: (ActorRef, Source[DataSet, NotUsed]) = {
     (ref, source)
   }
-
 }
