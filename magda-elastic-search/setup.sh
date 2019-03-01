01a8b414c2fbd81cc8acc3c9534c9950e74c1dcc
Jacky Jiang <t83714@gmail.com>
2019-03-01 13:46:04 +1100

    Install repository-gcs plugin

--- magda-elastic-search/setup.sh
+++ magda-elastic-search/setup.sh
@@ -10,6 +10,9 @@ mkdir /snapshots
 chown -R elasticsearch:elasticsearch /snapshots
 
 if [[ ! -z $GOOGLE_APPLICATION_CREDENTIALS ]]; then
+    echo "Install repository-gcs plugin..."
+    sudo -u elasticsearch -E bin/elasticsearch-plugin install -b repository-gcs
+    echo "Finished Install repository-gcs plugin..."
     echo "Creating gcs permissions..."
     sudo -u elasticsearch -E bin/elasticsearch-keystore create
     sudo -u elasticsearch -E bin/elasticsearch-keystore add-file gcs.client.default.credentials_file $GOOGLE_APPLICATION_CREDENTIALS
