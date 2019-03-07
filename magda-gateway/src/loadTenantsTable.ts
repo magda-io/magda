2ee3d0daf112fb02db69bab7bc637e64b686c0de
Mike Wu <mike.wu@data61.csiro.au>
2019-03-07 14:19:01 +1100

    Fixed import paths and revised docker entrypoint change.

--- magda-gateway/src/loadTenantsTable.ts
+++ magda-gateway/src/loadTenantsTable.ts
@@ -1,12 +1,12 @@
-import RegistryClient from "../../magda-typescript-common/src/registry/RegistryClient";
-import { Tenant } from "../../magda-typescript-common/src/generated/registry/api";
+import RegistryClient from "@magda/magda-typescript-common/dist/registry/RegistryClient";
+import { Tenant } from "@magda/magda-typescript-common/dist/generated/registry/api";
 
 export default function loadTenantsTable(
     tenantsTable: Map<String, Tenant>,
     url: string
 ) {
     let registryClient = new RegistryClient({ baseUrl: url });
-    registryClient.getTenants().then(tenants => {
+    registryClient.getTenants().then((tenants: any[]) => {
         let tenantsJson = <Array<Tenant>>tenants;
         tenantsJson.forEach(t => tenantsTable.set(t.domainName, t));
     });
