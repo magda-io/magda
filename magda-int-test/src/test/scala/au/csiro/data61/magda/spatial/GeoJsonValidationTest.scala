3bbfe02eb528dc31b13aa6e7a3a826800432ef58
Jacky Jiang <jacky.jiang@data61.csiro.au>
2019-03-15 17:02:15 +1100

    Added test cases

---
+++ magda-int-test/src/test/scala/au/csiro/data61/magda/spatial/GeoJsonValidationTest.scala
@@ -0,0 +1,57 @@
+package au.csiro.data61.magda.spatial
+import au.csiro.data61.magda.model.misc.Location
+import org.scalatest.{Assertion, FlatSpec, FunSpec, Matchers}
+import org.slf4j.LoggerFactory
+
+import scala.util.{Failure, Success, Try}
+
+
+class GeoJsonValidationTest extends FunSpec with Matchers {
+
+  val logger = LoggerFactory.getLogger(getClass)
+
+  describe("GeoJson Data Validation") {
+    it("should detect invalidation data with invalid Longitude"){
+      val geoJson = "{\"type\": \"Polygon\", \"coordinates\": [[[20106.11903, -31.85658], [152.11903, -28.53715], [147.39402, -28.53715], [147.39402, -31.85658], [152.11903, -31.85658]]]}"
+      withClue(){
+        Try(Location(geoJson)) match {
+          case Failure(e) =>
+            logger.info("Detected invalid GeoJson data: {}", e.toString)
+            succeed
+          case Success(v) =>
+            val g = v.geoJson
+            fail("Failed to detect invalid geoJson Data")
+        }
+      }
+    }
+
+    it("should detect invalidation data with invalid latitude"){
+      val geoJson = "{\"type\": \"Polygon\", \"coordinates\": [[[152.11903, -91.85658], [152.11903, -28.53715], [147.39402, -28.53715], [147.39402, -31.85658], [152.11903, -31.85658]]]}"
+      withClue(){
+        Try(Location(geoJson)) match {
+          case Failure(e) =>
+            logger.info("Detected invalid GeoJson data: {}", e.toString)
+            succeed
+          case Success(v) =>
+            val g = v.geoJson
+            fail("Failed to detect invalid geoJson Data")
+        }
+      }
+    }
+
+    it("should return `Location` if it comes with valid coordinates"){
+      val geoJson = "{\"type\": \"Polygon\", \"coordinates\": [[[152.11903, -31.85658], [152.11903, -28.53715], [147.39402, -28.53715], [147.39402, -31.85658], [152.11903, -31.85658]]]}"
+      withClue(){
+        Try(Location(geoJson)) match {
+          case Failure(e) =>
+            logger.info("Detected invalid GeoJson data: {}", e.toString)
+            succeed
+          case Success(v) =>
+            v.geoJson.isEmpty should be(false)
+        }
+      }
+    }
+
+  }
+}
+
--- magda-scala-common/src/main/scala/au/csiro/data61/magda/spatial/GeoJsonValidator.scala
+++ magda-scala-common/src/main/scala/au/csiro/data61/magda/spatial/GeoJsonValidator.scala
@@ -8,16 +8,42 @@ import spray.json._
 object GeoJsonValidator {
 
   def validate(geoJson:GeoJson.Typed):GeoJson = geoJson match {
-    case geoJson:GeoJson.Point => Point.fromJson(geoJson.toJson.toString)
-    case geoJson:GeoJson.MultiPoint => MultiPoint.fromJson(geoJson.toJson.toString)
-    case geoJson:GeoJson.LineString => LineString.fromJson(geoJson.toJson.toString)
-    case geoJson:GeoJson.MultiLineString => MultiLineString.fromJson(geoJson.toJson.toString)
-    case geoJson:GeoJson.Polygon => Polygon.fromJson(geoJson.toJson.toString)
-    case geoJson:GeoJson.MultiPolygon => MultiPolygon.fromJson(geoJson.toJson.toString)
-    case geoJson:GeoJson.GeometryCollection[_] => GeometryCollection.fromJson(geoJson.toJson.toString)
-    case geoJson:GeoJson.Feature[_,_] => Feature.fromJson(geoJson.toJson.toString)
-    case geoJson:GeoJson.FeatureCollection[_,_] => FeatureCollection.fromJson(geoJson.toJson.toString)
+    case geoJson:GeoJson.Point =>
+      validateCoordinate(geoJson.coordinates)
+      Point.fromJson(geoJson.toJson.toString)
+    case geoJson:GeoJson.MultiPoint =>
+      validateCoordinates(geoJson.coordinates)
+      MultiPoint.fromJson(geoJson.toJson.toString)
+    case geoJson:GeoJson.LineString =>
+      validateCoordinates(geoJson.coordinates)
+      LineString.fromJson(geoJson.toJson.toString)
+    case geoJson:GeoJson.MultiLineString =>
+      validateCoordinatesCollection(geoJson.coordinates)
+      MultiLineString.fromJson(geoJson.toJson.toString)
+    case geoJson:GeoJson.Polygon =>
+      validateCoordinatesCollection(geoJson.coordinates)
+      Polygon.fromJson(geoJson.toJson.toString)
+    case geoJson:GeoJson.MultiPolygon =>
+      validateCoordinatesCollections(geoJson.coordinates)
+      MultiPolygon.fromJson(geoJson.toJson.toString)
     case _ => throw new Exception(s"Invalid GeoJson object: ${geoJson.getClass.toString}")
   }
+
+  /**
+    * In GeoJSON and WKT, and therefore Elasticsearch, the correct coordinate order is longitude, latitude (X, Y) within coordinate arrays.
+    * This differs from many Geospatial APIs (e.g., Google Maps) that generally use the colloquial latitude, longitude (Y, X).
+    * A Longitude (x) can be -180 to +180 & A latitude (y) can only be in the range of -90 to +90.
+    */
+  def validateCoordinate(c:GeoJson.Coordinate): Boolean = {
+    if(c.x > 180 || c.x < -180) throw new Exception(s"Invalid Longitude (x) range: ${c.x}. Should be within -180 to +180")
+    if(c.y > 90 || c.y < -90) throw new Exception(s"Invalid latitude (x) range: ${c.x}. Should be within -90 to +90")
+    true
+  }
+
+  def validateCoordinates(cs:Seq[GeoJson.Coordinate]): Boolean = !cs.exists{c:GeoJson.Coordinate => !validateCoordinate(c)}
+
+  def validateCoordinatesCollection(col:Seq[Seq[GeoJson.Coordinate]]): Boolean = !col.exists{cs:Seq[GeoJson.Coordinate] => !validateCoordinates(cs)}
+
+  def validateCoordinatesCollections(cols:Seq[Seq[Seq[GeoJson.Coordinate]]]): Boolean = !cols.exists{col:Seq[Seq[GeoJson.Coordinate]] => !validateCoordinatesCollection(col)}
 
 }
