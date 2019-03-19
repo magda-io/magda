e6b77f8b97307d4492303d4fcd5a3eebf9df1cd0
Alex Gilleran <alex.gilleran@nicta.com.au>
2019-03-20 10:57:38 +1100

    Removed lexicon.js from babel compilation.

--- magda-web-client/craco.config.js
+++ magda-web-client/craco.config.js
@@ -8,9 +8,36 @@ module.exports = {
             );
             webpackConfig.module.rules = updatedRules;
 
-            // webpackConfig.plugins = webpackConfig.plugins.filter(plugin => {
-            //     return plugin.constructor.name !== "ForkTSCheckerWebpackPlugin";
-            // });
+            // For some reason the unusual structure of lexicon.js causes babel-web-server to take 20 minutes to compile.
+            // Exclude it
+            webpackConfig.module.rules = webpackConfig.module.rules.map(
+                rule => {
+                    if (rule.oneOf) {
+                        rule.oneOf.forEach(rule => {
+                            if (
+                                rule.options &&
+                                rule.options.presets &&
+                                rule.options.presets.length &&
+                                rule.options.presets[0].some &&
+                                rule.options.presets[0].some(
+                                    preset =>
+                                        preset.indexOf(
+                                            "babel-preset-react-app"
+                                        ) > -1
+                                ) &&
+                                !rule.include
+                            ) {
+                                rule.exclude = [
+                                    rule.exclude,
+                                    /.*\/lexicon.js$/
+                                ];
+                            }
+                        });
+                    }
+
+                    return rule;
+                }
+            );
 
             return webpackConfig;
         }
