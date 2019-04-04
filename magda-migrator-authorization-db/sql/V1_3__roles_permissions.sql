9c9781bb63c9ec16f0c01c33be7b98ddf68a2290
Jacky Jiang <jacky.jiang@data61.csiro.au>
2019-04-04 16:18:28 +1100

    Initial roles permissions table schema

---
+++ magda-migrator-authorization-db/sql/V1_3__roles_permissions.sql
@@ -0,0 +1,134 @@
+CREATE EXTENSION IF NOT EXISTS "pg_trgm";
+
+CREATE SEQUENCE IF NOT EXISTS operations_id_seq;
+
+CREATE TABLE "public"."operations" (
+    "id" int4 NOT NULL DEFAULT nextval('operations_id_seq'::regclass),
+    "name" varchar(250) NOT NULL DEFAULT ''::character varying,
+    "description" text NOT NULL DEFAULT ''::text,
+    "resource_id" int4 NOT NULL,
+    PRIMARY KEY ("id")
+) WITH (
+    OIDS = FALSE
+);
+
+CREATE TABLE "public"."permission_operations" (
+    "id" uuid NOT NULL,
+    "permission_id" uuid NOT NULL,
+    "operation_id" int4 NOT NULL,
+    PRIMARY KEY ("id")
+) WITH (
+    OIDS = FALSE
+);
+
+
+CREATE TABLE "public"."permissions" (
+    "id" uuid NOT NULL,
+    "name" varchar(250) NOT NULL DEFAULT ''::character varying,
+    "resource_id" int4 NOT NULL,
+    "user_ownership_constraint" bool NOT NULL DEFAULT false,
+    "org_unit_ownership_constraint" bool NOT NULL DEFAULT false,
+    "pre_authorised_constraint" bool NOT NULL DEFAULT false,
+    "description" text NOT NULL DEFAULT ''::text,
+    "create_by" uuid,
+    "create_time" timestamptz NOT NULL DEFAULT now(),
+    "edit_by" uuid,
+    "edit_time" timestamptz NOT NULL DEFAULT now(),
+    PRIMARY KEY ("id")
+) WITH (
+    OIDS = FALSE
+);
+
+
+CREATE SEQUENCE IF NOT EXISTS resources_id_seq;
+
+CREATE TABLE "public"."resources" (
+    "id" int4 NOT NULL DEFAULT nextval('resources_id_seq'::regclass),
+    "name" varchar(250) NOT NULL DEFAULT ''::character varying,
+    "description" text NOT NULL DEFAULT ''::text,
+    PRIMARY KEY ("id")
+) WITH (
+    OIDS = FALSE
+);
+
+CREATE TABLE "public"."role_permissions" (
+    "id" uuid NOT NULL,
+    "role_id" uuid NOT NULL,
+    "permission_id" uuid NOT NULL,
+    PRIMARY KEY ("id")
+) WITH (
+    OIDS = FALSE
+);
+
+CREATE TABLE "public"."roles" (
+    "id" uuid NOT NULL,
+    "name" varchar(250) NOT NULL DEFAULT ''::character varying,
+    "description" text NOT NULL DEFAULT ''::text,
+    "create_by" uuid,
+    "create_time" timestamptz NOT NULL DEFAULT now(),
+    "edit_by" uuid,
+    "edit_time" timestamptz NOT NULL DEFAULT now(),
+    PRIMARY KEY ("id")
+) WITH (
+    OIDS = FALSE
+);
+
+CREATE TABLE "public"."user_roles" (
+    "id" uuid NOT NULL,
+    "user_id" uuid NOT NULL,
+    "role_id" uuid NOT NULL,
+    PRIMARY KEY ("id")
+) WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE "public"."operations" ADD FOREIGN KEY ("resource_id") REFERENCES "public"."resources"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."permission_operations" ADD FOREIGN KEY ("operation_id") REFERENCES "public"."operations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."permission_operations" ADD FOREIGN KEY ("permission_id") REFERENCES "public"."permissions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."permissions" ADD FOREIGN KEY ("edit_by") REFERENCES "public"."users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "public"."permissions" ADD FOREIGN KEY ("resource_id") REFERENCES "public"."resources"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "public"."permissions" ADD FOREIGN KEY ("create_by") REFERENCES "public"."users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "public"."role_permissions" ADD FOREIGN KEY ("role_id") REFERENCES "public"."roles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."role_permissions" ADD FOREIGN KEY ("permission_id") REFERENCES "public"."permissions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."roles" ADD FOREIGN KEY ("edit_by") REFERENCES "public"."users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "public"."roles" ADD FOREIGN KEY ("create_by") REFERENCES "public"."users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "public"."user_roles" ADD FOREIGN KEY ("role_id") REFERENCES "public"."roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "public"."user_roles" ADD FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- Add index
+
+CREATE INDEX permission_operations_permission_id_idx ON "public"."permission_operations" USING btree ("permission_id");
+CREATE INDEX permission_operations_operation_id_idx ON "public"."permission_operations" USING btree ("operation_id");
+
+CREATE INDEX resource_id_idx ON "public"."permissions" USING btree (resource_id);
+
+CREATE INDEX role_permissions_permission_id_idx ON "public"."role_permissions" USING btree ("permission_id");
+CREATE INDEX role_permissions_role_id_idx ON "public"."role_permissions" USING btree ("role_id");
+
+CREATE INDEX user_roles_role_id_idx ON "public"."user_roles" USING btree ("role_id");
+CREATE INDEX user_roles_user_id_idx ON "public"."user_roles" USING btree ("user_id");
+
+-- Add index for like with leading wildcard %xx%
+
+CREATE INDEX operations_name_gin_trgm_idx ON "public"."operations" USING gin ("name" gin_trgm_ops);
+CREATE INDEX permissions_name_gin_trgm_idx ON "public"."permissions" USING gin ("name" gin_trgm_ops);
+CREATE INDEX resources_name_gin_trgm_idx ON "public"."resources" USING gin ("name" gin_trgm_ops);
+CREATE INDEX roles_name_gin_trgm_idx ON "public"."roles" USING gin ("name" gin_trgm_ops);
+
+-- Grant clientUser all permissions
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO ${clientUserName};
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO ${clientUserName};
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO ${clientUserName};
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO ${clientUserName};
+
+-- Add default roles
+INSERT INTO "public"."roles" ("id", "name", "description", "create_by", "create_time", "edit_by", "edit_time") VALUES ('00000000-0000-0001-0000-000000000000', 'Anonymous Users', 'Default role for unauthenticated users', NULL, '2019-04-04 04:18:41.21616+00', NULL, '2019-04-04 04:18:41.21616+00'),
+('00000000-0000-0002-0000-000000000000', 'Authenticated Users', 'Default role for authenticated users', NULL, '2019-04-04 04:20:30.728639+00', NULL, '2019-04-04 04:20:30.728639+00'),
+('00000000-0000-0003-0000-000000000000', 'Admin Users', 'Default role for admin users', NULL, '2019-04-04 04:20:54.376504+00', NULL, '2019-04-04 04:20:54.376504+00');
+
+-- Add default roles to the default admin user
+INSERT INTO "public"."user_roles" ("id", "user_id", "role_id") VALUES ('5d73c6e5-bdc9-4cb3-9f06-fada3ae7648e', '00000000-0000-4000-8000-000000000000', '00000000-0000-0002-0000-000000000000'),
+('7b9a62e2-6f23-4cc2-93ff-a2bc31e476b0', '00000000-0000-4000-8000-000000000000', '00000000-0000-0003-0000-000000000000');
+
+
