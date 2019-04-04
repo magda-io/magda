195b8ef1ddc144434821fa4c586c9934fe733ff3
Jacky Jiang <jacky.jiang@data61.csiro.au>
2019-04-04 16:24:52 +1100

    Added dataset resource definitions & default operations

---
+++ magda-migrator-authorization-db/sql/V1_4__default_dataset_resource_operations.sql
@@ -0,0 +1,12 @@
+INSERT INTO "public"."resources" ("id", "name", "description") VALUES ('1', 'Draft Dataset', 'Datasets in draft status'),
+('2', 'Published Dataset', 'Datasets in non-draft (published) status');
+
+INSERT INTO "public"."operations" ("id", "name", "description", "resource_id") VALUES ('1', 'Read Draft Dataset', '', '1'),
+('2', 'Create Draft Dataset', '', '1'),
+('3', 'Update Draft Dataset', '', '1'),
+('4', 'Delete Draft Dataset', '', '1'),
+('5', 'Publish Draft Dataset', '', '1'),
+('6', 'Unpublish Published Dataset', '', '2'),
+('7', 'Update Published Dataset (License Info Only)', '', '2'),
+('8', 'Update Published Dataset (Non-License Info Only)', '', '2'),
+('9', 'Read Publish Dataset', '', '2');
