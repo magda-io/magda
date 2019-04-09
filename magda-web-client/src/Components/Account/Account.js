00abcce000d9c9fb71d9caa5a940dbe493038a43
Jacky Jiang <jacky.jiang@data61.csiro.au>
2019-04-09 15:40:53 +1000

    Display permission details on account page

--- magda-web-client/src/Components/Account/Account.js
+++ magda-web-client/src/Components/Account/Account.js
@@ -24,6 +24,55 @@ class Account extends React.Component {
         this.props.requestAuthProviders();
     }
 
+    renderRoles() {
+        const user = this.props.user;
+        return (
+            <p>
+                Roles:{" "}
+                {user.roles && user.roles.length ? (
+                    <ul>{user.roles.map(role => this.renderRole(role))}</ul>
+                ) : (
+                    "N/A"
+                )}
+            </p>
+        );
+    }
+
+    renderRole(roleItem) {
+        return (
+            <li key={roleItem.id}>
+                {roleItem.name}
+                <div>Role Permissions: </div>
+                {roleItem.permissionIds && roleItem.permissionIds.length ? (
+                    <ul>
+                        {roleItem.permissionIds.map(permissionId =>
+                            this.renderPermissionById(permissionId)
+                        )}
+                    </ul>
+                ) : (
+                    "N/A"
+                )}
+            </li>
+        );
+    }
+
+    getPermissionById(permissionId) {
+        const user = this.props.user;
+        if (!user.permissions || !user.permissions.length) return null;
+        for (let i = 0; i < user.permissions.length; i++) {
+            if (user.permissions[i].id === permissionId)
+                return user.permissions[i];
+        }
+        return null;
+    }
+
+    renderPermissionById(permissionId) {
+        const permission = this.getPermissionById(permissionId);
+        return (
+            <li key={permissionId}>{permission ? permission.name : "N/A"}</li>
+        );
+    }
+
     render() {
         const pageTitle = this.props.user.id ? "Account" : "Sign In";
         return (
@@ -53,14 +102,7 @@ class Account extends React.Component {
                             <h1>Account</h1>
                             <p>Display Name: {this.props.user.displayName}</p>
                             <p>Email: {this.props.user.email}</p>
-                            <p>
-                                Role:{" "}
-                                {this.props.user.roles.length
-                                    ? this.props.user.roles
-                                          .map(item => item.name)
-                                          .join("; ")
-                                    : "N/A"}
-                            </p>
+                            {this.renderRoles()}
                         </div>
                     )}
                 </div>
