bfabe9dcaa0d9070924ecf7e17aad8b0dd5c19de
Mike Wu <mike.wu@data61.csiro.au>
2019-04-10 14:20:53 +1000

    Added a missing file.

---
+++ magda-typescript-common/src/registry/TenantConsts.ts
@@ -0,0 +1,4 @@
+// TODO: Investigate how to use swagger codegen to automatically generate these constants.
+// These constants are the same as defined in the scala model Registry.
+export const MAGDA_TENANT_ID_HEADER = "X-Magda-TenantId";
+export const MAGDA_ADMIN_PORTAL_ID = 0;
