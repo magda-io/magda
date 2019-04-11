313a670f4d16e75788fac101a5528bd075f692f2
Jacky Jiang <t83714@gmail.com>
2019-04-11 13:17:12 +1000

    error processing

--- magda-authorization-api/src/createOpaRouter.ts
+++ magda-authorization-api/src/createOpaRouter.ts
@@ -56,6 +56,64 @@ export default function createOpaRouter(options: OpaRouterOptions): Router {
 
     router.use(bodyParser.json({ type: "application/json" }));
 
+    function normaliseInputField(reqData: any) {
+        if (reqData.input && typeof reqData.input === "object") return;
+        reqData.input = {};
+    }
+
+    function processQueryParams(req: express.Request, reqData: any) {
+        if (!req.query || !Object.keys(req.query).length) return null;
+
+        const { q, input, unknowns, ...otherQueryParams } = req.query;
+
+        if (q && typeof q === "string" && q.length) {
+            reqData["query"] = q;
+        }
+
+        if (input && typeof input === "string" && input.length) {
+            // --- supply whole input object as JSON string through `input` query param
+            try {
+                const inputData = JSON.parse(input);
+                reqData.input = { ...reqData.input, ...inputData };
+            } catch (e) {}
+        } else {
+            /**
+             * Supply input as JSON path notion.
+             * e.g. input.companyName=ABC&input.people.0.name=Joe will be converted to
+             * input = {
+             *   "companyName": "ABC",
+             *   "people": [{
+             *      "name": Joe
+             *   }]
+             * }
+             */
+            const data: any = {};
+            Object.keys(req.query)
+                .filter(key => key.indexOf("input.") === 0)
+                .forEach(key => {
+                    if (key.replace("input.", "").trim() === "") return;
+                    objectPath.set(data, key, req.query[key]);
+                });
+            if (data.input && Object.keys(data.input).length) {
+                reqData.input = { ...reqData.input, ...data.input };
+            }
+        }
+
+        if (unknowns) {
+            // --- could be a string or array of string
+            if (_.isString(unknowns) && unknowns.length) {
+                reqData["unknowns"] = [unknowns];
+            } else if (_.isArray(unknowns) && unknowns.length) {
+                reqData["unknowns"] = unknowns;
+            }
+        }
+
+        if (!otherQueryParams || !Object.keys(otherQueryParams).length)
+            return null;
+
+        return otherQueryParams;
+    }
+
     async function appendUserInfoToInput(
         req: express.Request,
         res: express.Response
@@ -69,8 +127,12 @@ export default function createOpaRouter(options: OpaRouterOptions): Router {
         }
 
         let reqData: any = {};
-        let reqQueryParams: any = {};
         const contentType = req.get("content-type");
+
+        const reqOpts: request.CoreOptions = {
+            method: "post",
+            json: reqData
+        };
 
         // --- merge userInfo into possible income input data via POST
         if (
@@ -81,79 +143,37 @@ export default function createOpaRouter(options: OpaRouterOptions): Router {
             reqData = req.body;
         }
 
-        // --- process parameters from quert string
-        if (req.query && Object.keys(req.query).length) {
-            const { q, input, unknowns, ...otherQueryParams } = req.query;
+        try {
+            const reqQueryParams: any = processQueryParams(req, reqData);
 
-            if (q && typeof q === "string" && q.length) {
-                reqData["query"] = q;
+            if (reqQueryParams) {
+                // --- if still extra query params left, pass to opa
+                reqOpts.qs = reqQueryParams;
             }
-
-            if (input && typeof input === "string" && input.length) {
-                // --- supply whole input object as JSON string through `input` query param
-                try {
-                    const inputData = JSON.parse(input);
-                    if (!reqData.input) {
-                        reqData.input = inputData;
-                    } else {
-                        reqData.input = { ...reqData.input, ...inputData };
-                    }
-                } catch (e) {}
-            } else {
-                /**
-                 * Supply input as JSON path notion.
-                 * e.g. input.companyName=ABC&input.people.0.name=Joe will be converted to
-                 * input = {
-                 *   "companyName": "ABC",
-                 *   "people": [{
-                 *      "name": Joe
-                 *   }]
-                 * }
-                 */
-                const data = {};
-                Object.keys(req.query)
-                    .filter(key => key.indexOf("input.") === 0)
-                    .forEach(key => {
-                        if (key.replace("input.", "").trim() === "") return;
-                        objectPath.set(data, key, req.query[key]);
-                    });
-            }
-
-            if (unknowns) {
-                // --- could be a string or array of string
-                if (_.isString(unknowns) && unknowns.length) {
-                    reqData["unknowns"] = [unknowns];
-                } else if (_.isArray(unknowns) && unknowns.length) {
-                    reqData["unknowns"] = unknowns;
-                }
-            }
-
-            reqQueryParams = otherQueryParams;
+        } catch (e) {
+            throw new Error(`Failed to process query parameters: ${e}`);
         }
 
-        if (!reqData.input) {
-            reqData.input = {};
-        }
+        normaliseInputField(reqData);
 
         reqData.input.user = userInfo;
-
-        const reqOpts: request.CoreOptions = {
-            method: "post",
-            json: reqData
-        };
-
-        if (Object.keys(reqQueryParams).length) {
-            reqOpts.qs = reqQueryParams;
-        }
 
         request(`${opaUrl}${req.path}`, reqOpts).pipe(res);
     }
 
+    async function proxyRequest(req: express.Request, res: express.Response) {
+        try {
+            await appendUserInfoToInput(req, res);
+        } catch (e) {
+            res.status(500).send(`Failed to proxy OPA request: ${e}`);
+        }
+    }
+
     opaRoutes.map(route => {
         if (route.method == "post") {
-            router.post(route.path, appendUserInfoToInput);
+            router.post(route.path, proxyRequest);
         } else {
-            router.get(route.path, appendUserInfoToInput);
+            router.get(route.path, proxyRequest);
         }
     });
 
