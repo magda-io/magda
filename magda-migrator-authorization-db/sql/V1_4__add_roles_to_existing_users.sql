fc69a8e85fced429c26d7fcd1e71cc95197a2585
Jacky Jiang <jacky.jiang@data61.csiro.au>
2019-05-13 13:12:56 +1000

    fixed migration sql

--- magda-migrator-authorization-db/sql/V1_4__add_roles_to_existing_users.sql
+++ magda-migrator-authorization-db/sql/V1_4__add_roles_to_existing_users.sql
@@ -1,13 +1,19 @@
--- Add default roles to existing users
+-- Add default Authenticated Users role to existing users
 INSERT INTO user_roles 
 (id, user_id, role_id)
 (
-	SELECT uuid_generate_v4() as id, u.id as user_id, 
-	(CASE 
-		WHEN u."isAdmin" = true THEN '00000000-0000-0003-0000-000000000000'::uuid
-		ELSE '00000000-0000-0002-0000-000000000000'::uuid
-	END) as role_id
+	SELECT uuid_generate_v4() as id, u.id as user_id, '00000000-0000-0002-0000-000000000000'::uuid as role_id
 	FROM users AS u
 	LEFT JOIN user_roles AS r ON r.user_id = u.id
-	WHERE r.id is NULL
+	WHERE u.id NOT IN (SELECT user_roles.user_id FROM user_roles WHERE user_roles.role_id = '00000000-0000-0002-0000-000000000000'::uuid) 
+);
+
+-- Add default Authenticated Users role to existing admin users
+INSERT INTO user_roles 
+(id, user_id, role_id)
+(
+	SELECT uuid_generate_v4() as id, u.id as user_id, '00000000-0000-0003-0000-000000000000'::uuid as role_id
+	FROM users AS u
+	LEFT JOIN user_roles AS r ON r.user_id = u.id
+	WHERE u."isAdmin" = true AND u.id NOT IN (SELECT user_roles.user_id FROM user_roles WHERE user_roles.role_id = '00000000-0000-0003-0000-000000000000'::uuid) 
 )
