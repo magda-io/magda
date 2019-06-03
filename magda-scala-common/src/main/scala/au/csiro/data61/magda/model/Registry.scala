7565d0f4b3d10a263f4cb566d4f979bca265b24b
Mike Wu <mike.wu@data61.csiro.au>
2019-06-03 10:54:47 +1000

    Added jsdoc/scaladoc.

--- magda-scala-common/src/main/scala/au/csiro/data61/magda/model/Registry.scala
+++ magda-scala-common/src/main/scala/au/csiro/data61/magda/model/Registry.scala
@@ -15,9 +15,35 @@ import scala.annotation.meta.field
 import scala.util.{Failure, Success, Try}
 
 object Registry {
-  val MAGDA_TENANT_ID_HEADER: String = "X-Magda-Tenant-Id"
+  /**
+  * == MAGDA_ADMIN_PORTAL_ID and MAGDA_SYSTEM_ID play different roles. ==
+  * 
+  * === 1. Role of MAGDA_ADMIN_PORTAL_ID in single tenant mode===
+  * Tenant IDs are foreign keys in many tables. When migrating the existing DB, all existing entries in table aspects,
+  * records, recordaspects and events must have non-null values in field of tenantId. They are all set to
+  * MAGDA_ADMIN_PORTAL_ID. MAGDA_ADMIN_PORTAL_ID is the tenant id in single tenant deployment (default deployment).
+  * <p/>
+  * === 2. Role of MAGDA_ADMIN_PORTAL_ID in multi-tenant mode ===
+  * It manages tenants only. It creates, enables or disable tenants.
+  * <p/>
+  * === 3. Role of MAGDA_SYSTEM_ID ===
+  * It is not important in single tenant mode but necessary in multi-tenant mode. It is mainly used to perform global
+  * operations. For example, when re-indexing datasets, it will retrieve records and record aspects of all tenants from
+  * DB. Many minions also take this role, e.g. a broken link minion.
+ */
   val MAGDA_ADMIN_PORTAL_ID: BigInt = 0
+
+  /**
+    * Request with this tenant ID can perform global operations on registry.
+    *
+    * @see [[au.csiro.data61.magda.model.Registry.MAGDA_ADMIN_PORTAL_ID]]
+    */
   val MAGDA_SYSTEM_ID: BigInt = -1
+
+  /**
+    * The header name of tenant ID.
+    */
+  val MAGDA_TENANT_ID_HEADER: String = "X-Magda-Tenant-Id"
 
   @ApiModel(description = "A type of aspect in the registry, unique for a tenant.")
   case class AspectDefinition(
