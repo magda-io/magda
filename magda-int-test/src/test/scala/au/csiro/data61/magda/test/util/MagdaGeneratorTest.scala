c55ca016ff0d35b4e7efbea55758c2171090d9a7
Mike Wu <mike.wu@data61.csiro.au>
2019-06-19 11:40:48 +1000

    Testing gitlab cache.

--- magda-int-test/src/test/scala/au/csiro/data61/magda/test/util/MagdaGeneratorTest.scala
+++ magda-int-test/src/test/scala/au/csiro/data61/magda/test/util/MagdaGeneratorTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
 trait MagdaGeneratorTest extends GeneratorDrivenPropertyChecks {
   val processors = Math.max(2, if (ContinuousIntegration.isCi) Runtime.getRuntime.availableProcessors - 2 else Runtime.getRuntime.availableProcessors - 1)
-  val minSuccessful = if (ContinuousIntegration.isCi) 1 else 50
+  val minSuccessful = if (ContinuousIntegration.isCi) 1 else 1
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
-    PropertyCheckConfiguration(workers = PosInt.from(processors).get, sizeRange = 50, minSuccessful = PosInt.from(minSuccessful).get)
+    PropertyCheckConfiguration(workers = PosInt.from(processors).get, sizeRange = 2, minSuccessful = PosInt.from(minSuccessful).get)
 }
