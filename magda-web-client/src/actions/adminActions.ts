77d5e0fe6412ac691f8da9635311dc1fbdc084ec
Mike Wu <mike.wu@data61.csiro.au>
2019-06-27 14:57:13 +1000

    Removed extra slash in the path.

--- magda-web-client/src/actions/adminActions.ts
+++ magda-web-client/src/actions/adminActions.ts
@@ -2,23 +2,23 @@ import { config } from "config";
 import request from "helpers/request";
 
 export async function listConnectors() {
-    return request("GET", `${config.adminApiUrl}/connectors`);
+    return request("GET", `${config.adminApiUrl}connectors`);
 }
 
 export async function startConnector(connectorId) {
     return request(
         "POST",
-        `${config.adminApiUrl}/connectors/${connectorId}/start`
+        `${config.adminApiUrl}connectors${connectorId}/start`
     );
 }
 
 export async function stopConnector(connectorId) {
     return request(
         "POST",
-        `${config.adminApiUrl}/connectors/${connectorId}/stop`
+        `${config.adminApiUrl}connectors${connectorId}/stop`
     );
 }
 
 export async function deleteConnector(connectorId) {
-    return request("DELETE", `${config.adminApiUrl}/connectors/${connectorId}`);
+    return request("DELETE", `${config.adminApiUrl}connectors${connectorId}`);
 }
