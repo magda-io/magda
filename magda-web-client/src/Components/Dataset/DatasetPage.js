869916c1487bafd95d10c81e9cefb0188106e971
Jacky Jiang <jacky.jiang@data61.csiro.au>
2019-09-12 14:51:08 +1000

    Fixed "NOT SET" appears on the dataset page for non-admins

--- magda-web-client/src/Components/Dataset/DatasetPage.js
+++ magda-web-client/src/Components/Dataset/DatasetPage.js
@@ -569,13 +569,15 @@ class RecordHandler extends React.Component {
                                         onChange={datasetChange("contactPoint")}
                                         editor={multilineTextEditor}
                                     >
-                                        {this.props.dataset.contactPoint && (
+                                        {this.props.dataset.contactPoint ? (
                                             <ContactPoint
                                                 contactPoint={
                                                     this.props.dataset
                                                         .contactPoint
                                                 }
                                             />
+                                        ) : (
+                                            <></>
                                         )}
                                     </ToggleEditor>
                                     <ToggleEditor
