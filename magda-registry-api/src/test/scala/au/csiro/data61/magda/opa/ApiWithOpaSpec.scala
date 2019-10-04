b6dfa479b30b20517d008141fbd18ef8b5da84a0
Mike Wu <mike.wu@data61.csiro.au>
2019-10-04 17:48:32 +1000

    Resolve merge conflicts.

--- magda-registry-api/src/test/scala/au/csiro/data61/magda/opa/ApiWithOpaSpec.scala
+++ magda-registry-api/src/test/scala/au/csiro/data61/magda/opa/ApiWithOpaSpec.scala
@@ -97,7 +97,10 @@ abstract class ApiWithOpaSpec
 
     val theRecordPolicyId = recordPolicyId
 
-    if (theRecordPolicyId.endsWith("esri_owner_groups")) {
+    if (theRecordPolicyId.endsWith("esri_owner_groups") && !userId.equals(
+          adminUser
+        )) {
+
       /**
         * The current Java JWT library is not capable of creating custom claims that are json objects.
         * The typescript library comes to help. These jwt tokens are created by magda-typescript-common/src/test/session/buildJwtForRegistryEsriOpaTest.ts.
@@ -107,7 +110,7 @@ abstract class ApiWithOpaSpec
         *     cd magda-typescript-common
         *     yarn build
         *     yarn create_esri_jwt
-       */
+        */
       val jwtToken =
         if (userId.equals("00000000-0000-1000-0000-000000000000"))
           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIwMDAwMDAwMC0wMDAwLTEwMDAtMDAwMC0wMDAwMDAwMDAwMDAiLCJzZXNzaW9uIjp7InNlc3Npb24iOnsiZXNyaUdyb3VwcyI6WyJEZXAuIEEiLCJCcmFuY2ggQSwgRGVwLiBBIiwiQnJhbmNoIEIsIERlcC4gQSIsIlNlY3Rpb24gQywgQnJhbmNoIEIsIERlcC4gQSJdfX0sImlhdCI6MTU2ODI4ODgzNn0.V8VzOqKKngc2Fykuy7C_oBjvJRhJeosLnN8a066ffuo"
@@ -180,13 +183,15 @@ abstract class ApiWithOpaSpec
     *   +--------------------------------------------------------------+
     *
     */
-  val userId0 = "00000000-0000-1000-0000-000000000000" // admin user
+  val adminUser = "00000000-0000-1000-9999-000000000000"
+  val userId0 = "00000000-0000-1000-0000-000000000000"
   val userId1 = "00000000-0000-1000-0001-000000000000"
   val userId2 = "00000000-0000-1000-0002-000000000000"
   val userId3 = "00000000-0000-1000-0003-000000000000"
   val anonymous = "anonymous"
 
   val userIdsAndExpectedRecordIdIndexesWithoutLink = List(
+    (adminUser, List(0, 1, 2, 3, 4, 5)),
     (userId0, List(0, 1, 2, 3, 4, 5)),
     (userId1, List(1, 4)),
     (userId2, List(2, 3, 4, 5)),
@@ -195,6 +200,7 @@ abstract class ApiWithOpaSpec
   )
 
   val userIdsAndExpectedRecordIdIndexesWithSingleLink = List(
+    (adminUser, List(2)),
     (userId0, List(2)),
     (userId1, List()),
     (userId2, List(2)),
@@ -327,12 +333,12 @@ abstract class ApiWithOpaSpec
 
     aspectDefs.map(aspectDef => {
       Get(s"/v0/aspects/${aspectDef.id}") ~> addTenantIdHeader(TENANT_0) ~> addJwtToken(
-        userId0,
+        adminUser,
         ""
       ) ~> param.api(Full).routes ~> check {
         if (status == StatusCodes.NotFound) {
           Post(s"/v0/aspects", aspectDef) ~> addTenantIdHeader(TENANT_0) ~> addJwtToken(
-            userId0,
+            adminUser,
             ""
           ) ~> param.api(Full).routes ~> check {
             status shouldBe StatusCodes.OK
@@ -407,12 +413,12 @@ abstract class ApiWithOpaSpec
 
     testRecords.map(record => {
       Get(s"/v0/records/${record.id}") ~> addTenantIdHeader(TENANT_0) ~> addJwtToken(
-        userId0,
+        adminUser,
         ""
       ) ~> param.api(Full).routes ~> check {
         if (status == StatusCodes.NotFound) {
           Post(s"/v0/records", record) ~> addTenantIdHeader(TENANT_0) ~> addJwtToken(
-            userId0,
+            adminUser,
             ""
           ) ~> param.api(Full).routes ~> check {
             status shouldBe StatusCodes.OK
@@ -426,18 +432,23 @@ abstract class ApiWithOpaSpec
 
   lazy val singleLinkRecordIdMapDereferenceIsFalse
       : Map[(String, String), String] =
-    Map((userId0, "record-2") -> "record-1", (userId2, "record-2") -> "")
+    Map(
+      (adminUser, "record-2") -> "record-1",
+      (userId0, "record-2") -> "record-1",
+      (userId2, "record-2") -> ""
+    )
 
   lazy val singleLinkRecordIdMapDereferenceIsTrue
       : Map[(String, String), JsObject] =
     Map(
+      (adminUser, "record-2") -> testRecords(1).toJson.asJsObject,
       (userId0, "record-2") -> testRecords(1).toJson.asJsObject,
       (userId2, "record-2") -> JsObject.empty
     )
 
   GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(
-    enabled = false,
-    singleLineMode = true,
+    enabled = true,
+    singleLineMode = false,
     logLevel = 'debug
   )
 
