286e13bfd79aba6099d8483da8a097ad8e7a59f1
Sajid Ibne Anower <sajidanower23@gmail.com>
2019-10-10 10:57:58 +1100

    Use tables in ToolTips

--- magda-web-client/src/Components/Dataset/Add/ToolTip.tsx
+++ magda-web-client/src/Components/Dataset/Add/ToolTip.tsx
@@ -7,11 +7,21 @@ import "./ToolTip.scss";
 export default function ToolTip(props: any) {
     return (
         <p className="tooltip-root">
-            <img
-                src={props.icon ? props.icon : LightBulbIcon}
-                className="tooltip-image"
-            />
-            <span>{props.children}</span>
+            <table>
+                <tr>
+                    <td>
+                        <img
+                            src={props.icon ? props.icon : LightBulbIcon}
+                            className="tooltip-image"
+                        />
+                    </td>
+                    <td>
+                        <span className="tooltip-content">
+                            {props.children}
+                        </span>
+                    </td>
+                </tr>
+            </table>
         </p>
     );
 }
