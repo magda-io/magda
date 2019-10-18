cdb8a6b7a242acaf2eb7fa311cdd08604cb071e0
Sajid Ibne Anower <sajidanower23@gmail.com>
2019-10-18 16:19:34 +1100

    Revert DEV_FEATURE_FLAGS to it's original form

--- magda-web-client/src/config.ts
+++ magda-web-client/src/config.ts
@@ -17,7 +17,7 @@ const fallbackApiHost = "https://dev.magda.io/";
 
 const DEV_FEATURE_FLAGS = {
     cataloguing: true,
-    previewAddDataset: false
+    previewAddDataset: true
 };
 
 const homePageConfig: {
