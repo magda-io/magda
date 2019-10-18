09c458f59adfc75b46af2dc1e2ce27e676b5c643
Kevin Ring <kevin@kotachrome.com>
2019-10-18 17:56:39 +1100

    First version of Terria minion.

---
+++ magda-minion-terria/src/index.ts
@@ -0,0 +1,39 @@
+/// <reference types="../../terriajs/lib/ThirdParty/terriajs-cesium" />
+import { XMLHttpRequest } from "xmlhttprequest-ssl";
+const anyGlobal: any = global;
+anyGlobal.window = global;
+anyGlobal.XMLHttpRequest = XMLHttpRequest;
+
+require("amd-loader");
+import commonYargs from "@magda/minion-framework/dist/commonYargs";
+import minion from "@magda/minion-framework/dist/index";
+import onRecordFound from "./onRecordFound";
+
+const ID = "minion-terria";
+const argv = commonYargs(6501, "http://localhost:6501");
+
+const aspectDefinition = {
+    id: "terria",
+    name: "Terria Catalog Details",
+    jsonSchema: require("@magda/registry-aspects/terria.schema.json")
+};
+
+minion({
+    argv,
+    aspects: [],
+    async: true,
+    id: ID,
+    onRecordFound,
+    optionalAspects: [
+        "terria",
+        "group",
+        "dcat-dataset-strings",
+        "dcat-distribution-strings",
+        "dataset-distributions",
+        "dataset-format"
+    ],
+    writeAspectDefs: [aspectDefinition]
+}).catch((e: Error) => {
+    console.error("Error: " + e.message, e);
+    process.exit(1);
+});
---
+++ magda-minion-terria/src/onRecordFound.ts
@@ -0,0 +1,71 @@
+import { Record } from "@magda/typescript-common/dist/generated/registry/api";
+import Registry from "@magda/typescript-common/dist/registry/AuthorizedRegistryClient";
+import MagdaReference from "terriajs/lib/Models/MagdaReference";
+import Terria from "terriajs/lib/Models/Terria";
+import CommonStrata from "terriajs/lib/Models/CommonStrata";
+import { JsonObject } from "terriajs/lib/Core/Json";
+import registerCatalogMembers from "terriajs/lib/Models/registerCatalogMembers";
+import saveStratumToJson from "terriajs/lib/Models/saveStratumToJson";
+
+registerCatalogMembers();
+
+const terria = new Terria();
+
+export default async function onRecordFound(
+    record: Record,
+    registry: Registry
+): Promise<void> {
+    const terriaAspect = record.aspects.terria;
+
+    // We don't want the Terria aspect to influence how the record is intepreted.
+    record.aspects.terria = undefined;
+
+    const reference = new MagdaReference(record.id, terria);
+    reference.setTrait(
+        CommonStrata.definition,
+        "magdaRecord",
+        (record as unknown) as JsonObject
+    );
+
+    await reference.loadReference().catch(() => {});
+
+    if (reference.target) {
+        const underride = reference.target.strata.get("underride");
+        if (underride) {
+            const json = saveStratumToJson(reference.target.traits, underride);
+            if (terriaAspect) {
+                // Patch the existing terria aspect
+                await registry
+                    .patchRecordAspect(
+                        record.id,
+                        "terria",
+                        [
+                            {
+                                op: terriaAspect.underride ? "replace" : "add",
+                                path: "/underride",
+                                value: json
+                            }
+                        ],
+                        record.tenantId
+                    )
+                    .catch(e => {
+                        console.error(e);
+                    });
+            } else {
+                // Brand new terria aspect
+                await registry
+                    .putRecordAspect(
+                        record.id,
+                        "terria",
+                        {
+                            underride: json
+                        },
+                        record.tenantId
+                    )
+                    .catch(e => {
+                        console.error(e);
+                    });
+            }
+        }
+    }
+}
