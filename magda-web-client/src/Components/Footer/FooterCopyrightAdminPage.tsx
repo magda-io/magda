f61151daff52a1d2beed88986bf9163ced394f24
Sajid Ibne Anower <sajidanower23@gmail.com>
2019-11-04 16:01:05 +1100

    Use empty image and htmlContent by default

--- magda-web-client/src/Components/Footer/FooterCopyrightAdminPage.tsx
+++ magda-web-client/src/Components/Footer/FooterCopyrightAdminPage.tsx
@@ -20,10 +20,9 @@ class StoriesAdminPage extends Component<any, any> {
                 pattern={`${pathPrefix}/*`}
                 newContent={{
                     order: 999,
-                    logoSrc:
-                        "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAKCAYAAAD2Fg1xAAAAI0lEQVR42u3PAREAMAgAoTe50bceHjRgXm0HjIiIiIiIiEh9iR0O99ylthgAAAAASUVORK5CYII=",
+                    logoSrc: "",
                     href: "http://magda.io",
-                    htmlContent: "New Copyright"
+                    htmlContent: ""
                 }}
                 hasOrder={true}
                 edit={this.edit.bind(this)}
