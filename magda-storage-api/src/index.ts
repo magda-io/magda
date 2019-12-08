e280c5a8a2f7157ed1bd0799492e49311bb93270
Sajid Ibne Anower <sajidanower23@gmail.com>
2019-12-09 09:53:42 +1100

    Remove addJwtSecretFromEnvVar

--- magda-storage-api/src/index.ts
+++ magda-storage-api/src/index.ts
@@ -1,57 +1,52 @@
-import addJwtSecretFromEnvVar from "@magda/typescript-common/dist/session/addJwtSecretFromEnvVar";
 import * as express from "express";
 import * as yargs from "yargs";
 import createApiRouter from "./createApiRouter";
 import MagdaMinioClient from "./MagdaMinioClient";
 // import GoogleCloudStorageClient from "./GoogleCloudStorageClient";
 
-const argv = addJwtSecretFromEnvVar(
-    yargs
-        .config()
-        .help()
-        .option("listenPort", {
-            describe:
-                "The TCP/IP port on which the datastore-api should listen.",
-            type: "number",
-            default: 6120
-        })
-        .option("bucket", {
-            describe:
-                "The Google Cloud Storage bucket from which to serve data.",
-            type: "string",
-            demand: true
-        })
-        .option("gcsKeyFile", {
-            describe:
-                "The JSON key file to use to access the Google Cloud Storage bucket.",
-            type: "string"
-        })
-        .option("minioAccessKey", {
-            describe: "The access key to your minio server.",
-            type: "string",
-            demand: true
-        })
-        .option("minioSecretKey", {
-            describe: "The secret key to your minio server.",
-            type: "string",
-            demand: true
-        })
-        .option("minioEnableSSL", {
-            describe: "Whether or not to use https over http. Defaults to true",
-            type: "boolean",
-            default: true
-        })
-        .option("minioServerHost", {
-            describe: "Host where MinIO server is running.",
-            type: "string",
-            default: "localhost"
-        })
-        .option("minioServerPort", {
-            describe: "Port where MinIO server is running.",
-            type: "number",
-            default: 9000
-        }).argv
-);
+const argv = yargs
+    .config()
+    .help()
+    .option("listenPort", {
+        describe: "The TCP/IP port on which the datastore-api should listen.",
+        type: "number",
+        default: 6120
+    })
+    .option("bucket", {
+        describe: "The Google Cloud Storage bucket from which to serve data.",
+        type: "string",
+        demand: true
+    })
+    .option("gcsKeyFile", {
+        describe:
+            "The JSON key file to use to access the Google Cloud Storage bucket.",
+        type: "string"
+    })
+    .option("minioAccessKey", {
+        describe: "The access key to your minio server.",
+        type: "string",
+        demand: true
+    })
+    .option("minioSecretKey", {
+        describe: "The secret key to your minio server.",
+        type: "string",
+        demand: true
+    })
+    .option("minioEnableSSL", {
+        describe: "Whether or not to use https over http. Defaults to true",
+        type: "boolean",
+        default: true
+    })
+    .option("minioServerHost", {
+        describe: "Host where MinIO server is running.",
+        type: "string",
+        default: "localhost"
+    })
+    .option("minioServerPort", {
+        describe: "Port where MinIO server is running.",
+        type: "number",
+        default: 9000
+    }).argv;
 
 var app = express();
 
