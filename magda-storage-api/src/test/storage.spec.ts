d12bdda1a2985c8ba439b3bc5b7f56c06bd20dc9
Sajid Ibne Anower <sajidanower23@gmail.com>
2019-12-11 16:20:54 +1100

    Add download and upload tests

--- magda-storage-api/src/test/storage.spec.ts
+++ magda-storage-api/src/test/storage.spec.ts
@@ -2,14 +2,14 @@ import {} from "mocha";
 import * as sinon from "sinon";
 import * as express from "express";
 import * as _ from "lodash";
-import * as supertest from "supertest";
+import * as request from "supertest";
 
 import createApiRouter from "../createApiRouter";
 import MagdaMinioClient from "../MagdaMinioClient";
 
 console.log(createApiRouter);
 
-describe("proxying", () => {
+describe("Storage API tests", () => {
     let app: express.Application;
 
     beforeEach(() => {
@@ -21,8 +21,8 @@ describe("proxying", () => {
                     endPoint: "localhost",
                     port: 9000,
                     useSSL: false,
-                    accessKey: "",
-                    secretKey: "",
+                    accessKey: process.env["MINIO_ACCESS_KEY"],
+                    secretKey: process.env["MINIO_SECRET_KEY"],
                     bucket: "magda-test-1"
                 })
             })
@@ -37,12 +37,31 @@ describe("proxying", () => {
 
     describe("Upload", () => {
         it("Uploading a simple file", () => {
-            return supertest(app)
+            return request(app)
+                .put("/v0/test-file")
+                .set("Accept", "application/json")
+                .set("Content-Type", "text/plain")
+                .send("LALALALALALALALALA")
+                .expect(200);
+        });
+    });
+
+    describe("Download", () => {
+        it("Uploading and then downloading the simple file", function() {
+            return request(app)
                 .put("/v0/test-file")
                 .set("Accept", "application/json")
                 .set("Content-Type", "text/csv")
-                .send("LALALALALALALALALA")
-                .expect(200);
+                .send("Testing download")
+                .expect(200)
+                .then(_res => {
+                    return request(app)
+                        .get("/v0/test-file")
+                        .set("Accept", "application/json")
+                        .set("Accept", "text/plain")
+                        .expect(200)
+                        .expect("Testing download");
+                });
         });
     });
 });
