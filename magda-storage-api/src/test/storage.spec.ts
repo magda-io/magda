9818c4c930e5fde7d25fd99f09e3ec2da73dcea3
Sajid Ibne Anower <sajidanower23@gmail.com>
2019-12-12 12:59:28 +1100

    Add creation and removal of buckets in tests

--- magda-storage-api/src/test/storage.spec.ts
+++ magda-storage-api/src/test/storage.spec.ts
@@ -7,24 +7,46 @@ import * as request from "supertest";
 import createApiRouter from "../createApiRouter";
 import MagdaMinioClient from "../MagdaMinioClient";
 
+const Minio = require("minio");
+
 console.log(createApiRouter);
 
 describe("Storage API tests", () => {
     let app: express.Application;
+    const minioClientOpts = {
+        endPoint: process.env["MINIO_HOST"],
+        port: Number(process.env["MINIO_PORT"]),
+        useSSL: false,
+        accessKey: process.env["MINIO_ACCESS_KEY"],
+        secretKey: process.env["MINIO_SECRET_KEY"],
+        bucket: "magda-test-1"
+    };
+    const minioClient = new Minio.Client(minioClientOpts);
+
+    before(() => {
+        minioClient.makeBucket(minioClientOpts.bucket, function(err: Error) {
+            if (err) {
+                return console.log("Error creating bucket.", err);
+            }
+            console.log('Bucket created successfully in "us-east-1".');
+        });
+    });
+
+    after(() => {
+        minioClient.removeBucket(minioClientOpts.bucket, function(err: Error) {
+            if (err) {
+                return console.log("unable to remove bucket: ", err);
+            }
+            console.log("Bucket removed successfully.");
+        });
+    });
 
     beforeEach(() => {
         app = express();
         app.use(
             "/v0",
             createApiRouter({
-                objectStoreClient: new MagdaMinioClient({
-                    endPoint: "localhost",
-                    port: 9000,
-                    useSSL: false,
-                    accessKey: process.env["MINIO_ACCESS_KEY"],
-                    secretKey: process.env["MINIO_SECRET_KEY"],
-                    bucket: "magda-test-1"
-                })
+                objectStoreClient: new MagdaMinioClient(minioClientOpts)
             })
         );
     });
