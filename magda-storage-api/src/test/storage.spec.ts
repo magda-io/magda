8b11abd8433ab7854ba2e5fa1f3991bb492d945f
Sajid Ibne Anower <sajidanower23@gmail.com>
2019-12-19 10:46:33 +1100

    Add tests for csv and json files

--- magda-storage-api/src/test/storage.spec.ts
+++ magda-storage-api/src/test/storage.spec.ts
@@ -64,7 +64,7 @@ describe("Storage API tests", () => {
             return request(app)
                 .put("/v0/download-test-file-1")
                 .set("Accept", "application/json")
-                .set("Content-Type", "text/csv")
+                .set("Content-Type", "text/plain")
                 .send("Testing download")
                 .expect(200)
                 .then(_res => {
@@ -73,7 +73,8 @@ describe("Storage API tests", () => {
                         .set("Accept", "application/json")
                         .set("Accept", "text/plain")
                         .expect(200)
-                        .expect("Testing download");
+                        .expect("Testing download")
+                        .expect("Content-Type", "text/plain");
                 });
         });
 
@@ -90,9 +91,11 @@ describe("Storage API tests", () => {
                         .set("Accept", "application/json")
                         .set("Accept", "text/plain")
                         .expect(200)
-                        .expect("");
+                        .expect("")
+                        .expect("Content-Type", "text/plain");
                 });
         });
+
         it("Binary content", function() {
             const img: Buffer = fs.readFileSync("src/test/test_image.jpg");
             return request(app)
@@ -106,7 +109,46 @@ describe("Storage API tests", () => {
                         .get("/v0/binary-content")
                         .set("Accept", "image/jpg")
                         .expect(200)
-                        .expect(img);
+                        .expect(img)
+                        .expect("Content-Type", "image/jpg");
+                });
+        });
+        it("CSV File", function() {
+            const csvContent = fs.readFileSync(
+                "src/test/test_csv_1.csv",
+                "utf-8"
+            );
+            return request(app)
+                .put("/v0/test-csv-1")
+                .set("Content-Type", "text/csv")
+                .send(csvContent)
+                .expect(200)
+                .then(_res => {
+                    return request(app)
+                        .get("/v0/test-csv-1")
+                        .set("Accept", "text/csv")
+                        .expect(200)
+                        .expect(csvContent)
+                        .expect("Content-Type", "text/csv");
+                });
+        });
+        it("JSON File", function() {
+            const jsonContent = fs.readFileSync(
+                "src/test/test_json_1.json",
+                "utf-8"
+            );
+            return request(app)
+                .put("/v0/test-json-1")
+                .set("Content-Type", "application/json")
+                .send(jsonContent)
+                .expect(200)
+                .then(_res => {
+                    return request(app)
+                        .get("/v0/test-json-1")
+                        .set("Accept", "application/json")
+                        .expect(200)
+                        .expect(jsonContent)
+                        .expect("Content-Type", "application/json");
                 });
         });
     });
