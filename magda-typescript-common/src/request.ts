fa78da625cfa25c9dd1bafed17955438446e8ce5
Jacky Jiang <t83714@gmail.com>
2020-02-02 21:19:32 +1100

    fixed api-extract dst rollup  error

--- magda-typescript-common/src/request.ts
+++ magda-typescript-common/src/request.ts
@@ -1,9 +1,9 @@
-import request from "request";
+const requestOriginal = require("request");
 import readPkgUp from "read-pkg-up";
 const pkg = readPkgUp.sync().pkg;
 
 // include user agent derived from package.json in all http requests
-export default request.defaults({
+export default requestOriginal.defaults({
     headers: {
         "User-Agent": "".concat(
             pkg.name.replace("/", "-").replace("@", ""),
