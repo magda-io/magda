38ac1b5e26d64c366822963d50e8c24c69de3245
Sajid Ibne Anower <sajidanower23@gmail.com>
2020-03-11 16:02:30 +1100

    Remove preview-map from core

--- magda-preview-map/lib/Views/AboutButton.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import MenuButton from 'terriajs/lib/ReactViews/Map/MenuButton';
-
-export default function AboutButton() { return (<MenuButton caption="About" href="about.html"/>)};
