81fea1eac07849fac40f33d206eee21e3426eec8
Sajid Ibne Anower <sajidanower23@gmail.com>
2020-03-20 17:51:55 +1100

    Add giant-tick

--- magda-web-client/src/Components/Dataset/Add/Pages/DatasetAddEndPreviewPage.tsx
+++ magda-web-client/src/Components/Dataset/Add/Pages/DatasetAddEndPreviewPage.tsx
@@ -1,20 +1,17 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import giantTick from "assets/giant-tick.svg";
 
 export default function DatasetAddEndPreviewPage() {
     return (
         <div className="row end-preview-page">
             <div className="col-sm-12">
-                <h2>You've completed the Add Dataset Preview!</h2>
+                <h2>
+                    You're all done! <img src={giantTick} />
+                </h2>
                 <hr />
-                <p>
-                    Soon, you'll be able to submit the dataset so that it can be
-                    discovered via search and shared with your colleagues. For
-                    now, your dataset is saved on your local computer, and you
-                    can see it at any time in the{" "}
-                    <Link to="/dataset/list">drafts page</Link>.
-                </p>
+                <p>Your dataset has been successfully sent off for approval.</p>
 
                 <p>
                     In the meantime, we on the Magda team are looking for
