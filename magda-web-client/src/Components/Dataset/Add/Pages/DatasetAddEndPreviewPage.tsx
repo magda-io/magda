a7bc170fe408119d7769ee8c0072b7562414b277
Sajid Ibne Anower <sajidanower23@gmail.com>
2020-03-27 10:21:11 +1100

    Use the draft image button

--- magda-web-client/src/Components/Dataset/Add/Pages/DatasetAddEndPreviewPage.tsx
+++ magda-web-client/src/Components/Dataset/Add/Pages/DatasetAddEndPreviewPage.tsx
@@ -2,15 +2,17 @@ import React from "react";
 
 import "./DatasetAddEndPreviewPage.scss";
 
-import giantTick from "assets/giant-tick.svg";
+import giantTickIcon from "assets/giant-tick.svg";
+import draftIcon from "assets/format-active.svg";
+// import printIcon from "assets/print.svg";
 
 export default function DatasetAddEndPreviewPage() {
     return (
-        <div className="row end-preview-page">
-            <div className="col-sm-12">
+        <div className="row">
+            <div className="col-sm-12 end-preview-page">
                 <div className="end-preview-container-1">
                     <img
-                        src={giantTick}
+                        src={giantTickIcon}
                         style={{ justifyContent: "center", display: "flex" }}
                     />
                     <h2>You're all done!</h2>
@@ -21,7 +23,27 @@ export default function DatasetAddEndPreviewPage() {
                         Your dataset has been successfully sent off for
                         approval.
                     </h3>
+                    <p style={{ fontSize: "15px" }}>
+                        You can view the status of your dataset from{" "}
+                        <a href="/">your home page.</a>
+                    </p>
                 </div>
+            </div>
+
+            <div>
+                <button className="au-btn next-button">
+                    View your draft dataset
+                    <img
+                        className="draft-image-con"
+                        src={draftIcon}
+                        style={{
+                            marginLeft: "-200px",
+                            marginTop: "-10px",
+                            width: "30px",
+                            height: "30px"
+                        }}
+                    />
+                </button>
             </div>
         </div>
     );
