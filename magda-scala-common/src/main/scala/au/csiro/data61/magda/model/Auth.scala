006d940dbdfaef9858072d04d45200db08c742b8
Alex Gilleran <alex@alexgilleran.com>
2020-04-02 10:14:08 +1100

    Aligning user object with auth api version.

--- magda-scala-common/src/main/scala/au/csiro/data61/magda/model/Auth.scala
+++ magda-scala-common/src/main/scala/au/csiro/data61/magda/model/Auth.scala
@@ -3,7 +3,10 @@ package au.csiro.data61.magda.model
 import spray.json.DefaultJsonProtocol
 
 object Auth {
-  case class User(userId: String, isAdmin: Boolean)
+  case class User(
+      id: String,
+      isAdmin: Boolean
+  )
 
   trait AuthProtocols extends DefaultJsonProtocol {
     implicit val userFormat = jsonFormat2(User)
