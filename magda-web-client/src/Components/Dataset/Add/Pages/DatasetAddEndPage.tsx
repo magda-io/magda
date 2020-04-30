099c9d75c792a1978fae60d41f818d8b704b432a
Alex Gilleran <alex.gilleran@nicta.com.au>
2020-04-30 16:47:58 +1000

    Made the ui of the all done page fit the design a bit closer.

--- magda-web-client/src/Components/Dataset/Add/Pages/DatasetAddEndPage.tsx
+++ magda-web-client/src/Components/Dataset/Add/Pages/DatasetAddEndPage.tsx
@@ -1,9 +1,11 @@
 import "./DatasetAddEndPage.scss";
 
+import React from "react";
+import { Link } from "react-router-dom";
+
 import giantTickIcon from "assets/giant-tick.svg";
 import draftIcon from "assets/format-active.svg";
 import printIcon from "assets/print.svg";
-import React from "react";
 
 type Props = {
     datasetId: string;
@@ -13,51 +15,46 @@ type Props = {
 // If you are not in preview mode
 export default function DatasetAddEndPage(props: Props) {
     const draftPage = "/dataset/" + props.datasetId + "/details";
-    const viewDraft = () => {
-        return props.history.push(draftPage);
-    };
 
-    const printDraft = () => {
-        return props.history.push(draftPage + "?print=true");
-    };
     return (
         <div className="row">
             <div className="col-sm-12 end-preview-page-1">
                 <div className="end-preview-container-1">
                     <img src={giantTickIcon} className="giant-tick" />
-                    <h2>You're all done!</h2>
+                    <h1 className="end-preview-heading">You're all done!</h1>
                 </div>
                 <div className="end-preview-container-2">
-                    <h3>
+                    <h2 className="end-preview-subheading">
                         Your dataset has been successfully sent off for
                         approval.
-                    </h3>
-                    <br />
+                    </h2>
                     <p className="dataset-status-txt">
                         You can view the status of your dataset from{" "}
                         <a href="/">your home page</a>.
                     </p>
                 </div>
             </div>
-            <div className="end-preview-page-2">
-                <button
-                    className="au-btn next-button end-preview-button draft-dataset-btn"
-                    onClick={viewDraft}
-                >
-                    <img className="draft-image-icon" src={draftIcon} />
-                    <p className="draft-dataset-txt">View your draft dataset</p>
-                </button>
-                <br />
-                <br />
-                <button
-                    className="au-btn next-button end-preview-button print-metadata-btn"
-                    onClick={printDraft}
-                >
-                    <img className="print-icon" src={printIcon} />
-                    <p className="print-metadata-txt">
-                        Print a copy of your metadata
-                    </p>
-                </button>
+            <div className="col-sm-12 end-preview-page-2">
+                <div>
+                    <Link to={draftPage}>
+                        <a className="au-btn next-button end-preview-button draft-dataset-btn">
+                            <img className="draft-image-icon" src={draftIcon} />
+                            <span className="draft-dataset-txt">
+                                View your draft dataset
+                            </span>
+                        </a>
+                    </Link>
+                </div>
+                <div>
+                    <Link to={draftPage + "?print=true"}>
+                        <a className="au-btn next-button end-preview-button print-metadata-btn">
+                            <img className="print-icon" src={printIcon} />
+                            <span className="print-metadata-txt">
+                                Print a copy of your metadata
+                            </span>
+                        </a>
+                    </Link>
+                </div>
             </div>
         </div>
     );
