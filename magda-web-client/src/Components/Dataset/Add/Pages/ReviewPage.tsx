3a595f8904f1e974607b2c970b09d45f002494d5
Jacky Jiang <t83714@gmail.com>
2020-05-08 17:32:13 +1000

    Added initial style of the review page

--- magda-web-client/src/Components/Dataset/Add/Pages/ReviewPage.tsx
+++ magda-web-client/src/Components/Dataset/Add/Pages/ReviewPage.tsx
@@ -1,11 +1,26 @@
 import "./ReviewPage.scss";
 
 import React from "react";
+import ToolTip from "Components/Dataset/Add/ToolTip";
 
 export default function DatasetAddEndPage(props) {
     return (
-        <div className="row people-and-production-page">
-            <div className="col-sm-12">review page...</div>
+        <div className="row review-page">
+            <div className="col-sm-12">
+                <h2>Review before you submit</h2>
+                <div className="top-tooltip">
+                    <ToolTip>
+                        To keep this page short, we’re defaulted to show your
+                        responses to the mandatory fields. Expand each section
+                        to view all the metadata fields
+                    </ToolTip>
+                </div>
+
+                <div className="section-heading">
+                    Dataset files and services
+                    <button className="au-btn">Edit</button>
+                </div>
+            </div>
         </div>
     );
 }
