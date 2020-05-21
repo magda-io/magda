f340c9dbd6bcfdb48ce08f31b9e04dee343ab7ea
Jacky Jiang <t83714@gmail.com>
2020-05-21 12:01:25 +1000

    Add timestampe column

--- magda-migrator-authorization-db/sql/V1_8__credentials.sql
+++ magda-migrator-authorization-db/sql/V1_8__credentials.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "public"."credentials" (
     "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "user_id" uuid NOT NULL,
+    "timestamp" timestamp with time zone DEFAULT (now() at time zone 'utc'),
     "hash" varchar(60) NOT NULL DEFAULT ''::character varying,
     PRIMARY KEY ("id")
 ) WITH (
