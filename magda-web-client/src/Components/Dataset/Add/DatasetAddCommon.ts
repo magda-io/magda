b63ecbed62d5ee88f93787a90c16358b95eb88d1
Sajid Ibne Anower <sajidanower23@gmail.com>
2020-06-04 14:40:39 +1000

    Set publishStatus to published when dataset approval flow is off

--- magda-web-client/src/Components/Dataset/Add/DatasetAddCommon.ts
+++ magda-web-client/src/Components/Dataset/Add/DatasetAddCommon.ts
@@ -528,7 +528,9 @@ export function createBlankState(user: User): State {
             defaultLicense: "world"
         },
         datasetPublishing: {
-            state: "draft",
+            state: config.featureFlags.datasetApprovalWorkflowOn
+                ? "draft"
+                : "published",
             level: "agency",
             contactPointDisplay: "team"
         },
