aedf6cacc4fa8841ac5f3f5d79ce591ca53bcf9d
Alex Gilleran <alex@alexgilleran.com>
2020-06-10 10:59:20 +1000

    disabling terser sourcemap

--- magda-web-client/craco.config.js
+++ magda-web-client/craco.config.js
@@ -15,6 +15,8 @@ module.exports = {
             );
             if (terser) {
                 terser.options.parallel = false;
+                terser.options.sourceMap =
+                    process.env.GENERATE_SOURCEMAP !== "false";
             }
 
             const updatedRules = webpackConfig.module.rules.filter(
