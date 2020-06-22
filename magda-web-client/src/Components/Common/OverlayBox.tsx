b6067fff0ed3b95b6b6eb6f156b159d4462cee18
Jacky Jiang <t83714@gmail.com>
2020-06-22 12:11:48 +1000

    Completes Overlaybox

--- magda-web-client/src/Components/Common/OverlayBox.tsx
+++ magda-web-client/src/Components/Common/OverlayBox.tsx
@@ -1,46 +1,66 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import Modal from "react-modal";
 
 import "./OverlayBox.scss";
 import { ReactComponent as DismissIcon } from "assets/dismiss.svg";
 
-type PropsType = {
-    isOpen: boolean;
+Modal.setAppElement("#root");
+
+type OverlayBoxOwnProps = {
+    isOpen?: boolean;
     className?: string;
     title?: string;
     showCloseButton?: boolean;
-    onClose?: () => void;
 };
+
+type PropsType = OverlayBoxOwnProps &
+    Partial<Exclude<Modal.Props, OverlayBoxOwnProps>>;
 
 const DEFAULT_TITLE = "Untitled Overlay";
 const customStyles = {
     overlay: {
         display: "flex",
-        "align-items": "center",
-        "justify-content": "center",
-        "background-color": null
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "none"
     },
     content: {
-        top: null,
-        bottom: null,
-        "border-radius": "8px",
-        padding: null
+        top: undefined,
+        bottom: undefined,
+        borderRadius: "8px",
+        padding: undefined
     }
 };
 
 const OverlayBox: FunctionComponent<PropsType> = (props) => {
-    const title = props.title ? props.title : DEFAULT_TITLE;
+    const {
+        title,
+        isOpen: defaultIsOpen,
+        className,
+        showCloseButton,
+        ...restProps
+    } = props;
+    const runtimeTitle = title ? title : DEFAULT_TITLE;
+
+    const [isOpen, setIsOpen] = useState<boolean>(
+        typeof defaultIsOpen === "boolean" ? defaultIsOpen : false
+    );
 
     return (
-        <Modal style={customStyles} isOpen={props.isOpen} contentLabel={title}>
+        <Modal
+            style={customStyles}
+            isOpen={isOpen}
+            contentLabel={runtimeTitle}
+            {...restProps}
+        >
             <div
                 className={`overlay-box-outter-container ${
                     props.className ? props.className : ""
                 }`}
             >
                 <div className="overlay-box-header">
-                    {title}
-                    <DismissIcon />
+                    {runtimeTitle}
+                    <DismissIcon onClick={() => setIsOpen(false)} />
                 </div>
                 <div className="overlay-box-body">{props.children}</div>
             </div>
