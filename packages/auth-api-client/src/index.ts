1cde2b50266cf41518ff1af573354a580279255c
Jacky Jiang <t83714@gmail.com>
2020-10-19 12:49:32 +1100

    release auth-api-client packages

---
+++ packages/auth-api-client/src/index.ts
@@ -0,0 +1,11 @@
+import { default as AuthApiClient } from "@magda/typescript-common/dist/authorization-api/ApiClient";
+export {
+    User,
+    OrgUnit,
+    Operation,
+    Permission,
+    PublicUser,
+    Role
+} from "@magda/typescript-common/dist/authorization-api/model";
+export { Maybe } from "tsmonad";
+export default AuthApiClient;
