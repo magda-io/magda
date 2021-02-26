82c09e91eca6e131440fd61a5849c5c118cc6a1b
yayalu <yaya.lu.01@gmail.com>
2021-02-26 13:53:05 +1100

    Fixed import spacing

--- magda-web-client/src/Components/Common/ContactPoint.js
+++ magda-web-client/src/Components/Common/ContactPoint.js
@@ -1,6 +1,5 @@
-import PropTypes from "prop-types";
-
 import React from "react";
+import PropTypes from "prop-types";
 import MarkdownViewer from "Components/Common/MarkdownViewer";
 import MagdaNamespacesConsumer from "Components/i18n/MagdaNamespacesConsumer";
 import ToggleButton from "./ToggleButton";
