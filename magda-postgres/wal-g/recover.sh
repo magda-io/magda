9494b3105d3862425d633f30d1f01a9cc00e9f2e
Jacky Jiang <jacky.jiang@data61.csiro.au>
2021-08-19 22:48:49 +1000

    make recovery base backup name adjustable

--- magda-postgres/wal-g/recover.sh
+++ magda-postgres/wal-g/recover.sh
@@ -4,21 +4,23 @@
 # https://www.postgresql.org/docs/13/continuous-archiving.html#BACKUP-PITR-RECOVERY
 
 # Turn on recovery conf
-cp /wal-g/recovery.conf /opt/bitnami/postgresql/conf/conf.d/recovery.conf
+cp -f /wal-g/recovery.conf /opt/bitnami/postgresql/conf/conf.d/recovery.conf
 
 # disable remote connections
 mv /opt/bitnami/postgresql/conf/pg_hba.conf /opt/bitnami/postgresql/conf/pg_hba.conf.orig
-cp /wal-g/pg_hba.conf /opt/bitnami/postgresql/conf/pg_hba.conf
+cp -f /wal-g/pg_hba.conf /opt/bitnami/postgresql/conf/pg_hba.conf
 
 # disable archive mode
 if [ -f /opt/bitnami/postgresql/conf/conf.d/archive.conf ]
 then 
+    echo "moving away archive.conf before recover..."
     mv /opt/bitnami/postgresql/conf/conf.d/archive.conf /opt/bitnami/postgresql/conf/conf.d/archive.conf.orig
 fi
 
 # backup pg_wal
-if [ ! -f /wal-g/pg_wal ]
-then 
+if [ ! -d /wal-g/pg_wal ] && [ -d $PGDATA/pg_wal ]
+then
+    echo "saving a copy of pg_wal before fetch base backup..."
     mv $PGDATA/pg_wal /wal-g/pg_wal
 fi
 
@@ -26,11 +28,23 @@ fi
 rm -rf $PGDATA/*
 
 # fetch most recent full backup
-wal-g backup-fetch $PGDATA LATEST
+if [ -z "$MAGDA_RECOVERY_BASE_BACKUP_NAME" ]
+then 
+    echo "fetch LATEST backup..."
+    wal-g backup-fetch $PGDATA LATEST
+else 
+    echo "fetch backup: ${MAGDA_RECOVERY_BASE_BACKUP_NAME}..."
+    wal-g backup-fetch $PGDATA "$MAGDA_RECOVERY_BASE_BACKUP_NAME"
+fi
+
 
 # Remove any files present in pg_wal/ that is restored from backup
 # Restore with previous saved copy
 rm -rf $PGDATA/pg_wal
-cp -r /wal-g/pg_wal $PGDATA/pg_wal
+if [ -d /wal-g/pg_wal ]
+then 
+    echo "restoring a saved copy of pg_wal after fetch base backup..."
+    cp -rf /wal-g/pg_wal $PGDATA/pg_wal
+fi
 
 # after this line, postgresql will start and enter recovery mode
