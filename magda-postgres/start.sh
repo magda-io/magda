64c1a230686397d34b70b50a056dc2c6f033acca
Jacky Jiang <jacky.jiang@data61.csiro.au>
2021-08-29 12:35:26 +1000

    adjust hba file via script rather than upstream helm chart config: - the init script requires the trust local entry to set admin password

--- magda-postgres/start.sh
+++ magda-postgres/start.sh
@@ -7,9 +7,31 @@ set -o nounset
 set -o pipefail
 #set -o xtrace
 
-if [ "${MAGDA_RECOVERY_MODE:=false}" = "true" ] && [ ! -f /wal-g/recovery.complete ]
+# Load libraries
+. /opt/bitnami/scripts/liblog.sh
+
+# Load PostgreSQL environment variables
+. /opt/bitnami/scripts/postgresql-env.sh
+
+if [[ "${MAGDA_BACKUP_MODE:=false}" = "true" ]]
 then
-    echo "Entering recovery mode..."
+    info "Backup mode is turned on." 
+    info "Making sure replication config in ${POSTGRESQL_PGHBA_FILE} is available for creating base backup remotely..."
+    FOUND_REPLICATION_ENTRY=$(cat ${POSTGRESQL_PGHBA_FILE} | grep replication)
+    if [[ "$?" = "0" ]]
+    then
+        info "Found the following 'replication' entry, skip adding 'replication' entry."
+        echo $FOUND_REPLICATION_ENTRY
+    else
+        echo "# replication keyword allows base backup to be created remotely using wal-g" >> $POSTGRESQL_PGHBA_FILE
+        echo "host     replication     all        0.0.0.0/0               md5" >> $POSTGRESQL_PGHBA_FILE
+        info "Cannot locate any 'replication' entry, added 'replication' entry to ${POSTGRESQL_PGHBA_FILE}."
+    fi
+fi
+
+if [[ "${MAGDA_RECOVERY_MODE:=false}" = "true" ]] && [[ ! -f /wal-g/recovery.complete ]]
+then
+    info "Recovery mode is turned on. Entering recovery mode..."
     /wal-g/recover.sh
 fi
 
