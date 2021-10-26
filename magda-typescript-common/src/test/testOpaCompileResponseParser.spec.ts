e382eb5a9560a3fb54f8944ccfeb49d0fc91f90b
Jacky Jiang <jacky.jiang@data61.csiro.au>
2021-10-27 00:51:33 +1100

    add more test cases

--- magda-typescript-common/src/test/testOpaCompileResponseParser.spec.ts
+++ magda-typescript-common/src/test/testOpaCompileResponseParser.spec.ts
@@ -9,6 +9,7 @@ import testDataUnconditionalNotMacthed from "./sampleOpaResponses/unconditionalN
 import testDataUnconditionalNotMacthedWithExtraRefs from "./sampleOpaResponses/unconditionalNotMacthedWithExtraRefs.json";
 import testDataUnconditionalFalseSimple from "./sampleOpaResponses/unconditionalFalseSimple.json";
 import testDataUnconditionalTrueSimple from "./sampleOpaResponses/unconditionalTrueSimple.json";
+import testDataDatasetPermissionWithOrgUnitConstraint from "./sampleOpaResponses/datasetPermissionWithOrgUnitConstraint.json";
 import "mocha";
 
 /**
@@ -289,5 +290,56 @@ describe("Test OpaCompileResultParser with unconditional false (simple response)
         const result = parser.evaluateAsHumanReadableString();
         expect(parser.hasWarns).to.be.equal(false);
         expect(result).to.be.equal("false");
+    });
+});
+
+describe("Test OpaCompileResultParser with datasetPermissionWithOrgUnitConstraint", function () {
+    it("Parse sample response with no errors", function () {
+        const parser = new OpaCompileResponseParser();
+        const data = parser.parse(
+            JSON.stringify(testDataDatasetPermissionWithOrgUnitConstraint)
+        );
+        expect(parser.hasWarns).to.be.equal(false);
+        expect(data).to.be.an("array");
+    });
+
+    it("Should evalute query from parse result correctly", function () {
+        const parser = new OpaCompileResponseParser();
+        parser.parse(
+            JSON.stringify(testDataDatasetPermissionWithOrgUnitConstraint)
+        );
+        const result = parser.evaluate();
+        expect(parser.hasWarns).to.be.equal(false);
+        expect(result.isCompleteEvaluated).to.be.equal(false);
+        expect(result.residualRules).to.be.an("array");
+        expect(result.residualRules.length).to.be.equal(2);
+        expect(result.residualRules[0].isCompleteEvaluated).to.be.equal(false);
+        expect(result.residualRules[0].expressions.length).to.be.equal(2);
+        expect(result.residualRules[0].expressions[0].terms.length).to.be.equal(
+            3
+        );
+        expect(
+            result.residualRules[0].expressions[0].toHumanReadableString()
+        ).to.be.equal('input.object.dataset.publishingState = "published"');
+        expect(result.residualRules[0].expressions[1].terms.length).to.be.equal(
+            3
+        );
+        expect(
+            result.residualRules[0].expressions[1].toHumanReadableString()
+        ).to.be.equal(
+            '"5447fcb1-74ec-451c-b6ef-007aa736a346" = input.object.dataset.accessControl.orgUnitOwnerId'
+        );
+    });
+
+    it("Should generate correct human readable string", function () {
+        const parser = new OpaCompileResponseParser();
+        parser.parse(
+            JSON.stringify(testDataDatasetPermissionWithOrgUnitConstraint)
+        );
+        const result = parser.evaluateAsHumanReadableString();
+        expect(parser.hasWarns).to.be.equal(false);
+        expect(result).to.be.equal(
+            `( input.object.dataset.publishingState = "published" AND \n"5447fcb1-74ec-451c-b6ef-007aa736a346" = input.object.dataset.accessControl.orgUnitOwnerId )\nOR\n( input.object.dataset.publishingState = "published" AND \n"b749759e-6e6a-44c0-87ab-4590744187cf" = input.object.dataset.accessControl.orgUnitOwnerId )`
+        );
     });
 });
