bb5dd071e01438ff0944732e675018a5232bd3b7
Jacky Jiang <jacky.jiang@data61.csiro.au>
2021-11-07 22:14:36 +1100

    - add buildx to magda-builder-nodejs image - adjust CI job dependencies

--- gitlab-ci-buildx-setup.sh
+++ gitlab-ci-buildx-setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# All env vars are supposed to be provided in CI runtime env
+docker info
+docker -v
 
+# All env vars are supposed to be provided in CI runtime env
 docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
 
-mkdir -p ~/.docker/cli-plugins
-wget -O ~/.docker/cli-plugins/docker-buildx https://github.com/docker/buildx/releases/download/${BUILDX_VERSION}/buildx-${BUILDX_VERSION}.${BUILDX_ARCH}
-chmod +x ~/.docker/cli-plugins/docker-buildx
 docker context create builder-context
 docker buildx create --name builderx --driver docker-container --use builder-context 
