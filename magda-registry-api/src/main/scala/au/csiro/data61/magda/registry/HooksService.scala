28d6632ec8aad86e01677c3c2d8a42e212ee59e4
Jacky Jiang <t83714@gmail.com>
2022-01-27 10:50:02 +1100

    bug fixes

--- magda-registry-api/src/main/scala/au/csiro/data61/magda/registry/HooksService.scala
+++ magda-registry-api/src/main/scala/au/csiro/data61/magda/registry/HooksService.scala
@@ -359,7 +359,7 @@ class HooksService(
             input = Some(
               JsObject(
                 "object" -> JsObject(
-                  "webhook" -> hook.toPolicyEngineContextData
+                  "webhook" -> hook.toJson
                 )
               )
             )
