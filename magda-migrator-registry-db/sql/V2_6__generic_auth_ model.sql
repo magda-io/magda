0d670b11d3df24bbb01814a7df46b43162a7b3d3
Jacky Jiang <t83714@gmail.com>
2022-01-27 09:26:43 +1100

    fix SQL upgrade scripts

--- magda-migrator-registry-db/sql/V2_6__generic_auth_ model.sql
+++ magda-migrator-registry-db/sql/V2_6__generic_auth_ model.sql
@@ -1,11 +1,11 @@
 -- Add owner_id, creator_id & editor_id field to webhooks table
-ALTER TABLE "public"."webhooks"
+ALTER TABLE webhooks
 ADD COLUMN "owner_id" uuid DEFAULT NULL,
 ADD COLUMN "creator_id" uuid DEFAULT NULL,
 ADD COLUMN "editor_id" uuid DEFAULT NULL,
 ADD COLUMN "create_time" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ADD COLUMN "edit_time" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
-CREATE INDEX webhooks_owner_id_idx ON "public"."webhooks" USING btree ("owner_id");
-CREATE INDEX webhooks_create_time_idx ON "public"."webhooks" USING btree ("create_time");
-CREATE INDEX webhooks_create_time_idx ON "public"."webhooks" USING btree ("edit_time");
+CREATE INDEX webhooks_owner_id_idx ON webhooks USING btree ("owner_id");
+CREATE INDEX webhooks_create_time_idx ON webhooks USING btree ("create_time");
+CREATE INDEX webhooks_create_time_idx ON webhooks USING btree ("edit_time");
