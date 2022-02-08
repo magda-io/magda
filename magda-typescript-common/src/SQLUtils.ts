10828aef2610b507e58d9e81787a496b62ee872c
Jacky Jiang <t83714@gmail.com>
2022-02-09 00:55:28 +1100

    change the default of `useLowerCaseColumnName` parameter for getTableColumnName

--- magda-typescript-common/src/SQLUtils.ts
+++ magda-typescript-common/src/SQLUtils.ts
@@ -43,7 +43,7 @@ export function escapeIdentifier(id: string): SQLSyntax {
 export function getTableColumnName(
     columnName: String,
     tableRef: String = "",
-    useLowerCaseColumnName: Boolean = true
+    useLowerCaseColumnName: Boolean = false
 ): SQLSyntax {
     const id = [
         tableRef,
