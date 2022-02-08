805576f12597fcef6d740766fd6435b2ec5d60c6
Jacky Jiang <t83714@gmail.com>
2022-02-08 17:26:30 +1100

    image size adjustment

--- magda-web-client/src/Components/Home/HomePage.tsx
+++ magda-web-client/src/Components/Home/HomePage.tsx
@@ -41,16 +41,16 @@ const getBgImg = (backgroundImageUrls) => {
 
     const screenSizes = Object.keys(imageMap);
 
-    function getBackgroundImage(imageUrl) {
-        return {
-            backgroundImage: "url(" + imageUrl + ")",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover"
-        };
-    }
+    // function getBackgroundImage(imageUrl) {
+    //     return {
+    //         backgroundImage: "url(" + imageUrl + ")",
+    //         backgroundPosition: "center",
+    //         backgroundRepeat: "no-repeat",
+    //         backgroundSize: "cover"
+    //     };
+    // }
     return (
-        <div>
+        <>
             {screenSizes.map((size, i) => (
                 <MediaQuery
                     key={size}
@@ -61,13 +61,13 @@ const getBgImg = (backgroundImageUrls) => {
                             : screenSizes[i + 1] + "px"
                     }
                 >
-                    <div
+                    <img
+                        src={imageMap[size]}
                         className="homepage-background-img"
-                        style={getBackgroundImage(imageMap[size])}
                     />
                 </MediaQuery>
             ))}
-        </div>
+        </>
     );
 };
 
