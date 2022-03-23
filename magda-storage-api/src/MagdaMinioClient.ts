1c1c41e1925a70604bdd276c3aa2add6db37162d
Jacky Jiang <t83714@gmail.com>
2022-03-23 21:11:10 +1100

    add presign url methods

--- magda-storage-api/src/MagdaMinioClient.ts
+++ magda-storage-api/src/MagdaMinioClient.ts
@@ -3,10 +3,19 @@ import { CreateBucketResponse } from "./ObjectStoreClient";
 import { Stream, Readable } from "stream";
 import * as Minio from "minio";
 import { UploadedObjectInfo } from "minio";
+import urijs from "urijs";
+
+type HeadersParamType = {
+    [key: string]: string | number;
+};
 
 export default class MagdaMinioClient {
     public readonly client: Minio.Client;
     public readonly region: string;
+    public readonly endPoint: string;
+    public readonly port: string;
+    public readonly useSSL: boolean;
+    public readonly endPointBaseUrl: string;
 
     constructor({
         endPoint,
@@ -25,6 +34,137 @@ export default class MagdaMinioClient {
             region
         });
         this.region = region;
+        this.endPoint = endPoint;
+        this.port = port;
+        this.useSSL = useSSL;
+        this.endPointBaseUrl = urijs({
+            protocol: useSSL ? "https" : "http",
+            hostname: endPoint,
+            port: port ? port : 80
+        }).toString();
+    }
+
+    async createGetObjectPresignUrl(
+        bucketName: string,
+        objectName: string,
+        expires?: number,
+        respHeaders?: HeadersParamType,
+        requestDate?: Date
+    ): Promise<string> {
+        let result: string;
+        if (requestDate) {
+            result = await this.client.presignedGetObject(
+                bucketName,
+                objectName,
+                expires,
+                respHeaders,
+                requestDate
+            );
+        } else if (respHeaders) {
+            result = await this.client.presignedGetObject(
+                bucketName,
+                objectName,
+                expires,
+                respHeaders
+            );
+        } else if (respHeaders) {
+            result = await this.client.presignedGetObject(
+                bucketName,
+                objectName,
+                expires
+            );
+        } else if (expires) {
+            result = await this.client.presignedGetObject(
+                bucketName,
+                objectName,
+                expires
+            );
+        } else {
+            result = await this.client.presignedGetObject(
+                bucketName,
+                objectName
+            );
+        }
+        return result;
+    }
+
+    /**
+     * return presigned url path that can be used to access externally.
+     * Here we assume requests are proxied to minio at `/api/v0/storage/gateway`
+     * e.g. /api/v0/storage/gateway/bucket1/folder1/folder2/object1?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=minio%2F20220323%2Funspecified-region%2Fs3%2Faws4_request&X-Amz-Date=20220323T020200Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=1ace2877887b50a7b9befef1b7b89c5cf7e223f296c15bb176e16fb409e7159c
+     *
+     * @param {string} bucketName
+     * @param {string} objectName
+     * @param {number} [expires]
+     * @param {HeadersParamType} [respHeaders]
+     * @param {Date} [requestDate]
+     * @return {*}
+     * @memberof MagdaMinioClient
+     */
+    async createGetObjectPresignExternalUri(
+        bucketName: string,
+        objectName: string,
+        expires?: number,
+        respHeaders?: HeadersParamType,
+        requestDate?: Date
+    ) {
+        const result = await this.createGetObjectPresignUrl(
+            bucketName,
+            objectName,
+            expires,
+            respHeaders,
+            requestDate
+        );
+        return (
+            "/api/v0/storage/gateway" +
+            result.substr(this.endPointBaseUrl.length)
+        );
+    }
+
+    async createPutObjectPresignUrl(
+        bucketName: string,
+        objectName: string,
+        expires?: number
+    ): Promise<string> {
+        let result: string;
+        if (expires) {
+            result = await this.client.presignedPutObject(
+                bucketName,
+                objectName,
+                expires
+            );
+        } else {
+            result = await this.client.presignedPutObject(
+                bucketName,
+                objectName
+            );
+        }
+        return result;
+    }
+
+    /**
+     * return presigned url path that can be used to access externally.
+     *
+     * @param {string} bucketName
+     * @param {string} objectName
+     * @param {number} [expires]
+     * @return {*}
+     * @memberof MagdaMinioClient
+     */
+    async createPutObjectPresignExternalUri(
+        bucketName: string,
+        objectName: string,
+        expires?: number
+    ) {
+        const result = await this.createPutObjectPresignUrl(
+            bucketName,
+            objectName,
+            expires
+        );
+        return (
+            "/api/v0/storage/gateway" +
+            result.substr(this.endPointBaseUrl.length)
+        );
     }
 
     createBucket(bucket: string): Promise<CreateBucketResponse> {
