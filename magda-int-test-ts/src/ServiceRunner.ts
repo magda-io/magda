897b991cf0f612dfbaad63d63d34c0e86f396e6b
Jacky Jiang <t83714@gmail.com>
2022-04-12 11:44:59 +1000

    fix incorrect quote in port-forward command

--- magda-int-test-ts/src/ServiceRunner.ts
+++ magda-int-test-ts/src/ServiceRunner.ts
@@ -195,7 +195,7 @@ export default class ServiceRunner {
                     `Port ${localPort} already has an existing port forwarding process running.`
                 );
             }
-            const portForwardCmd = `socat TCP4-LISTEN:${localPort},fork,reuseaddr TCP4:${hostname}:${remotePort}"`;
+            const portForwardCmd = `socat TCP4-LISTEN:${localPort},fork,reuseaddr TCP4:${hostname}:${remotePort}`;
             const portForwardProcess = child_process.spawn(portForwardCmd, {
                 stdio: "inherit",
                 shell: true
