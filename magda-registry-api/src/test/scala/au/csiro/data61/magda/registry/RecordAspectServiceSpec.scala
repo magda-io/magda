c8e4b0e5ec5c22c6602ab6cf06d47b97da81ded8
Jacky Jiang <t83714@gmail.com>
2022-06-21 22:27:59 +1000

    Add test cases for Modify endpoint {put} /v0/registry/records/aspects/{aspectId}

--- magda-registry-api/src/test/scala/au/csiro/data61/magda/registry/RecordAspectServiceSpec.scala
+++ magda-registry-api/src/test/scala/au/csiro/data61/magda/registry/RecordAspectServiceSpec.scala
@@ -35,7 +35,278 @@ class RecordAspectServiceSpec extends ApiSpec {
     param.authFetcher.resetMock()
   }
 
-  describe("Record Aspect Service Auth Logic") {
+  def verifyAspectData(
+      param: FixtureParam,
+      recordId: String,
+      aspectId: String,
+      jsonString: String
+  ) = {
+    Get(s"/v0/records/${recordId}/aspects/${aspectId}") ~> addUserId() ~> addTenantIdHeader(
+      TENANT_1
+    ) ~> param.api(Full).routes ~> check {
+      withClue(responseAs[String]) {
+        status shouldEqual StatusCodes.OK
+      }
+      val aspectData = responseAs[JsObject]
+      aspectData.toString() shouldBe
+        jsonString.parseJson.toString()
+    }
+  }
+
+  describe("Record Aspect Service Logic") {
+    describe(
+      "Modify endpoint {put} /v0/registry/records/aspects/{aspectId}"
+    ) {
+      it("should merge aspect correctly when merge = true") { param =>
+        settingUpTestData(
+          param,
+          List(
+            Record(
+              id = "record-1",
+              name = "test record 2",
+              aspects = Map(
+                "test-aspect" ->
+                  """
+                    | {
+                    |    "a" : [1,2],
+                    |    "b" : "yes",
+                    |    "d" : {
+                    |      "d1":1
+                    |    },
+                    |    "e": 2
+                    | }
+                    |""".stripMargin.parseJson.asJsObject
+              ),
+              None,
+              None
+            ),
+            Record(
+              id = "record-2",
+              name = "test record 2",
+              aspects = Map(
+                "test-aspect" ->
+                  """
+                    | {
+                    |    "a" : [3,4]
+                    | }
+                    |""".stripMargin.parseJson.asJsObject
+              ),
+              None,
+              None
+            ),
+            Record(
+              id = "record-3",
+              name = "test record 3",
+              aspects = Map(),
+              None,
+              None
+            )
+          )
+        )
+
+        Put(
+          s"/v0/records/aspects/test-aspect?merge=true",
+          JsObject(
+            "recordIds" -> JsArray(
+              Vector(
+                JsString("record-1"),
+                JsString("record-2"),
+                JsString("record-3")
+              )
+            ),
+            "data" -> JsObject(
+              "a" -> JsArray(Vector(JsNumber(2), JsNumber(5))),
+              "b" -> JsString("no"),
+              "c" -> JsString("c-value"),
+              "d" -> JsObject("d2" -> JsNumber(2))
+            )
+          )
+        ) ~> addUserId() ~> addTenantIdHeader(
+          TENANT_1
+        ) ~> param.api(Full).routes ~> check {
+          withClue(responseAs[String]) {
+            status shouldEqual StatusCodes.OK
+          }
+          val eventList = responseAs[List[Long]]
+          eventList.length shouldBe 3
+        }
+
+        verifyAspectData(
+          param,
+          "record-1",
+          "test-aspect",
+          """
+            |{
+            |  "a" : [1,2,5],
+            |  "b" : "no",
+            |  "c" : "c-value",
+            |  "d" : {
+            |      "d1":1,
+            |      "d2":2
+            |   },
+            |   "e": 2
+            |}
+            |""".stripMargin
+        )
+
+        verifyAspectData(
+          param,
+          "record-2",
+          "test-aspect",
+          """
+            |{
+            |  "a" : [3,4,2,5],
+            |  "b" : "no",
+            |  "c" : "c-value",
+            |  "d" : {
+            |      "d2":2
+            |   }
+            |}
+            |""".stripMargin
+        )
+
+        verifyAspectData(
+          param,
+          "record-3",
+          "test-aspect",
+          """
+            |{
+            |  "a" : [2,5],
+            |  "b" : "no",
+            |  "c" : "c-value",
+            |  "d" : {
+            |      "d2":2
+            |   }
+            |}
+            |""".stripMargin
+        )
+      }
+
+      it("should replace aspect data when merge = false") { param =>
+        settingUpTestData(
+          param,
+          List(
+            Record(
+              id = "record-1",
+              name = "test record 2",
+              aspects = Map(
+                "test-aspect" ->
+                  """
+                    | {
+                    |    "a" : [1,2],
+                    |    "b" : "yes",
+                    |    "d" : {
+                    |      "d1":1
+                    |    },
+                    |    "e": 2
+                    | }
+                    |""".stripMargin.parseJson.asJsObject
+              ),
+              None,
+              None
+            ),
+            Record(
+              id = "record-2",
+              name = "test record 2",
+              aspects = Map(
+                "test-aspect" ->
+                  """
+                    | {
+                    |    "a" : [3,4]
+                    | }
+                    |""".stripMargin.parseJson.asJsObject
+              ),
+              None,
+              None
+            ),
+            Record(
+              id = "record-3",
+              name = "test record 3",
+              aspects = Map(),
+              None,
+              None
+            )
+          )
+        )
+
+        Put(
+          s"/v0/records/aspects/test-aspect",
+          JsObject(
+            "recordIds" -> JsArray(
+              Vector(
+                JsString("record-1"),
+                JsString("record-2"),
+                JsString("record-3")
+              )
+            ),
+            "data" -> JsObject(
+              "a" -> JsArray(Vector(JsNumber(2), JsNumber(5))),
+              "b" -> JsString("no"),
+              "c" -> JsString("c-value"),
+              "d" -> JsObject("d2" -> JsNumber(2))
+            )
+          )
+        ) ~> addUserId() ~> addTenantIdHeader(
+          TENANT_1
+        ) ~> param.api(Full).routes ~> check {
+          withClue(responseAs[String]) {
+            status shouldEqual StatusCodes.OK
+          }
+          val eventList = responseAs[List[Long]]
+          eventList.length shouldBe 3
+        }
+
+        verifyAspectData(
+          param,
+          "record-1",
+          "test-aspect",
+          """
+            |{
+            |  "a" : [2,5],
+            |  "b" : "no",
+            |  "c" : "c-value",
+            |  "d" : {
+            |      "d2":2
+            |   }
+            |}
+            |""".stripMargin
+        )
+
+        verifyAspectData(
+          param,
+          "record-2",
+          "test-aspect",
+          """
+            |{
+            |  "a" : [2,5],
+            |  "b" : "no",
+            |  "c" : "c-value",
+            |  "d" : {
+            |      "d2":2
+            |   }
+            |}
+            |""".stripMargin
+        )
+
+        verifyAspectData(
+          param,
+          "record-3",
+          "test-aspect",
+          """
+            |{
+            |  "a" : [2,5],
+            |  "b" : "no",
+            |  "c" : "c-value",
+            |  "d" : {
+            |      "d2":2
+            |   }
+            |}
+            |""".stripMargin
+        )
+      }
+
+    }
+
     describe(
       "Modify endpoint {put} /v0/registry/records/{recordId}/aspects/{aspectId}"
     ) {
