e04767ac1f06410f8435969e49fbda51ece72e5c
Jacky Jiang <t83714@gmail.com>
2022-07-05 10:47:18 +1000

    Allow the `no cache` behaviour of `whoami` & `getUserById` api to be turned off

--- magda-authorization-api/src/utilityMiddlewares.ts
+++ magda-authorization-api/src/utilityMiddlewares.ts
@@ -1,4 +1,8 @@
 export const NO_CACHE = function (req: any, res: any, next: any) {
+    if (req?.query?.allowCache === "true") {
+        next();
+        return;
+    }
     res.set({
         "Cache-Control": "no-cache, no-store, must-revalidate",
         Pragma: "no-cache",
