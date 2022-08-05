1ad3026d444fa7a6343db97726d401208a724fa3
Jacky Jiang <t83714@gmail.com>
2022-08-05 23:29:53 +1000

    add api docs to tenants api

--- magda-tenant-api/src/createTenantsRouter.ts
+++ magda-tenant-api/src/createTenantsRouter.ts
@@ -10,6 +10,9 @@ import {
     createServiceProbe
 } from "magda-typescript-common/src/express/status";
 import AuthDecisionQueryClient from "magda-typescript-common/src/opa/AuthDecisionQueryClient";
+import ServerError from "magda-typescript-common/src/ServerError";
+import handleServerError from "magda-typescript-common/src/handleServerError";
+import { DatabaseError } from "pg";
 
 export interface ApiRouterOptions {
     database: Database;
@@ -47,6 +50,27 @@ export default function createApiRouter(options: ApiRouterOptions) {
 
     installStatusRouter(router, status);
 
+    /**
+     * @apiGroup Tenants
+     * @api {get} /v0/tenant/tenants Get the list of all tenants
+     * @apiDescription Get the list of all tenants
+     * You need have `object/tenant/read` permission to access this API.
+     * > Please note: multi-tenancy are still an experimental feature. API might changes largely over the time.
+     *
+     * @apiSuccessExample {json} 200
+     *    [{
+     *       "domainname": "example.com",
+     *       "id": "6",
+     *       "enabled": true
+     *    }]
+     *
+     * @apiErrorExample {json} 401/500
+     *    {
+     *      "isError": true,
+     *      "errorCode": 401, //--- or 500 depends on error type
+     *      "errorMessage": "Not authorized"
+     *    }
+     */
     router.get(
         "/tenants",
         requireUnconditionalAuthDecision(authDecisionClient, {
@@ -65,12 +89,40 @@ export default function createApiRouter(options: ApiRouterOptions) {
                     res.send("Incorrect tenant ID");
                 }
             } catch (e) {
-                console.error(e);
-                res.status(500);
+                handleServerError(req, res, e);
             }
         }
     );
 
+    /**
+     * @apiGroup Tenants
+     * @api {post} /v0/tenant/tenants Create a new tenant
+     * @apiDescription Create a new tenant
+     * You need have `object/tenant/create` permission to access this API.
+     * > Please note: multi-tenancy are still an experimental feature. API might changes largely over the time.
+     *
+     * @apiParam (Body) {string} domainname the domain name serves the tenant.
+     * @apiParam (Body) {boolean} [enabled] Default to `true`.
+     *
+     * @apiParamExample (Body) {json}:
+     *     {
+     *       "domainname": "example.com"
+     *     }
+     *
+     * @apiSuccessExample {json} 200
+     *    {
+     *       "domainname": "example.com",
+     *       "id": "6",
+     *       "enabled": true
+     *    }
+     *
+     * @apiErrorExample {json} 401/500
+     *    {
+     *      "isError": true,
+     *      "errorCode": 401, //--- or 500 depends on error type
+     *      "errorMessage": "Not authorized"
+     *    }
+     */
     router.post(
         "/tenants",
         requireUnconditionalAuthDecision(authDecisionClient, {
@@ -78,8 +130,20 @@ export default function createApiRouter(options: ApiRouterOptions) {
         }),
         async function (req, res) {
             try {
+                if (!req?.body?.domainname) {
+                    throw new ServerError(
+                        "`domainname` field is required",
+                        400
+                    );
+                }
                 if (hasAdminPortalId(req)) {
-                    const tenantId = await database.createTenant(req.body);
+                    const tenantId = await database.createTenant({
+                        domainname: req.body.domainname,
+                        enabled:
+                            typeof req?.body?.enabled === "boolean"
+                                ? req.body.enabled
+                                : true
+                    });
                     res.json(tenantId);
                     res.status(201);
                 } else {
@@ -88,10 +152,19 @@ export default function createApiRouter(options: ApiRouterOptions) {
                     res.send("Incorrect tenant ID.");
                 }
             } catch (e) {
-                console.error(e);
-                res.status(500);
+                if (e instanceof DatabaseError && e.code === "23505") {
+                    handleServerError(
+                        req,
+                        res,
+                        new ServerError(
+                            "Cannot create a new tenant for an existing domain",
+                            400
+                        )
+                    );
+                } else {
+                    handleServerError(req, res, e);
+                }
             }
-            res.end();
         }
     );
 
