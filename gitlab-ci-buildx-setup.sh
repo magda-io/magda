7d2b3885d155336f8bf6b9bc73be905c43066e9b
Jacky Jiang <t83714@gmail.com>
2023-05-18 20:10:20 +1000

    force build container bootstrap

--- gitlab-ci-buildx-setup.sh
+++ gitlab-ci-buildx-setup.sh
@@ -7,4 +7,4 @@ docker -v
 docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
 
 docker context create builder-context
-docker buildx create --name builderx --driver docker-container --use builder-context 
+docker buildx create --name builderx --driver docker-container --use builder-context --bootstrap
