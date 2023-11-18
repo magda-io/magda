a228fcf88ef0370f79fff284690eea77a80ea5c6
Jacky Jiang <t83714@gmail.com>
2023-11-18 17:31:04 +1100

    remove pancake from script

--- magda-scss-compiler/setup.sh
+++ magda-scss-compiler/setup.sh
@@ -10,13 +10,3 @@ rm -Rf node_modules/@gov.au/pancake-sass/node_modules/node-sass
 echo "Rebuild node-sass"
 npm rebuild node-sass
 echo "Rebuild node-sass done!"
-echo "Re-cook the pancake"
-#Symbol link doesn't work
-cp -R node_modules/@magda/web-client ./web-client
-rm -Rf ./web-client/src/pancake
-./node_modules/@gov.au/pancake/bin/pancake ./web-client
-# Copy back to node_modules/@magda/web-client. Therefore, the same program can run correctly both in / out docker
-# Will make local test easier
-rm -Rf ./node_modules/@magda/web-client/src/pancake
-cp -R ./web-client/src/pancake ./node_modules/@magda/web-client/src/pancake
-echo "Pancake re-cooked~"
