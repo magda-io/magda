804aa51d0cb82213d60c8690b548b84bd5d7031f
Jacky Jiang <t83714@gmail.com>
2024-04-15 15:25:43 +1000

    update sitemaps docs

--- magda-web-server/src/buildSitemapRouter.ts
+++ magda-web-server/src/buildSitemapRouter.ts
@@ -55,10 +55,12 @@ export default function buildSitemapRouter({
      * @apiDescription A [sitemaps protocol interface](https://www.sitemaps.org/protocol.html) that is prepared for external search engines to harvest datasets from Magda.
      * The sitemap index is produced based on the live data in the metadata store database. By default, the sitemap index will be cached for 86400 seconds (24 hours).
      * This setting can be adjusted via `sitemapCacheSeconds` of [web-server](https://github.com/magda-io/magda/tree/main/deploy/helm/internal-charts/web-server) module helm chart.
-     * Please note: due to the cache and the search engine indexing delay, the total number of datasets in the sitemap index may be different from the dataset total count from the search API.
-     * This sitemaps endpoint is recorded on the default /robots.txt endpoint that follows the [Robots Exclusion Standard](https://en.wikipedia.org/wiki/Robots_exclusion_standard#About_the_standard).
+     * Please note:
+     * 1> due to the cache and the search engine indexing delay, the total number of datasets in the sitemap index may be different from the dataset total count from the search API.
+     * 2> This sitemaps endpoint is recorded on the default /robots.txt endpoint that follows the [Robots Exclusion Standard](https://en.wikipedia.org/wiki/Robots_exclusion_standard#About_the_standard).
+     * 3> Only public datasets are included in the sitemaps. In fact, the sitemaps index is generated with anonymous user access.
      *
-     * @apiSuccessExample {json} 200
+     * @apiSuccessExample {xml} 200
      *    <?xml version="1.0" encoding="UTF-8"?>
      *      <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      *          <sitemap>
@@ -114,19 +116,17 @@ export default function buildSitemapRouter({
 
     /**
      * @apiGroup Sitemaps
-     * @api {get} /sitemap/main.xml Sitemaps main index
-     * @apiDescription List
+     * @api {get} /sitemap/main.xml Sitemaps index entry for the home page
+     * @apiDescription Sitemaps index entry for the home page
      *
-     * @apiSuccessExample {json} 200
+     * @apiSuccessExample {xml} 200
      *    <?xml version="1.0" encoding="UTF-8"?>
-     *      <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     *          <sitemap>
-     *              <loc>https://example.com/sitemap/main.xml</loc>
-     *          </sitemap>
-     *          <sitemap>
-     *              <loc>https://example.com/sitemap/dataset/afterToken/0.xml</loc>
-     *          </sitemap>
-     *       </sitemapindex>
+     *      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+     *          <url>
+     *              <loc>https://example.com/</loc>
+     *              <changefreq>daily</changefreq>
+     *          </url>
+     *      </urlset>
      *
      */
     app.get("/sitemap/main.xml", async (req, res) => {
@@ -152,6 +152,24 @@ export default function buildSitemapRouter({
         }
     });
 
+    /**
+     * @apiGroup Sitemaps
+     * @api {get} /sitemap/dataset/afterToken/{pageToken}.xml Sitemaps index entry for datasets
+     * @apiDescription The sitemaps entrypoint `/sitemap.xml` contains links to all index entries for datasets (paginated by the `pageToken` parameter).
+     * Each index entry contains a list of dataset page links.
+     * You can either visit the dataset page directly (A text based SEO friendly view will be shown if the `User-Agent` request header is [recognised as search engine bot](https://github.com/magda-io/magda/blob/main/magda-web-server/src/shouldRenderCrawlerView.ts#L20)).
+     * Or extract the dataset ID from the dataset page link and access the detailed metadata in JSON format via the [registry get a record by ID API](#api-Registry_Record_Service-GetV0RegistryRecordsId).
+     *
+     * @apiSuccessExample {xml} 200
+     *    <?xml version="1.0" encoding="UTF-8"?>
+     *      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+     *          <url>
+     *              <loc>https://example.com/dataset/ds-a3d075b9-70d7-40b3-a693-70db2a415765</loc>
+     *              <changefreq>weekly</changefreq>
+     *          </url>
+     *      </urlset>
+     *
+     */
     app.get("/sitemap/dataset/afterToken/:afterToken.xml", async (req, res) => {
         try {
             const sms = new SitemapStream({
