1c1f92b3b36107c6b0d58d0eb3fd36187f79da79
Jacky Jiang <t83714@gmail.com>
2024-05-16 11:17:07 +1000

    clear up opensearch building files

--- magda-opensearch-dashboards/setup-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./bin/opensearch-dashboards-plugin remove securityDashboards
-echo "Starting up"
-./opensearch-dashboards-docker-entrypoint.sh opensearch-dashboards
--- magda-opensearch/setup-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Remove security plugin..."
-./bin/opensearch-plugin remove opensearch-security --purge
-
-echo "Starting up"
-./opensearch-docker-entrypoint.sh opensearch
