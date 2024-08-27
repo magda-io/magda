16b13f9ae7f1b5579cee122bc2bc3b0c724ba6cb
Jacky Jiang <t83714@gmail.com>
2024-08-28 01:33:02 +1000

    truncate the context datasets

--- magda-web-client/src/Components/Chatbot/AgentChain.ts
+++ magda-web-client/src/Components/Chatbot/AgentChain.ts
@@ -3,6 +3,7 @@ import {
     ChatPromptTemplate,
     MessagesPlaceholder
 } from "@langchain/core/prompts";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { InitProgressCallback, InitProgressReport } from "@mlc-ai/web-llm";
 import { HumanMessage, BaseMessage, AIMessage } from "@langchain/core/messages";
 import {
@@ -26,14 +27,16 @@ import {
     EVENT_TYPE_PARTIAL_MSG_FINISH
 } from "./Messaging";
 
-const systemPromptTemplate = `
-The current date and time is: {today_date}. You are Magda. An AI assistant designed to help users to locate relevant datasets and answer questions based on the information in the datasets.
-- You need to answer user's latest question based on chat history and possible relevant datasets.
-- When user's question is a general conversation, you should 
+const systemPromptTemplate = `You must identify yourself as "Magda", an AI assistant designed to help users to locate relevant datasets and answer questions based on the information in the datasets.
+- You need to answer user's latest question based on chat history and possible relevant datasets in the context section below.
+- When user's question is a general conversation, you should respond with a greeting.
 - When possible, you should output your answer in markdown format to present information in a visually appealing manner.
 - If you don't know the answer, you should just say that you don't know, you will not try to make up an answer.
+- You can only respond with dataset information available (if any) in the context section below. Do not mention any other datasets unless they are list in context section below.
 
-## Datasets might be relevant to the user question:
+## Context:
+The current date and time is: {today_date}. 
+Possible relevant datasets:
 {relevant_datasets}
 `;
 
@@ -76,9 +79,9 @@ class AgentChain {
             model: "Qwen2-7B-Instruct-q4f16_1-MLC",
             loadProgressCallback: this.onProgress.bind(this),
             chatOptions: {
-                temperature: 0,
-                context_window_size: 32768,
-                sliding_window_size: -1
+                temperature: 0
+                // context_window_size: 32768,
+                // sliding_window_size: -1
             }
         });
         this.chain = this.createChain();
@@ -96,14 +99,34 @@ class AgentChain {
         if (!question) {
             return notFound;
         }
-        const result = await searchDatasets({ q: question });
+        const result = await searchDatasets({ q: question, limit: 3 });
         if (!result?.dataSets?.length) {
             return notFound;
         }
-        const datasets = result.dataSets;
-        return json2Yaml.stringify(
-            datasets.length > 3 ? datasets.slice(0, 3) : datasets
-        );
+        const datasets = result.dataSets.map((item) => ({
+            datasetTitle: item.title,
+            description: item.description,
+            distributions: item.distributions.map((dist) => ({
+                title: dist.title,
+                description: dist.description,
+                downloadURL: dist.downloadURL,
+                format: dist.format
+            }))
+        }));
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 3000,
+            chunkOverlap: 1
+        });
+        return (
+            await Promise.all(
+                datasets
+                    .map((item) => json2Yaml.stringify(item))
+                    .map(async (item) => {
+                        const doc = await splitter.createDocuments([item]);
+                        return doc[0].pageContent;
+                    })
+            )
+        ).join("\n");
     }
 
     async stream(question: string): Promise<AsyncIterable<ChatEventMessage>> {
@@ -187,6 +210,10 @@ class AgentChain {
                 }
             }),
             promptTemplate,
+            RunnableLambda.from((input) => {
+                console.log("propmt:", input?.messages?.[0]?.content);
+                return input;
+            }),
             this.model,
             new StringOutputParser()
         ]);
