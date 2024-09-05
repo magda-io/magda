edede7ddb9b389ad8ee222fc3044877b23834b3d
Jacky Jiang <t83714@gmail.com>
2024-09-06 00:43:38 +1000

    refactor integration test serviceRunner & fix process termination issue & docker resource prune

--- magda-int-test-ts/src/tests/searchApi.spec.ts
+++ magda-int-test-ts/src/tests/searchApi.spec.ts
@@ -83,7 +83,7 @@ async function searchDataset(q: string, userId?: string) {
     );
 }
 
-describe("search api auth integration tests", function (this) {
+describe("search api hybrid integration tests", function (this) {
     this.timeout(ENV_SETUP_TIME_OUT);
 
     const serviceRunner = new ServiceRunner();
@@ -101,6 +101,51 @@ describe("search api auth integration tests", function (this) {
     let branchBId: string = "";
     // dataset will be set to section C
     let sectionCId: string = "";
+    const datasets: any[] = [
+        {
+            aspects: {
+                "dcat-dataset-strings": {
+                    title: "a good milk chocolate receipt",
+                    description: `Milk chocolate is a type of chocolate that is made from a blend of cocoa solids, cocoa butter, sugar, and milk or milk powder. Unlike dark chocolate, which has a higher percentage of cocoa solids and little to no milk content, milk chocolate contains a significant amount of milk, giving it a creamier texture and a sweeter, milder flavor. Milk chocolate typically has a lower cocoa content (often between 10% to 50%) compared to dark chocolate. It is commonly used in candy bars, confections, and various desserts.`
+                },
+                publishing: {
+                    state: "published"
+                },
+                "access-control": {
+                    orgUnitId: sectionCId
+                }
+            }
+        },
+        {
+            aspects: {
+                "dcat-dataset-strings": {
+                    title: "a good chocolate milk receipt",
+                    description: `Chocolate milk is a sweetened, flavored milk beverage that is made by mixing milk (usually cow's milk) with cocoa powder or chocolate syrup and often sugar. The drink can be served cold or hot and is popular as both a refreshing treat and a comfort beverage. Chocolate milk can be made at home or purchased pre-mixed in bottles or cartons. Some versions also include additional ingredients like vanilla extract or stabilizers to improve texture and flavor.`
+                },
+                publishing: {
+                    state: "published"
+                },
+                "access-control": {
+                    orgUnitId: sectionCId
+                }
+            }
+        },
+        {
+            aspects: {
+                "dcat-dataset-strings": {
+                    title: "sydney water revenue report",
+                    description:
+                        "The Sydney Water Revenue Report is a quarterly report that provides information on the revenue collected by Sydney Water. The report ID is RS2SDW2S."
+                },
+                publishing: {
+                    state: "published"
+                },
+                "access-control": {
+                    orgUnitId: sectionCId
+                }
+            }
+        }
+    ];
 
     before(async function (this) {
         this.timeout(ENV_SETUP_TIME_OUT);
@@ -150,55 +195,15 @@ describe("search api auth integration tests", function (this) {
     it("should rank chocolate milk over milk chocolate and not return irrelevant result when searching `chocolate milk`", async () => {
         const datasetId1 = await createTestDatasetByUser(
             DEFAULT_ADMIN_USER_ID,
-            {
-                aspects: {
-                    "dcat-dataset-strings": {
-                        title: "a good milk chocolate receipt",
-                        description: `Milk chocolate is a type of chocolate that is made from a blend of cocoa solids, cocoa butter, sugar, and milk or milk powder. Unlike dark chocolate, which has a higher percentage of cocoa solids and little to no milk content, milk chocolate contains a significant amount of milk, giving it a creamier texture and a sweeter, milder flavor. Milk chocolate typically has a lower cocoa content (often between 10% to 50%) compared to dark chocolate. It is commonly used in candy bars, confections, and various desserts.`
-                    },
-                    publishing: {
-                        state: "published"
-                    },
-                    "access-control": {
-                        orgUnitId: sectionCId
-                    }
-                }
-            } as any
+            datasets[0]
         );
         const datasetId2 = await createTestDatasetByUser(
             DEFAULT_ADMIN_USER_ID,
-            {
-                aspects: {
-                    "dcat-dataset-strings": {
-                        title: "a good chocolate milk receipt",
-                        description: `Chocolate milk is a sweetened, flavored milk beverage that is made by mixing milk (usually cow's milk) with cocoa powder or chocolate syrup and often sugar. The drink can be served cold or hot and is popular as both a refreshing treat and a comfort beverage. Chocolate milk can be made at home or purchased pre-mixed in bottles or cartons. Some versions also include additional ingredients like vanilla extract or stabilizers to improve texture and flavor.`
-                    },
-                    publishing: {
-                        state: "published"
-                    },
-                    "access-control": {
-                        orgUnitId: sectionCId
-                    }
-                }
-            } as any
+            datasets[1]
         );
         const datasetId3 = await createTestDatasetByUser(
             DEFAULT_ADMIN_USER_ID,
-            {
-                aspects: {
-                    "dcat-dataset-strings": {
-                        title: "sydney water revenue report",
-                        description:
-                            "The Sydney Water Revenue Report is a quarterly report that provides information on the revenue collected by Sydney Water. The report ID is RS2SDW2S."
-                    },
-                    publishing: {
-                        state: "published"
-                    },
-                    "access-control": {
-                        orgUnitId: sectionCId
-                    }
-                }
-            } as any
+            datasets[2]
         );
 
         let indexResult = await Try(indexerApiClient.indexDataset(datasetId3));
@@ -236,55 +241,15 @@ describe("search api auth integration tests", function (this) {
     it("should rank milk chocolate over chocolate milk and not return irrelevant result when searching `milk chocolate`", async () => {
         const datasetId1 = await createTestDatasetByUser(
             DEFAULT_ADMIN_USER_ID,
-            {
-                aspects: {
-                    "dcat-dataset-strings": {
-                        title: "a good milk chocolate receipt",
-                        description: `Milk chocolate is a type of chocolate that is made from a blend of cocoa solids, cocoa butter, sugar, and milk or milk powder. Unlike dark chocolate, which has a higher percentage of cocoa solids and little to no milk content, milk chocolate contains a significant amount of milk, giving it a creamier texture and a sweeter, milder flavor. Milk chocolate typically has a lower cocoa content (often between 10% to 50%) compared to dark chocolate. It is commonly used in candy bars, confections, and various desserts.`
-                    },
-                    publishing: {
-                        state: "published"
-                    },
-                    "access-control": {
-                        orgUnitId: sectionCId
-                    }
-                }
-            } as any
+            datasets[0]
         );
         const datasetId2 = await createTestDatasetByUser(
             DEFAULT_ADMIN_USER_ID,
-            {
-                aspects: {
-                    "dcat-dataset-strings": {
-                        title: "a good chocolate milk receipt",
-                        description: `Chocolate milk is a sweetened, flavored milk beverage that is made by mixing milk (usually cow's milk) with cocoa powder or chocolate syrup and often sugar. The drink can be served cold or hot and is popular as both a refreshing treat and a comfort beverage. Chocolate milk can be made at home or purchased pre-mixed in bottles or cartons. Some versions also include additional ingredients like vanilla extract or stabilizers to improve texture and flavor.`
-                    },
-                    publishing: {
-                        state: "published"
-                    },
-                    "access-control": {
-                        orgUnitId: sectionCId
-                    }
-                }
-            } as any
+            datasets[1]
         );
         const datasetId3 = await createTestDatasetByUser(
             DEFAULT_ADMIN_USER_ID,
-            {
-                aspects: {
-                    "dcat-dataset-strings": {
-                        title: "sydney water revenue report",
-                        description:
-                            "The Sydney Water Revenue Report is a quarterly report that provides information on the revenue collected by Sydney Water. The report ID is RS2SDW2S"
-                    },
-                    publishing: {
-                        state: "published"
-                    },
-                    "access-control": {
-                        orgUnitId: sectionCId
-                    }
-                }
-            } as any
+            datasets[2]
         );
 
         let indexResult = await Try(indexerApiClient.indexDataset(datasetId3));
@@ -316,5 +281,49 @@ describe("search api auth integration tests", function (this) {
         expect(r.value?.dataSets?.[1]?.identifier).to.equal(datasetId2);
         // should only find 2 relevant datasets
         expect(r.value?.dataSets?.length).to.equal(2);
+    });
+
+    it("should be able to searching keyword without semantic meaning `RS2SDW2S`", async () => {
+        const datasetId1 = await createTestDatasetByUser(
+            DEFAULT_ADMIN_USER_ID,
+            datasets[0]
+        );
+        const datasetId2 = await createTestDatasetByUser(
+            DEFAULT_ADMIN_USER_ID,
+            datasets[1]
+        );
+        const datasetId3 = await createTestDatasetByUser(
+            DEFAULT_ADMIN_USER_ID,
+            datasets[2]
+        );
+
+        let indexResult = await Try(indexerApiClient.indexDataset(datasetId3));
+        expect(indexResult.error).to.not.be.an.instanceof(Error);
+        expect(indexResult.value?.successes).to.equal(1);
+
+        indexResult = await Try(indexerApiClient.indexDataset(datasetId2));
+        expect(indexResult.error).to.not.be.an.instanceof(Error);
+        expect(indexResult.value?.successes).to.equal(1);
+
+        indexResult = await Try(indexerApiClient.indexDataset(datasetId1));
+        expect(indexResult.error).to.not.be.an.instanceof(Error);
+        expect(indexResult.value?.successes).to.equal(1);
+
+        let r = await Try(searchDataset("RS2SDW2S", testUserId));
+        expect(r.error).to.not.be.an.instanceof(Error);
+        console.log("test 3 Original datasets: ", [
+            datasetId1,
+            datasetId2,
+            datasetId3
+        ]);
+        console.log(
+            "test 3 Search result: ",
+            r.value?.dataSets.map((d: any) => d.identifier)
+        );
+        console.log("test 3 Search result data: ", r.value?.dataSets);
+        // first one should be the sydney report dataset
+        expect(r.value?.dataSets?.[0]?.identifier).to.equal(datasetId3);
+        // should only find 1 relevant datasets
+        //expect(r.value?.dataSets?.length).to.equal(1);
     });
 });
