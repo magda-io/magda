02e389a8cd37a40f90d280fbca298db21cffeebb
Jacky Jiang <t83714@gmail.com>
2024-11-06 23:48:22 +1100

    add searchDatasets tool

---
+++ magda-web-client/src/Components/Chatbot/tools/searchDataset.ts
@@ -0,0 +1,55 @@
+import { searchDatasets } from "api-clients/SearchApis";
+import { createChatEventMessageCompleteMsg } from "../Messaging";
+import { markdownTable } from "markdown-table";
+import { config } from "../../../config";
+
+const MAX_DESC_DISPLAY_LENGTH = 250;
+const { uiBaseUrl } = config;
+
+async function retrieveDatasets(question: string) {
+    const notFound =
+        "Sorry, I didn't find any datasets related to your inquiry.";
+    if (!question) {
+        return notFound;
+    }
+    const result = await searchDatasets({ q: question, limit: 5 });
+    if (!result?.dataSets?.length) {
+        return notFound;
+    }
+    const datasets = result.dataSets.map((item) => {
+        const desc = (item?.description?.length > MAX_DESC_DISPLAY_LENGTH
+            ? item.description.substring(0, MAX_DESC_DISPLAY_LENGTH + 1) + "..."
+            : item.description
+        ).replaceAll("\n", "<br/>");
+        const title = `[item.title](${
+            uiBaseUrl === "/"
+                ? `/datasets/${item.identifier}`
+                : `${uiBaseUrl}/datasets/${item.identifier}`
+        })`;
+        return [title, desc];
+    });
+
+    const table = markdownTable([["Title", "Description"], ...datasets]);
+    return `I found the following datasets might be related to your inquiry:\n ${table}`;
+}
+
+const searchDataset = {
+    name: "searchDataset",
+    func: async function (queryString: string) {
+        const queue = (this as any).queue;
+        queue.push(createChatEventMessageCompleteMsg("Searching datasets..."));
+        return await retrieveDatasets(queryString);
+    },
+    description:
+        "search and return datasets that are relevant to supplied `queryString`",
+    parameters: [
+        {
+            name: "queryString",
+            type: "string",
+            description: "a query string used to search relevant datasets."
+        }
+    ],
+    requiredParameters: ["queryString"]
+};
+
+export default searchDataset;
