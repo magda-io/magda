4b4e99acc2a700f4e4bd5ed38b516847b0347868
JingHuang Zhang <106435835+JHZhang2736@users.noreply.github.com>
2025-05-26 18:55:44 +1000

    Set Vector workload mode to "on_disk" (#3598)
    * Set vector workload mode to on_disk. Related parameters are now configurable.
    * Can now configure vector workload options via the Helm chart.
    * Upgrade opensearch to v2.19.1
    * set in-memory mode as the default config
    * update helm docs
    * Run hybrid search tests separately for `on_disk` and `in_memory` modes
    * replace vectorConfig with knnVectorFieldConfig; Add user instruction to helm doc; set to on_memory by default in helm charts;
    * fix scala format
    * CI integration test improvement: avoid port forward when not in Kubernetes

--- magda-scala-common/src/main/scala/au/csiro/data61/magda/search/elasticsearch/HybridSearchConfig.scala
+++ magda-scala-common/src/main/scala/au/csiro/data61/magda/search/elasticsearch/HybridSearchConfig.scala
@@ -8,11 +8,11 @@ object HybridSearchConfig {
   val configPath = "elasticSearch.indices.datasets.hybridSearch"
   var _conf: Option[Config] = None
 
-  def setAppConfig(appConfig: Config) = {
+  def setAppConfig(appConfig: Config): Unit = {
     _conf = Some(appConfig.getConfig(configPath))
   }
 
-  def get(useCache: Boolean = true) = {
+  def get(useCache: Boolean = true): Config = {
     if (useCache && _conf.nonEmpty) {
       _conf.get
     } else {
@@ -25,13 +25,34 @@ object HybridSearchConfig {
     }
   }
 
-  def enabled = get().getBoolean("enabled")
-  def k = get().getOptionalInt("k")
-  def minScore = get().getOptionalDouble("minScore")
-  def maxDistance = get().getOptionalDouble("maxDistance")
-  def searchPipeline = get().getConfig("searchPipeline")
-  def searchPipelineAutoCreate = searchPipeline.getBoolean("autoCreate")
-  def searchPipelineId = searchPipeline.getString("id")
+  def enabled: Boolean = get().getBoolean("enabled")
+  def k: Option[Int] = get().getOptionalInt("k")
+  def minScore: Option[Double] = get().getOptionalDouble("minScore")
+  def maxDistance: Option[Double] = get().getOptionalDouble("maxDistance")
+  def searchPipeline: Config = get().getConfig("searchPipeline")
+
+  def searchPipelineAutoCreate: Boolean =
+    searchPipeline.getBoolean("autoCreate")
+  def searchPipelineId: String = searchPipeline.getString("id")
+
+  private def knnVectorFieldConfig = get().getConfig("knnVectorFieldConfig")
+  def mode: Option[String] = knnVectorFieldConfig.getOptionalString("mode")
+  def dimension: Int = knnVectorFieldConfig.getInt("dimension")
+
+  def spaceType: Option[String] =
+    knnVectorFieldConfig.getOptionalString("spaceType")
+
+  def efConstruction: Option[Int] =
+    knnVectorFieldConfig.getOptionalInt("efConstruction")
+  def efSearch: Option[Int] = knnVectorFieldConfig.getOptionalInt("efSearch")
+  def m: Option[Int] = knnVectorFieldConfig.getOptionalInt("m")
+
+  def compressionLevel: Option[String] =
+    knnVectorFieldConfig.getOptionalString("compressionLevel")
+  private def encoderConfig = knnVectorFieldConfig.getConfig("encoder")
+  def encoderName: Option[String] = encoderConfig.getOptionalString("name")
+  def encoderType: Option[String] = encoderConfig.getOptionalString("type")
+  def encoderClip: Option[Boolean] = encoderConfig.getOptionalBoolean("clip")
 
   val queryContextFieldName = "queryContext"
   val queryContextVectorFieldName = "queryContextVector"
